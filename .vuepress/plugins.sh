be5c3a49f84925bf6d4222dee532187d079dc7a6
tchiotludo <tchiot.ludo@gmail.com>
2020-03-22 14:40:59 +0100

    feat(docs): add task-fs plugins

--- .vuepress/plugins.sh
+++ .vuepress/plugins.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")" || exit
 
-PLUGINS=('storage-gcs' 'storage-minio' 'task-gcp' 'task-kubernetes' 'task-notifications' 'task-serdes')
+PLUGINS=('storage-gcs' 'storage-minio' 'task-fs' 'task-gcp' 'task-kubernetes' 'task-notifications' 'task-serdes')
 
 mkdir build
 cd build || exit
