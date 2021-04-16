efa46825b29e951c891c3f57db57e42385c2341f
tchiotludo <tchiot.ludo@gmail.com>
2021-04-16 08:33:11 +0200

    fix(docs): add terraform docs

--- .vuepress/plugins.sh
+++ .vuepress/plugins.sh
@@ -3,9 +3,10 @@ set -e
 
 cd "$(dirname "$0")" || exit
 
-mkdir build
+mkdir -p build
 cd build || exit
 
+# plugins
 echo -e "\e[42m Doc :\e[0m Core"
 docker run --rm --name kestra-docs -i \
     -v "$(pwd)"/cores/docs:/app/docs \
@@ -17,3 +18,17 @@ docker run --rm --name kestra-docs -i \
 for DOC in $(find . -type d -name md); do
     cp -r "${DOC}"/* ../../plugins
 done
+
+# terraform
+cd ..
+mkdir -p terraform
+cd terraform || exit
+
+git clone https://github.com/kestra-io/terraform-provider-kestra
+cd terraform-provider-kestra/docs/
+rm index.md
+
+find . -type f -name "*.md" -exec sed -i 's/subcategory: ""/editLink: false/g' {} +
+find . -type f -name "*.md" -exec sed -Ei  's/page_title: "([^ ]+).*"/title: \1/g' {} +
+find . -type f -name "*.md" -exec sed -i 's/```terraform/```hcl/g' {} +
+cp -R * ../../../../docs/terraform/
---
+++ docs/terraform/README.md
@@ -0,0 +1,15 @@
+---
+order: 7
+---
+# Terraform Integration
+
+
+We provide a [terraform provider](https://registry.terraform.io/providers/kestra-io/kestra/latest) available on [Terraform registry](https://registry.terraform.io/) in order to facilitate all CI/CD management for your Kestra ressource.
+
+Find below all the terraform resources: 
+
+* Resources:
+  <ChildTableOfContents pageUrl="/docs/terraform/resources/"/>
+* Data Sources:
+  <ChildTableOfContents pageUrl="/docs/terraform/data-sources/"/>
+
