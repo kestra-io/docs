8ceb00352d8aa67a9cb03f63cc18fe0d8d918c5a
tchiotludo <tchiotludo@users.noreply.github.com>
2021-12-14 12:58:53 +0000

    Automated deployment: Tue Dec 14 12:58:53 UTC 2021 5ab5474aa7a4fe7fac78d5539916d208d94d1981

---
+++ assets/js/23.137de145.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{608:function(t,e,n){"use strict";n.r(e);var s={functional:!0,props:{type:{type:String,default:"success"},text:String},render:function(t,e){var n=e.props,s=e.slots;return t("span",{class:["badge","badge-"+n.type]},n.text||s().default)}},p=n(13),a=Object(p.a)(s,void 0,void 0,!1,null,null,null);e.default=a.exports}}]);
