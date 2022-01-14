fc2c74f35bc37ae12791cc3936d9165d31e06c75
tchiotludo <tchiotludo@users.noreply.github.com>
2022-01-14 14:18:16 +0000

    deploy: d1bc65805fc5ed28db9803f4abca33cc272816a6

--- assets/js/25.b1d14874.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{978:function(t,e,n){"use strict";n.r(e);var s={functional:!0,props:{type:{type:String,default:"success"},text:String},render:function(t,e){var n=e.props,s=e.slots;return t("span",{class:["badge","badge-"+n.type]},n.text||s().default)}},p=n(19),a=Object(p.a)(s,void 0,void 0,!1,null,null,null);e.default=a.exports}}]);
