92223941d84975ea010d4e5ad00ca8fabdb52abc
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-02-02 13:54:07 +0100

    feat(docs): add caption on every image

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -82,6 +82,20 @@ module.exports = {
         extendMarkdown: md => {
             md.set({breaks: true})
             md.use(require('markdown-it-mark'))
+            md.use((md) => {
+                let old = md.renderer.rules.image;
+
+                md.renderer.rules.image = function (tokens, idx, options, env, self) {
+                    if (tokens[idx].content) {
+                        let attrs = tokens[idx].attrs;
+                        let title = tokens[idx].content;
+                        let src = attrs[0][1];
+                        let alt = tokens[idx].content;
+                        return `<figure><img src="${src}" alt="${alt}" title="${title}" /><figcaption>${title}</figcaption></figure>`
+                    }
+                    return old(tokens, idx, options, env, self);
+                };
+            });
 
             md.renderer.rules.table_open = function (tokens, idx) {
                 return '<div class="table-responsive"><table class="table table-bordered table-hover table-striped">';
