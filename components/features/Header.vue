b9deadf6f32c47707acdaf755287b9042a762b41
Loïc Mathieu <loikeseke@gmail.com>
2023-04-12 11:32:51 +0200

    feat: update features page

--- components/features/Header.vue
+++ components/features/Header.vue
@@ -1,15 +1,22 @@
 <template>
-    <div class="main">
-        <div class="hero container text-center">
-            <h1>Build stunning data pipelines, blazing fast, deploy in clicks<span class="dot">.</span></h1>
-            <h2>Build declarative data pipelines at scale. Edit your code live from the UI and benefit from faster feedback loops without CI/CD</h2>
-            <div class="text-center cta">
-                <a href="https://demo.kestra.io" target="_blank" class="btn btn-lg btn-primary me-2">
-                    <Console /> Live demo
-                </a>
-                <a href="/docs/getting-started" class="btn btn-lg btn-secondary">
-                    <Flash /> Getting started
-                </a>
+    <div class="container-fluid">
+        <div class="hero container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Fast, Scalable, and Declarative Data Management</h1>
+                    <h2>Create and Deploy all kinds Data Pipelines with Ease and Speed with Kestra: Your All-in-One Solution for Orchestrating Workflows</h2>
+                    <div class="cta">
+                        <a href="/docs/getting-started" class="btn btn-lg btn-primary me-2">
+                            Get started <Flash />
+                        </a>
+                        <a href="https://demo.kestra.io" target="_blank" class="btn btn-lg btn-primary">
+                            Try live tool
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="/landing/features/features-ui.png" width="600"/>
+                </div>
             </div>
         </div>
     </div>
@@ -17,41 +24,20 @@
 
 <script>
     import Flash from "vue-material-design-icons/Flash.vue";
-    import Console from "vue-material-design-icons/Console.vue"
-    import Github from "vue-material-design-icons/Github.vue";
 
     export default {
-        components: {Github, Console, Flash},
-        computed: {
-            companies() {
-                return [
-                    "aimtec",
-                    "cleverconnect",
-                    "decathlon",
-                    "dnb",
-                    "fortinet",
-                    "hcl",
-                    "huawei",
-                    "iqtig",
-                    "leroymerlin",
-                    "ntico",
-                    "tencent",
-                    "twosix",
-                    "facily",
-                ]
-                    .sort(() => .5 - Math.random())
-            },
-        }
+        components: {Flash},
     }
 </script>
 
 <style lang="scss" scoped>
     @import "../../assets/styles/variable";
 
-    .main {
-        background: $purple-7;
+    .container-fluid {
+        background: $purple-19;
         color: var(--bs-white);
         padding-top: 80px;
         margin-top: -80px;
     }
 </style>
+
