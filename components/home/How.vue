bbd723ba6240de34a94e548175d90bbf67e09257
Benoit <benoit@kestra.io>
2023-04-18 09:33:58 +0200

    [feat/nuxt] add animations on homepage

--- components/home/How.vue
+++ components/home/How.vue
@@ -18,10 +18,10 @@
             </div>
 
             <div class="row mb-5">
-                <div class="col-md-6">
+                <div class="col-md-6" data-aos="fade-left" data-aos-duration="1000">
                     <img class="img-fluid" src="/landing/home/how-2.svg" alt="" />
                 </div>
-                <div class="col-md-6 p-5">
+                <div class="col-md-6 p-5" data-aos="fade-left" data-aos-duration="1000">
                     <p class="overline">Step 2</p>
                     <h3>Schedule your workflows</h3>
                     <p>Specify when and how often they should run, and how to trigger them. You can also trigger workflows manually, or run them as part of a larger pipeline.</p>
@@ -29,23 +29,23 @@
             </div>
 
             <div class="row mb-5">
-                <div class="col-md-6 p-5">
+                <div class="col-md-6 p-5" data-aos="fade-right" data-aos-duration="1000">
                     <p class="overline">Step 3</p>
                     <h3>Execute your workflows</h3>
                     <p>
                         When a workflow is triggered, Kestra orchestrates the execution of each step, ensuring that data is processed correctly and dependencies are satisfied.
                     </p>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-6" data-aos="fade-right" data-aos-duration="1000">
                     <img class="img-fluid" src="/landing/home/how-3.svg" alt="" />
                 </div>
             </div>
 
             <div class="row mb-5">
-                <div class="col-md-6">
+                <div class="col-md-6" data-aos="fade-left" data-aos-duration="1000">
                     <img class="img-fluid" src="/landing/home/how-4.svg" alt="" />
                 </div>
-                <div class="col-md-6 p-5">
+                <div class="col-md-6 p-5" data-aos="fade-left" data-aos-duration="1000">
                     <p class="overline">Step 4</p>
                     <h3>Integrate with your existing data stack</h3>
                     <p>Kestra integrates with a wide variety of data sources and tools, so you can easily connect your workflows to your existing data stack. This includes support for popular databases, file formats, APIs, and more.</p>
@@ -53,12 +53,12 @@
             </div>
 
             <div class="row mb-5">
-                <div class="col-md-6 p-5">
+                <div class="col-md-6 p-5" data-aos="fade-right" data-aos-duration="1000">
                     <p class="overline">Step 5</p>
                     <h3>Monitor and optimize your workflows</h3>
                     <p>Track the performance of your workflows, identify bottlenecks, and optimize them for speed and efficiency.</p>
                 </div>
-                <div class="col-md-6 row mb-5">
+                <div class="col-md-6 row mb-5" data-aos="fade-right" data-aos-duration="1000">
                     <img class="img-fluid" src="/landing/home/how-5.svg" alt="" />
                 </div>
             </div>
@@ -67,10 +67,10 @@
                 <p class="last-words">
                     By using Kestra as your orchestrator, you can easily automate complex workflows, integrate with your existing data stack, and improve the speed and efficiency of your data processing.
                 </p>
-                <NuxtLink href="/docs/getting-started" class="btn btn-lg btn-primary me-2">
+                <NuxtLink href="/docs/getting-started" class="btn btn-lg btn-primary me-2" data-aos="zoom-in">
                     Get started
                 </NuxtLink>
-                <a href="https://demo.kestra.io" target="_blank" class="btn btn-lg btn-secondary">
+                <a href="https://demo.kestra.io" target="_blank" class="btn btn-lg btn-secondary" data-aos="zoom-in">
                     Live demo
                 </a>
             </div>
