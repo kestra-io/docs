5761ec2c81e98bcba5fd24b8c0fea5905444222a
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-04-19 07:51:03 +0200

    feat(landing): redirect all .html files

---
+++ server/middleware/redirect.js
@@ -0,0 +1,7 @@
+export default defineEventHandler((event) => {
+    const url = event.node.req.url;
+
+    if (url.endsWith(".html")) {
+        return sendRedirect(event, url.substring(0, url.length - 5).toLocaleLowerCase(), 301);
+    }
+})
