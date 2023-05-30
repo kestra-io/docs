8e217de28a8dd754944fbc83504c9145b18dd698
brian-mulier-p <bmmulier@hotmail.fr>
2023-05-30 13:12:16 +0200

    fix(site): companies are now scrollable when overflowing (#277)

--- components/layout/Companies.vue
+++ components/layout/Companies.vue
@@ -1,23 +1,42 @@
 <template>
-    <div class="companies">
-        <template v-for="(img, index) in companies">
-            <img
-                data-aos="fade-up"
-                :class="{'inverted': inverted}"
-                :data-aos-delay="index*50"
-                :src="'/landing/companies/' + img  + '.svg'"
-                :alt="img"
-            />
-        </template>
+    <div ref="container" class="container">
+        <div ref="companies" class="companies">
+            <template v-for="(img, index) in companies">
+                <img
+                    data-aos="fade-up"
+                    :class="{'inverted': inverted}"
+                    :data-aos-delay="index*50"
+                    :src="'/landing/companies/' + img  + '.svg'"
+                    :alt="img"
+                />
+            </template>
+        </div>
     </div>
 </template>
 
 <script>
     export default defineComponent({
+        mounted() {
+            window.addEventListener("resize", this.autoScrollIfNeeded);
+            this.autoScrollIfNeeded();
+        },
         props: {
             inverted: {
                 type: Boolean,
                 default: false
+            }
+        },
+        methods: {
+            autoScrollIfNeeded() {
+                if (this.$refs && this.$refs.companies) {
+                    const companies = this.$refs.companies;
+                    const classes = companies.classList;
+                    if (companies.clientWidth >= this.$refs.container.parentElement.offsetWidth) {
+                        classes.add("scrolling");
+                    } else {
+                        classes.remove("scrolling")
+                    }
+                }
             }
         },
         computed: {
@@ -46,19 +65,41 @@
 <style lang="scss" scoped>
     @import "../../assets/styles/variable";
 
-    .companies {
-        display: flex;
-        flex-wrap: nowrap;
-        align-items: center;
-        justify-content: center;
-        overflow: hidden;
+    .container {
+        width: fit-content;
+        max-width: unset;
 
-        img {
-            margin-right: calc($spacer * 2);
+        .companies {
+            display: flex;
+            flex-wrap: nowrap;
+            overflow-x: auto;
+            overflow-y: hidden;
+            width: fit-content;
+            margin: auto;
+
+            &.scrolling {
+                animation: auto-scroll 30s infinite linear;
+            }
+
+            @keyframes auto-scroll {
+                0% {
+                    margin-left: 0;
+                }
+                50% {
+                    margin-left: -25%;
+                }
+                100% {
+                    margin-left: 0;
+                }
+            }
+
+            img {
+                margin-right: calc($spacer * 2);
 
 
-            &.inverted {
-                filter: invert(100%);
+                &.inverted {
+                    filter: invert(100%);
+                }
             }
         }
     }
