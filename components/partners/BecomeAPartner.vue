e8682b901da1098c54aeaf4436f9b8f6a2be2dfb
MartinRst <mproset@kestra.io>
2023-09-11 10:16:56 +0200

    Update BecomeAPartner.vue

--- components/partners/BecomeAPartner.vue
+++ components/partners/BecomeAPartner.vue
@@ -32,7 +32,7 @@
                     window.hbspt.forms.create({
                         region: "eu1",
                         portalId: "27220195",
-                        formId: "77f32ae3-0f49-404a-a28d-6dfe92c8bc78",
+                        formId: "d8175470-14ee-454d-afc4-ce8065dee9f2",
                         target: "#hubspotForm"
                     })
                 }
