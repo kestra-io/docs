19b527886fd8a25707957d4166ba22dfd590d9cf
mageexcel <manish.excel2011@gmail.com>
2023-09-14 10:50:54 +0530

    fixed blueprints details page header

--- components/blueprints/Header.vue
+++ components/blueprints/Header.vue
@@ -1,46 +1,44 @@
 <template>
-<div>
-    <div class="container-fluid pt-5">
-        <div class="row justify-content-center">
-            <div class="col-10">
-                <p class="top-breadcrumb me-4" data-aos="fade-right">
-                    SOLUTIONS > BLUEPRINTS
-                </p>
-                <div class="row">
-                    <div class="col-10">
-                        <h2>Event-driven workflow based on a Snowflake query condition</h2>
-                    </div>
-                    <div class="col-2">
-                        <div><button class="btn btn-primary mb-2">Try this blueprint</button></div>
-                        <div><button class="btn bg-white">Copy source code</button></div>
-                    </div>
+<div class="main">
+    <div class="container-fluid d-flex justify-content-center pt-5">
+        <div class="header">
+            <p class="top-breadcrumb me-4" data-aos="fade-right">
+                SOLUTIONS > BLUEPRINTS
+            </p>
+            <div class="row">
+                <div class="col-12 col-md-9">
+                    <h2>Event-driven workflow based on a Snowflake query condition</h2>
                 </div>
-                <ul class="nav nav-tabs">
-                    <li class="nav-item" @click="setTab('topology')" role="button">
-                        <span class="nav-link" :class="{ 'active': activeTab == 'topology' }" aria-current="page">Topology</span>
-                    </li>
-                    <li class="nav-item" @click="setTab('source-code')" role="button">
-                        <span class="nav-link" :class="{ 'active': activeTab == 'source-code' }">Source Code</span>
-                    </li>
-                </ul>
-                <div v-if="activeTab == 'topology'" class="pt-3">
-                    <div class="card">
-                        <div class="card-body">
-                        <!-- topology graph -->
-                            graph
-                        </div>
-                    </div>
+                <div class="col-12 col-md-3">
+                    <div class="d-flex justify-content-md-end justify-content-start"><button class="btn btn-primary mb-2">Try this blueprint</button></div>
+                    <div class="d-flex justify-content-md-end justify-content-start"><button class="btn bg-white">Copy source code</button></div>
                 </div>
-                <div v-else class="pt-3">
-                    <div class="card">
-                        <div class="card-body">
-                        <!--  source code -->
-                            source code
-                        </div>
-                    </div>
-                </div>
-                <p class="text-center my-4">Plugins Used in this Blueprints</p>
             </div>
+            <ul class="nav nav-tabs">
+                <li class="nav-item" @click="setTab('topology')" role="button">
+                    <span class="nav-link" :class="{ 'active': activeTab == 'topology' }" aria-current="page">Topology</span>
+                </li>
+                <li class="nav-item" @click="setTab('source-code')" role="button">
+                    <span class="nav-link" :class="{ 'active': activeTab == 'source-code' }">Source Code</span>
+                </li>
+            </ul>
+            <div v-if="activeTab == 'topology'" class="pt-3">
+                <div class="card">
+                    <div class="card-body">
+                    <!-- topology graph -->
+                        graph
+                    </div>
+                </div>
+            </div>
+            <div v-else class="pt-3">
+                <div class="card">
+                    <div class="card-body">
+                    <!--  source code -->
+                        source code
+                    </div>
+                </div>
+            </div>
+            <p class="text-center my-4">Plugins Used in this Blueprints</p>
         </div>
     </div>
     <div class="plugins-icons d-flex justify-content-center gap-3">
@@ -93,6 +91,10 @@ export default {
     color: $primary !important;
     background-color: transparent !important;
     border-bottom-color: $primary;
+  }
+
+  .header {
+    width: 80%;
   }
 }
 .plugins-icons {
