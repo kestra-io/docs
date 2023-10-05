c7c23b02f429344a36dafc523a1f084978839c3f
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-10-05 22:17:08 +0200

    feat(site): add a top banner for announcement

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
         <NuxtLoadingIndicator />
-        <LayoutHeader />
-        <main>
-            <slot />
-        </main>
-        <LayoutFooter />
-        <LayoutFixed />
+        <LayoutAnnounce v-if="topBanner !== 'ok'" />
+        <div class="wrapper" :class="{'announce': topBanner !== 'ok'}">
+            <LayoutHeader />
+            <main >
+                <slot />
+            </main>
+            <LayoutFooter />
+            <LayoutFixed />
+        </div>
     </div>
 </template>
 
@@ -17,15 +20,25 @@
         setup() {
             const nuxtApp = useNuxtApp();
 
+            const topBanner = useCookie('top-banner', {watch: true});
+
             nuxtApp.hook("page:start", () => {
                 document.querySelector('body').classList.add("loading");
+
             });
 
             nuxtApp.hook("page:finish", () => {
                 document.querySelector('body').classList.remove("loading");
             });
 
-            return {}
+            return {topBanner}
         },
     })
 </script>
+
+
+<style lang="scss">
+    .wrapper.announce {
+        margin-top: 30px;
+    }
+</style>
