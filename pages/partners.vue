ed97ca0b9bab16d4e83c97225d4fa44b9fa886d8
MartinRst <mproset@kestra.io>
2023-11-02 17:30:52 +0100

    Update partners.vue

--- pages/partners.vue
+++ pages/partners.vue
@@ -1,10 +1,5 @@
 <template>
     <div>
-        <Head>
-            <Title>Kestra Partners Ecosystem</Title>
-            <Meta name="description" content="Whether you are building something new, upgrading a legacy stack, or re-platforming, you can trust Kestra and our partner ecosystem to make that journey a success." />
-        </Head>
-
         <PartnersHeaders />
         <Partners />
         <PartnersIntegratesWith />
