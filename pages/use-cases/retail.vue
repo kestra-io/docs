3acced5e050aeb27553bd584a7ca9246f055bef2
Ridge Kimani <ridgekimani@outlook.com>
2023-11-20 22:03:41 +0300

    feat: add retail page

--- pages/use-cases/retail.vue
+++ pages/use-cases/retail.vue
@@ -1,15 +1,67 @@
-<script lang="ts">
-import {defineComponent} from 'vue'
-
-export default defineComponent({
-name: "retail"
-})
-</script>
-
 <template>
-  $END$
+    <Head>
+        <Title>{{ header.title }}</Title>
+        <Meta name="description" :content="header.description" />
+    </Head>
+    <div class="main">
+        <div class="container">
+            <UseCaseCard :cta="header.cta" :description="header.description">
+                <template v-slot:header>
+                    <h1>
+                        Orchestrate Your <br />Retail Ecosystem <br />with
+                        Kestra
+                    </h1>
+                </template>
+            </UseCaseCard>
+            <SolutionsDetailFeatures
+                :title="solutions.title"
+                :items="solutions.items"
+            />
+            <UseCaseCard :cta="leroy.cta" :description="leroy.description">
+                <template v-slot:header>
+                    <h4>
+                        Leroy Merlin France enabling data <br />mesh
+                        architecture and 900% <br />
+                        increase in productivity with Kestra:<br />
+                    </h4>
+                </template>
+            </UseCaseCard>
+            <UseCasePlugins
+                :title="plugins.title"
+                :plugins="plugins.items"
+                :cta="plugins.cta"
+            />
+            <UseCaseRetailOps
+                :items="ops.items"
+                :description="ops.description"
+                :header="ops.title"
+            />
+            <UseCaseFooter />
+        </div>
+    </div>
 </template>
 
-<style scoped lang="scss">
+<script>
+import retail from "~/data/retail.js";
+import UseCaseCard from "~/components/use-cases/Card.vue";
+import UseCaseFooter from "~/components/use-cases/Footer.vue";
+import UseCasePlugins from "~/components/use-cases/Plugins.vue";
+import UseCaseRetailOps from "~/components/use-cases/RetailOps.vue";
 
-</style>
+export default {
+    name: "Retail",
+
+    components: {
+        UseCaseCard,
+        UseCaseFooter,
+        UseCasePlugins,
+        UseCaseRetailOps,
+    },
+
+    data: () => ({
+        ...retail,
+    }),
+};
+</script>
+
+<style scoped lang="scss"></style>
