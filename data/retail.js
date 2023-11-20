548cbdf2829ad6c8f8de9a5e99444ebb6802d86b
Ridge Kimani <ridgekimani@outlook.com>
2023-11-20 13:33:50 +0300

    feat: add use cases

---
+++ data/retail.js
@@ -0,0 +1,145 @@
+export default {
+    header: {
+        title: "Kestra for Retail and Consuming goods",
+        description:
+            "From data pipelines to complex integrations, Kestra empowers retail businesses with an event-driven, language-agnostic orchestration platform that scales as you grow",
+
+        cta: [
+            {
+                text: 'Book a demo'
+            },
+            {
+               text: 'Get started'
+            }
+        ]
+    },
+
+    solutions: {
+        title: "Use Cases in Retail",
+        items: [
+            {
+                title: "Automated Inventory Management",
+                text: "Eliminate manual tracking to use Kestra to automate your inventory management system.",
+                icon: "",
+            },
+            {
+                title: "Enhance Customer Experience",
+                text: "Use Kestra to collect, analyze, and use customer feedback. Automate the delivery of personalized promotions and recommendations based on customer data.",
+                icon: "",
+            },
+            {
+                title: "Compliance and Reporting",
+                text: "Automate the reporting processes, ensuring that all data compiles with retail standards and regulations. Ideal for generating automated end-of-day sales reports, inventory counts, and more.",
+                icon: "",
+            },
+            {
+                title: "Report Automation for Supply Chain",
+                text: "Automate supplier evaluation and order placement, ensuring a smooth and efficient supply chain operation.",
+                icon: "",
+            },
+            {
+                title: "Streamlined Data Management",
+                text: "Centralize your fragmented data workflow into a unified control plane, thereby simplifying orchestration and improving efficiency.",
+                icon: "",
+            },
+        ],
+    },
+    operations: {
+        title: "End-To-End Solution for Managing Complex Retail Operations",
+        description:
+            "Powers your business-critical retail applications, streamlines inventory, optimizes supply chain and much more. By breaking own organizational silos. Kestra fosters a collaborative environment for better operational efficiency.",
+        items: [
+            {
+                "title": "YAML-Based Worflows",
+                "text": "Build, maintain and scale, your workflows with Kestra's YAML-based interface.",
+                "icon": ""
+            },
+            {
+                "title": "Full CI/CD Integration",
+                "text": "From Terraform to GitHub Actions, Kestra provides complete control over deployment and resource management.",
+                "icon": ""
+            },
+            {
+                "title": "Language Agnostic",
+                "text": "Kestra's language-agnostic capabilities let you integrate various languages. Customize workflows to align with your unique retail business logic.",
+                "icon": ""
+            },
+            {
+                "title": "Event-Driven Orchestration",
+                "text": "",
+                "icon": ""
+            },
+            {
+                "title": "Developer Centric Platform",
+                "text": "",
+                "icon": ""
+            },
+            {
+                "title": "User-Friendly Interface",
+                "text": "",
+                "icon": ""
+            }
+        ]
+    },
+    plugins: {
+        title: "Connect your Entire Stack With Plugins",
+        icons: [
+            {
+                name: 'Fivetran',
+                icon: '',
+            },
+            {
+                name: 'Snowflake',
+                icon: '',
+            },
+            {
+                name: 'AWS',
+                icon: '',
+            },
+            {
+                name: 'databricks',
+                icon: '',
+            },
+            {
+                name: 'Azure',
+                icon: '',
+            },
+            {
+                name: 'dbt',
+                icon: '',
+            },
+            {
+                name: 'Airbyte',
+                icon: '',
+            },
+            {
+                name: 'Docker',
+                icon: '',
+            },
+            {
+                name: 'Terraform',
+                icon: '',
+            },
+            {
+                name: 'Google Cloud',
+                icon: '',
+            },
+            {
+                name: 'Github',
+                icon: '',
+            }
+        ],
+        cta: 'See all plugins'
+    },
+    getStarted: {
+        "title": "Getting Started",
+        "cta": [
+            {
+                text: "Read the docs"
+            },
+            {
+                text: "Get started"
+            }
+        ]
+    }
+};
