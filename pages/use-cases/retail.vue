a21f76bf61d48d2c65b8b84354a74c78afcc520c
Ridge Kimani <ridgekimani@outlook.com>
2023-11-24 22:13:08 +0300

    update retail

--- pages/use-cases/retail.vue
+++ pages/use-cases/retail.vue
@@ -5,38 +5,39 @@
     </Head>
     <div class="main">
         <RetailCard :content="header" />
-        <Line class='line-margin'/>
+        <Line class="line-margin" />
         <UseCasesExamples :items="solutions.items" />
-        <Line class='line-margin'/>
+        <Line class="line-margin" />
         <LeroyMerlin :content="leroy" />
-        <Line class='line-margin'/>
+        <Line class="line-margin" />
         <RetailOps :content="ops" />
         <Plugins :content="plugins" />
-        <Line class='line-margin'/>
+        <Line class="line-margin" />
         <Footer :content="footer" />
     </div>
 </template>
 
 <script>
 import retail from "~/data/retail.js";
-import RetailCard from "~/components/use-cases/Card.vue";
-import Footer from "~/components/use-cases/Footer.vue";
-import Plugins from "~/components/use-cases/Plugins.vue";
-import RetailOps from "~/components/use-cases/RetailOps.vue";
-import LeroyMerlin from "~/components/use-cases/LeroyMerlin.vue";
+
 import Examples from "~/components/use-cases/Examples.vue";
+import Footer from "~/components/use-cases/Footer.vue";
 import Line from "~/components/use-cases/Line.vue";
+import LeroyMerlin from "~/components/use-cases/LeroyMerlin.vue";
+import Plugins from "~/components/use-cases/Plugins.vue";
+import RetailCard from "~/components/use-cases/Card.vue";
+import RetailOps from "~/components/use-cases/RetailOps.vue";
 
 export default {
     name: "Retail",
 
     components: {
-        Line,
-        RetailCard,
         Examples,
         Footer,
         LeroyMerlin,
+        Line,
         Plugins,
+        RetailCard,
         RetailOps,
     },
 
