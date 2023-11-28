e6e0b7a40bb0cd12b92df2b6548e447dd5ce06bb
mageexcel <manish.excel2011@gmail.com>
2023-11-28 11:01:45 +0530

    Fixed Teraform UI responsiveness

--- components/terraform/UseCases.vue
+++ components/terraform/UseCases.vue
@@ -1,12 +1,12 @@
 <template>
-    <LayoutSection title="Benefits of the Integration" >
+    <LayoutSection title="Benefits of the Integration" class="container">
         <div class="row justify-content-center align-items-center">
             <div class="col-12 col-md-5" data-aos="fade-left">
                 <img src="/terraform/use-cases/reliable-deployments.svg" class="w-100" alt="reliable deployments">
             </div>
             <div class="col-12 col-md-5">
                 <div class="row justify-content-center">
-                    <div class="col-12 col-md-9" data-aos="fade-right">
+                    <div class="col-12 col-md-9 px-5 px-md-0" data-aos="fade-right">
                         <h3>Reliable Deployments with Infrastructure as Code</h3>
                         <p>
                             Combine Kestra's orchestration capabilities with Terraform's infrastructure-as-code model. Provision resources in minutes for consistent, reproducible, and scalable workflows. <br/>
@@ -17,7 +17,7 @@
             </div>
             <div class="col-12 col-md-5">
                 <div class="row justify-content-center">
-                    <div class="col-12 col-md-9" data-aos="fade-left">
+                    <div class="col-12 col-md-9 px-5 px-md-0" data-aos="fade-left">
                         <h3>Version Control and Rollback</h3>
                         <p>Incorporating version control systems such as Git is a recommended standard. It enables rollbacks and comprehensive auditing. Terraform additionally helps you version control not only your code but also your infrastructure configuration.</p>
                     </div>
