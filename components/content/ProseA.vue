186afc58d468c15a56ec19d22ea9ced4d87249a9
Taron Chatoyan <chatoyan48@gmailc.com>
2023-12-26 13:04:01 +0400

    added api call for get plugins

--- components/content/ProseA.vue
+++ components/content/ProseA.vue
@@ -30,31 +30,32 @@
             `ProseA-${hash(route.path)}`,
             () => queryContent(route.path).only("_file").findOne()
         );
+        if (page.value) {
+            const routePath = route.path.replace(/\/$/, '')
+            const absolutePath = config.public.siteUrl + routePath;
+            if (link.match(/(\.\.\/){2,}/) && page.value._file.includes('index.md')) {
+                link = link.replace('../', '')
+            } else if (link.match(/(\.\.\/){1}/) && page.value._file.includes('index.md')) {
+                link = link.replace('../', '')
+            }
 
-        const routePath = route.path.replace(/\/$/, '')
-        const absolutePath = config.public.siteUrl + routePath;
-        if (link.match(/(\.\.\/){2,}/) && page.value._file.includes('index.md')) {
-            link = link.replace('../', '')
-        } else if (link.match(/(\.\.\/){1}/) && page.value._file.includes('index.md')) {
-            link = link.replace('../', '')
+            if (link.startsWith('./') && !page.value._file.includes('index.md')) {
+                link = (new URL(link, absolutePath).toString());
+            } else if (link.startsWith('./')) {
+                link = absolutePath + link.replace('./', '/')
+            } else {
+                link = (new URL(link, absolutePath).toString())
+            }
+
+            if (link.endsWith('/')) {
+                link = link.replace(/\/$/, '')
+            }
+
+            link = link.replace(/(\/).\d+\./g,'')
+            link = link.replace(/\.md/g,'')
+
+            link = link.replace(config.public.siteUrl, '')
         }
-
-        if (link.startsWith('./') && !page.value._file.includes('index.md')) {
-            link = (new URL(link, absolutePath).toString());
-        } else if (link.startsWith('./')) {
-            link = absolutePath + link.replace('./', '/')
-        } else {
-            link = (new URL(link, absolutePath).toString())
-        }
-
-        if (link.endsWith('/')) {
-            link = link.replace(/\/$/, '')
-        }
-
-        link = link.replace(/(\/).\d+\./g,'')
-        link = link.replace(/\.md/g,'')
-
-        link = link.replace(config.public.siteUrl, '')
     }
 
 </script>
