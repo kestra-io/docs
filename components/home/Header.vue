0ea392bddd8f75c6d399121f8a5202a8ec4ac16d
Ridge Kimani <ridgekimani@outlook.com>
2023-12-30 13:42:11 +0300

    feat: update title colors

--- components/home/Header.vue
+++ components/home/Header.vue
@@ -2,14 +2,27 @@
     <div class="main">
         <div class="hero container">
             <div class="text-block">
-                <h1 data-aos="fade-right"><text-scroller :texts="scrollingTexts" /><br />Everything Everywhere <br />All at Once.</h1>
+                <h1 data-aos="fade-right">
+                    <text-scroller :texts="scrollingTexts" /><br />Everything
+                    Everywhere <br />All at Once.
+                </h1>
 
                 <div class="buttons">
-                    <a href="#" class="btn btn-animated btn-dark-animated me-2 mb-2" data-aos="zoom-in" data-bs-toggle="modal" data-bs-target="#home-intro">
+                    <a
+                        href="#"
+                        class="btn btn-animated btn-dark-animated me-2 mb-2"
+                        data-aos="zoom-in"
+                        data-bs-toggle="modal"
+                        data-bs-target="#home-intro"
+                    >
                         <PlayCircleOutline /> Product Video
                     </a>
 
-                    <NuxtLink href="/docs/getting-started" class="btn btn-animated btn-purple-animated mb-2" data-aos="zoom-in">
+                    <NuxtLink
+                        href="/docs/getting-started"
+                        class="btn btn-animated btn-purple-animated mb-2"
+                        data-aos="zoom-in"
+                    >
                         <LightningBolt /> Get started
                     </NuxtLink>
                 </div>
@@ -18,12 +31,31 @@
 
         <Companies class="mt-4 pb-4 companies" />
     </div>
-    <div v-on="{ 'show.bs.modal': () => videoVisible = true, 'hidden.bs.modal': () => videoVisible = false }" class="modal modal-full fade" id="home-intro" tabindex="-1" aria-labelledby="home-intro" aria-hidden="true">
+    <div
+        v-on="{
+            'show.bs.modal': () => (videoVisible = true),
+            'hidden.bs.modal': () => (videoVisible = false),
+        }"
+        class="modal modal-full fade"
+        id="home-intro"
+        tabindex="-1"
+        aria-labelledby="home-intro"
+        aria-hidden="true"
+    >
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-body">
                     <div class="video-responsive">
-                        <iframe v-if="videoVisible" width="560" height="315" src="https://www.youtube.com/embed/h-P0eK2xN58?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        <iframe
+                            v-if="videoVisible"
+                            width="560"
+                            height="315"
+                            src="https://www.youtube.com/embed/h-P0eK2xN58?autoplay=1"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen
+                        ></iframe>
                     </div>
                 </div>
             </div>
@@ -32,92 +64,103 @@
 </template>
 
 <script>
-    import Console from "vue-material-design-icons/Console.vue"
-    import PlayOutline from "vue-material-design-icons/PlayOutline.vue"
-    import LightningBolt from "vue-material-design-icons/LightningBolt.vue"
-    import Companies from "../layout/Companies.vue";
-    import PlayCircleOutline from 'vue-material-design-icons/PlayCircleOutline.vue'
-    import TextScroller from "~/components/layout/TextScroller.vue";
+import Console from "vue-material-design-icons/Console.vue";
+import PlayOutline from "vue-material-design-icons/PlayOutline.vue";
+import LightningBolt from "vue-material-design-icons/LightningBolt.vue";
+import Companies from "../layout/Companies.vue";
+import PlayCircleOutline from "vue-material-design-icons/PlayCircleOutline.vue";
+import TextScroller from "~/components/layout/TextScroller.vue";
 
-    export default {
-        components: {Console, PlayOutline, LightningBolt, PlayCircleOutline, Companies, TextScroller},
-        data() {
-            return {
-                videoVisible: false,
-                scrollingTexts: [
-                    {text: "Orchestrate", color: "#C005EF"},
-                    {text: "Automate", color: "#5D48F5"},
-                    {text: "Schedule", color: "#7604E3"}
-                ]
-            }
-        }
+export default {
+    components: {
+        Console,
+        PlayOutline,
+        LightningBolt,
+        PlayCircleOutline,
+        Companies,
+        TextScroller,
+    },
+    data() {
+        return {
+            videoVisible: false,
+            componentIsMounted: false,
+            scrollingTexts: [
+                { text: "Orchestrate", color: "#E500EA" },
+                { text: "Automate", color: "#4281FF" },
+                { text: "Simplify", color: "#9D40FB" },
+            ],
+        };
+    },
+    mounted() {
+        this.componentIsMounted = true
     }
+};
 </script>
 
 <style lang="scss" scoped>
-    @import "../../assets/styles/variable";
+@import "../../assets/styles/variable";
 
-    .main {
-        background: #111113 url("/landing/home/header-bg.png") no-repeat center center;
+.main {
+    background: #111113 url("/landing/home/header-bg.png") no-repeat center
+        center;
 
-        .text-block {
-            margin: 5rem 0;
+    .text-block {
+        margin: 5rem 0;
+    }
+
+    h1 {
+        color: var(--bs-white);
+        text-align: center;
+        max-width: 100%;
+        font-size: 36px;
+        font-weight: 600;
+
+        @include media-breakpoint-up(md) {
+            font-size: 56px;
         }
 
-        h1 {
-            color: var(--bs-white);
-            text-align: center;
-            max-width: 100%;
-            font-size: 36px;
-            font-weight: 600;
-
-
-            @include media-breakpoint-up(md) {
-                font-size: 56px;
-            }
-
-            @include media-breakpoint-up(lg) {
-                font-size: 76px;
-            }
-
-            :deep(span) {
-                color: #E500EA;
-            }
+        @include media-breakpoint-up(lg) {
+            font-size: 76px;
         }
 
-        .buttons {
-            text-align: center;
+        :deep(span) {
+            color: #e500ea;
         }
+    }
 
-        .companies {
-            border-top: 1px solid rgba(255, 255, 255, 0.10);
+    .buttons {
+        text-align: center;
+    }
+
+    .companies {
+        border-top: 1px solid rgba(255, 255, 255, 0.1);
+    }
+
+    .companies-title {
+        position: relative;
+
+        &:after {
+            content: "";
+            position: absolute;
+            left: 50%;
+            bottom: calc(-1 * var(--spacer));
+            transform: translateX(-50%);
+            display: inline-block;
+            height: 2px;
+            width: 51px;
+            background: var(--bs-pink);
         }
+    }
 
-        .companies-title {
-            position: relative;
+    @include media-breakpoint-down(lg) {
+        .hero {
+            padding-top: 6rem;
+            padding-bottom: 2rem;
 
-            &:after {
-                content: '';
-                position: absolute;
-                left: 50%;
-                bottom: calc(-1 * var(--spacer));
-                transform: translateX(-50%);
-                display: inline-block;
-                height: 2px;
-                width: 51px;
-                background: var(--bs-pink);
-            }
-        }
-
-        @include media-breakpoint-down(lg) {
-            .hero {
-                padding-top: 6rem;
-                padding-bottom: 2rem;
-
-                .text-block {
-                    margin-bottom: 0;
-                }
+            .text-block {
+                margin-bottom: 0;
             }
         }
     }
+}
 </style>
