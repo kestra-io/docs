fc4cea99f358c04b4c6820960dc3c9a47cacdbd2
Manish <49708265+mageexcel@users.noreply.github.com>
2024-01-04 15:50:19 +0530

    fixed image overlapping issue (#748)
    * fixed image overlapping issue
    
    * fix teraform
    
    * fixed-terraform-enterprice

--- components/careers/Header.vue
+++ components/careers/Header.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="hero hero-sm container">
             <div class="row">
-                <div class="col-md-6 align-items-center d-flex">
+                <div class="col-md-6 align-items-center d-flex order-1 order-md-0">
                     <div>
                         <p data-aos="fade-left" class="overline">Careers</p>
                         <h1 data-aos="fade-right">Join our Team</h1>
@@ -12,7 +12,7 @@
                         </NuxtLink>
                     </div>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-6 order-0 order-md-1">
                     <img class="img-fluid" src="/landing/careers/header.svg" alt="A square with circles inside with emojis in It representing Kestra's inclusive and dynamic workplace culture" data-aos="zoom-in" />
                 </div>
             </div>
