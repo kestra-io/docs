0e661fc4b6b3e5dc5661747d2acc831ac8b58cf2
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-01-06 23:32:28 +0100

    feat(site): update posthog tracker

--- plugins/cookieconsent.client.ts
+++ plugins/cookieconsent.client.ts
@@ -1,16 +1,31 @@
+// noinspection TypeScriptValidateTypes
+
 import 'vanilla-cookieconsent'
 
 export default defineNuxtPlugin(nuxtApp => {
+    let isProd = process.env.NODE_ENV === "production";
+
+    !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+    posthog.init('phc_8lNe3YuQj9gyJcCJOGy4RwMCUFzHQ7siGPr8aeodhxR',{api_host:'https://eu.posthog.com'})
 
     nuxtApp.hook('page:finish', () => {
         const {grantConsent} = useGtag()
+
+        const posthogLoad = () => {
+            posthog.capture('$pageview');
+        };
+
+        const enabledAnalytics = () => {
+            grantConsent();
+            posthogLoad();
+        };
 
         const isEurope = Intl.DateTimeFormat().resolvedOptions().timeZone.indexOf("Europe") === 0;
 
         const cookieConsent = window.initCookieConsent()
 
-        if (!isEurope) {
-            grantConsent();
+        if (!isEurope && isProd) {
+            enabledAnalytics();
             cookieConsent.loadScript('https://js-eu1.hs-scripts.com/27220195.js');
 
             return;
@@ -29,8 +44,11 @@ export default defineNuxtPlugin(nuxtApp => {
             page_scripts: true,
             force_consent: true,
             onAccept: () => {
-                if(cookieConsent.allowedCategory('analytics')) {
-                    grantConsent();
+                if (isProd) {
+                    if (cookieConsent.allowedCategory('analytics')) {
+                        enabledAnalytics();
+                    }
+
                     if (cookieConsent.allowedCategory('marketing')) {
                         cookieConsent.loadScript('https://js-eu1.hs-scripts.com/27220195.js');
                     }
