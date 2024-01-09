9672c2be29963826e639a7ad838d3f1a5b4fe74c
brian.mulier <bmmulier@hotmail.fr>
2024-01-09 20:03:33 +0100

    fix(github-api): no more 403 upon fetching Github from server-side due to no User-Agent (Cloudflare issue)

--- server/api/github.js
+++ server/api/github.js
@@ -1,29 +1,29 @@
 import {useDataCache} from '#nuxt-multi-cache/composables'
 
-export default defineEventHandler(async(event) => {
+export default defineEventHandler(async (event) => {
     const {value, addToCache} = await useDataCache('github-metrics', event);
 
     if (value) {
         return value
     }
 
-    let result = await fetch("https://api.github.com/repos/kestra-io/kestra")
-        .then((response) => {
-            return response.json();
-        })
-        .then(value => {
-            return {
-                'stargazers': value.stargazers_count,
-                'watchers': value.watchers_count,
-                'issues': value.open_issues_count,
-                'forks': value.forks,
-                'network': value.network_count,
-                'subscribers': value.subscribers_count,
-                'size': value.size,
-            };
-        });
+    const headers = {'User-Agent': 'request'};
+    const result = await $fetch(
+        "https://api.github.com/repos/kestra-io/kestra",
+        {headers}
+    ).then(value => {
+        return {
+            'stargazers': value.stargazers_count,
+            'watchers': value.watchers_count,
+            'issues': value.open_issues_count,
+            'forks': value.forks,
+            'network': value.network_count,
+            'subscribers': value.subscribers_count,
+            'size': value.size,
+        };
+    });
 
     await addToCache(result, [], 3600);
 
-    return result
+    return result;
 })
