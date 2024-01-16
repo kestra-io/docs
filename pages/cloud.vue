3bd2f1a50870966b2a817b0385585724360fcbb7
Manish <49708265+mageexcel@users.noreply.github.com>
2024-01-17 00:29:52 +0530

    cloudpage (#701)

---
+++ pages/cloud.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+         <Head>
+            <Title>Register to Get Access to Kestra Cloud</Title>
+            <Meta name="description"
+                  content="Sign up to get notified as soon as Kestra Cloud launches"/>
+        </Head>
+
+        <CloudHeader />
+    </div>
+</template>
+
+
