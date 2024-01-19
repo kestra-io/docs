2754457e6b1f19945bf240daa38e373a05b5db39
Manish <49708265+mageexcel@users.noreply.github.com>
2024-01-19 18:14:19 +0530

    Price page (#794)
    * added-price-page
    
    * fixed-UI-changes
    
    * fixed-gloom-bordercolor
    
    * fixed-titles
    
    * fixed/roller-addedPricing

---
+++ pages/pricing/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="cont">
+        <Head>
+            <Title>Compare all Versions of Kestra</Title>
+            <Meta name="description"
+                content="Choose the right plan for your needs. Start with the Open Source version and scale with the Enterprise Edition of Kestra." />
+        </Head>
+        <Heading />
+      
+        <Companies />
+      
+        <Table />
+        <Faq />
+        <Footer />
+    </div>
+</template>
+
+<script setup>
+import Heading from "../../components/price/Heading.vue"
+import Companies from "../../components/price/Companies.vue"
+import Footer from "../../components/price/Footer.vue"
+import Table from "../../components/price/Table.vue"
+import Faq from "../../components/price/Faq.vue"
+
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/styles/variable";
+.cont{
+   background: $black-4 ;
+}
+</style>
