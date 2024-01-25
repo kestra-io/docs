cbaf534c270115f84ecf2e8d85bbdd550e85c26b
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-01-25 09:23:33 +0100

    fix(docs): ChildTableOfContents don't work anymore with navigation config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,9 +66,6 @@ export default defineNuxtConfig({
             // ],
             theme: 'github-dark'
         },
-        navigation: {
-            fields: ["_file", "_id"]
-        },
         markdown: {
             remarkPlugins: {
                 'remark-flexible-markers': {
