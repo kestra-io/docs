d108bb7abccc1cd17aab87883f13786b927843ad
Anna Geller <anna.m.geller@gmail.com>
2024-01-28 15:06:37 +0100

    fix broken maven link + tf redirect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -136,6 +136,7 @@ export default defineNuxtConfig({
 
     routeRules: {
         '/slack': {redirect: `${DEFAULT_KESTRA_API_URL}/communities/slack/redirect`},
+        '/docs/terraform': {redirect: '/docs/getting-started/terraform'},
         '/api/events/**': {proxy: 'https://eu.posthog.com/**'},
     },
 
