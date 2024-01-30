76ddd9c4627a54a113334def62a3350fe91a986d
Taron Chatoyan <47221390+t-chatoyan@users.noreply.github.com>
2024-01-30 00:38:57 -0800

    feat(plugins-blueprints-stories): dark mode design (#824)

--- components/stories/Card.vue
+++ components/stories/Card.vue
@@ -1,6 +1,6 @@
 <template>
     <NuxtLink :href="`/use-cases/stories/${story.id}-${slugify(story.title)}`">
-        <div class="card" data-aos="fade-right">
+        <div class="card bg-dark-2" data-aos="fade-right">
             <div class="card-body d-flex flex-column justify-content-between ">
                 <div>
                     <NuxtImg
@@ -10,17 +10,17 @@
                         densities="x1 x2"
                         :src="story.featuredImage"
                         :alt="`${story.title} image`"
-                        class="card-img-top rounded-3"
+                        class="card-img-top"
                     />
-                    <p class="card-title mt-3 mb-2 text-pink">{{ story.title }}</p>
+                    <p class="card-title mt-3 mb-2">{{ story.title }}</p>
                     <p class="card-meta-description">{{ story.description }}</p>
                     <div class="d-flex flex-wrap gap-3 my-3">
-                        <div class="icon" v-for="task in story.tasks" :key="task">
+                        <div class="icon bg-dark-4" v-for="task in story.tasks" :key="task">
                             <CommonTaskIcon :cls="task" />
                         </div>
                     </div>
                 </div>
-                <span class="author text-primary">
+                <span class="author">
                       Read the Story <img src="/stories/icons/arrow_right_alt.svg" alt="right icons" />
                   </span>
             </div>
@@ -44,40 +44,47 @@
 
     .card {
         height: 100%;
+        border-radius: 8px;
+        border: $block-border;
 
         .card-body {
-            .type {
-                font-size: $font-size-sm;
-                color: $primary;
+            padding: $rem-2;
+
+            .card-img-top {
+                border-radius: 4px;
+                border: 1px solid #404559;
             }
 
             .author {
                 font-size: $font-size-sm;
-                color: $gray-500;
+                color: $purple-35;
                 margin-bottom: 0;
+                font-weight: 400;
             }
 
             .icon {
-                border: 1px solid $white-2;
+                border-radius: 4px;
+                border: 1px solid $black-6;
                 padding: 0.313rem 0.625rem;
                 width: 44px;
             }
         }
 
         &-title {
+            color: #C6C1D9;
+            font-family: $font-family-monospace;
             font-size: $font-size-xs;
-            line-height: 1.625rem;
-        }
-
-        &-name {
-            font-size: $font-size-xs !important;
-            line-height: 1.75rem !important;
-            text-transform: uppercase !important;
+            font-weight: 700;
+            line-height: calc($spacer * 1.75);
+            text-transform: uppercase;
         }
 
         &-meta-description {
-            font-weight: 700;
+            color: $white;
+            font-family: $font-family-sans-serif;
             font-size: $h6-font-size;
+            font-weight: 700;
+            line-height: calc($spacer * 1.625);
         }
     }
 </style>
