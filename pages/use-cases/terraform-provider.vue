d6bb7dd9a5545162c018abc8f4c3151f03404659
Taron Chatoyan <47221390+t-chatoyan@users.noreply.github.com>
2024-01-30 00:39:50 -0800

    feat(terraform): dark mode design (#822)

--- pages/use-cases/terraform-provider.vue
+++ pages/use-cases/terraform-provider.vue
@@ -3,7 +3,7 @@
           <Title>Kestra Terraform Provider: Deploy All Your Workflows in a CI/CD Process</Title>
           <Meta name="description" content="Manage and deploy Kestra workflows directly within your existing Terraform environment, eliminating the need for separate configurations." />
         </Head>
-    <div class="main">
+    <div class="main bg-dark-4">
             <TerraformHeader/>
             <TerraformOverview />
             <TerraformUseCases />
@@ -34,7 +34,6 @@
 @import "../../assets/styles/variable";
 
 .main {
-    background-color: $purple-34;
-    color: white;
+    color: $white;
 }
 </style>
