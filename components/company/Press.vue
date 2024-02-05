12283e941c92c9cad2e62be55db2a4e8efb9fe8e
Taron Chatoyan <47221390+t-chatoyan@users.noreply.github.com>
2024-02-05 07:50:56 -0800

    feat(about-us): dark design (#842)

--- components/company/Press.vue
+++ components/company/Press.vue
@@ -4,16 +4,14 @@
             <div class="row mb-5">
                 <div class="col-md-6 p-5">
                     <Section
-                        title="Press Kit"
-                        subtitle="Brand Assets"
+                        title="Brand Assets"
                     >
-                        <p class="mb-5" data-aos="fade-right">
+                        <p class="mb-3" data-aos="fade-right">
                             For Media Inquiries and Exclusive Insights, Reach Out to Our Press Team at <a href="mailto:press@kestra.io">press@kestra.io</a>
                         </p>
-                        <a href="https://drive.google.com/drive/folders/1nwf1BeIZqAGC9uPiq33W-nvDwXDK5tPR?usp=sharing" target="_blank" class="btn btn-primary me-2" data-aos="zoom-in">
+                        <a href="https://drive.google.com/drive/folders/1nwf1BeIZqAGC9uPiq33W-nvDwXDK5tPR?usp=sharing" target="_blank" class="btn btn-animated btn-purple-animated me-2" data-aos="zoom-in">
                             Download assets
                         </a>
-
                     </Section>
                 </div>
             </div>
@@ -32,22 +30,43 @@
     @import "../../assets/styles/variable";
 
     .container-fluid {
-        background: url("/landing/company/presskit-logo.svg") no-repeat center center $purple-29;
+        background: url("/landing/company/presskit-logo.svg") no-repeat center center;
         background-size: cover;
+        margin-bottom: 5.625rem;
     }
 
     .container {
         padding-top: 0;
         padding-bottom: 0;
         margin-bottom: calc($spacer * -3);
+        border-bottom: $block-border;
+        border-top: $block-border;
 
         .col-md-6 {
             vertical-align: middle;
         }
 
         :deep(section) {
-            .subtitle, h2 {
+            h2 {
+                color: $white;
+                font-size: $h2-font-size;
+                font-weight: 300;
                 text-align: left;
+                margin-top: 3rem;
+            }
+
+            .main {
+                padding-top: 1.354rem;
+
+                p {
+                    color: $white;
+                    font-size: $h6-font-size;
+                    font-weight: 400;
+
+                    a {
+                        color: $purple-35;
+                    }
+                }
             }
         }
     }
