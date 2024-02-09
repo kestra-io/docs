c8afaf85be055e16a4b9cdb242ca8130d7d5aecf
Manish <49708265+mageexcel@users.noreply.github.com>
2024-02-09 18:11:34 +0530

    feat_test_1

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,6 @@
             <Meta name="description"
                   content="Use declarative language to build simpler, faster, scalable and flexible data pipelines"/>
         </Head>
-
         <HomeHeader/>
         <HomeFeatures/>
         <HomeUI/>
