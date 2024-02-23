6a030053e6ecb671b8dce5b87dc57e38fae94a46
Anna Geller <anna.m.geller@gmail.com>
2024-02-23 15:29:28 +0100

    Update Features.vue

--- components/home/Features.vue
+++ components/home/Features.vue
@@ -6,7 +6,6 @@
                     <Card
                         :icon="CalculatorVariant"
                         title="Embedded Visual Studio Code editor IDE"
-                        description="Decide, based on your use case, how declarative or imperative you want your workflows to be. Write your business logic in any language from the integrated Visual Studio Code editor IDE, and orchestrate it declaratively in minutes."
                     />
                 </div>
 
@@ -14,7 +13,6 @@
                     <Card
                         :icon="Lan"
                         title="Scalable event-driven multitenant architecture"
-                        description="Kestra's architecture is built on top of proven and time-tested technologies, including Postgres, Kafka and Elasticsearch, to enable scheduled and event-driven workflow patterns at any scale with tenant-level isolation."
                     />
                 </div>
 
@@ -22,18 +20,14 @@
                     <Card
                         :icon="Hubspot"
                         title="Run anywhere, code in any language"
-                        description="Integrate with any cloud, programming language, source system, database or data tool, and build your own plugins and blueprints to eliminate boilerplate code and automate repetitive tasks."
                     />
                 </div>
 
                 <div class="col-md-4 mb-4">
                     <Card
                         :icon="LightningBolt"
-                        title="Fast development cycles, simple deployment patterns"
+                        title="Fast development cycles, simple deployments"
                     >
-                        <template v-slot:descriptionHtml>
-                            Create flows in a simple <NuxtLink href="/features/declarative-data-orchestration">declarative YAML config</NuxtLink> directly from the UI with autocompletion, syntax validation and built-in documentation. Or use our official VS Code extension to integrate your workflows with Git and CI/CD systems.
-                        </template>
                     </Card>
                 </div>
 
@@ -41,7 +35,6 @@
                     <Card
                         :icon="RefreshAuto"
                         title="API-first design with a rich plugin ecosystem"
-                        description="Because every action in Kestra is API-driven, there are no limits to what you can automate, from changing flow and task configuration using internal tools to triggering, adding, or modifying workflows in any way you can imagine."
                     />
                 </div>
 
@@ -49,7 +42,6 @@
                     <Card
                         :icon="ImageFilterDrama"
                         title="Deploy anywhere with no single point of failure"
-                        description="Scale your data platform from a few executions to millions without hassle. You can deploy Kestra on-prem and cloud using Docker, our official Helm chart, Terraform provider, or a manual installation using a single binary file."
                     />
                 </div>
             </div>
