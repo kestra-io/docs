7a66bb4e288b6a4c1401ba841672fd9e23a44eca
Taron Chatoyan <chatoyan48@gmailc.com>
2024-03-11 15:56:50 +0400

    fixed meta description and title

--- pages/features/api-first.vue
+++ pages/features/api-first.vue
@@ -25,10 +25,10 @@
     meta: [
       { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'twitter:site', content: '@kestra_io' },
-      { name: 'twitter:title', content: "Code in Any language with Kestra the Language Agnostic Platform" },
+      { name: 'twitter:title', content: "Kestra API First Design" },
       {
         name: 'twitter:description',
-        content: "Separate your business logic from your orchestration logic with a versatile set of language-agnostic developer tools"
+        content: "interact with every aspect of the Kestra programmatically."
       },
       { name: 'twitter:image', content: `${origin}/landing/features/declarative/header-bg.svg` },
       { name: 'twitter:image:alt', content: "Declarative Data Orchestration with Kestra" }
