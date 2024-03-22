113bb878f4e5656684773238b9f7dfe6c4f04b7b
Taron Chatoyan <chatoyan48@gmailc.com>
2024-03-22 15:34:26 +0400

    Added a frontmatter element allowing to mark a feature as available only on specific kestra editions/plans and optionally only on specific version

---
+++ components/layout/FeatureScopeMarker.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="audit-logs mb-3">
+        <p class="title">Available on:</p>
+        <div class="editions">
+            <div class="edition-item" v-for="edition of editions" :style="`background-color: ${edition.color}`">
+                <p>
+                    {{edition.title}}
+                </p>
+            </div>
+        </div>
+        <div class="version" :style="`background-color: ${versionColor}`">
+            <p>
+                {{ version }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Section from '../layout/Section.vue';
+
+    export default {
+        components: {Section},
+        props: {
+          editions: {
+                type: Array,
+                required: true,
+          },
+          version: {
+                type: String,
+                required: true
+          },
+          versionColor: {
+            type: String,
+            required: true
+          }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "../../assets/styles/variable";
+
+    .audit-logs {
+        display: flex;
+        padding: calc($spacer * 0.5);
+        gap: 0 calc($spacer * 0.5);
+        align-items: center;
+        border: $block-border;
+        width: fit-content;
+
+        .title {
+            color: $white;
+            font-weight: 500;
+            font-size: $font-size-xl;
+            margin: 0;
+        }
+
+        .editions {
+            display: flex;
+            justify-content: center;
+            gap: calc($spacer * 0.5);
+
+            .edition-item {
+                border-radius: calc($spacer / 2);
+                padding: calc($spacer / 4) calc($spacer / 2);
+
+                p {
+                    color: $white-2;
+                    font-size: $font-size-base;
+                    font-weight: 400;
+                    margin: 0;
+                }
+            }
+        }
+
+        .version {
+            border-radius: calc($spacer / 2);
+            padding: calc($spacer / 4) calc($spacer / 2);
+            p  {
+                margin: 0;
+            }
+        }
+    }
+</style>
