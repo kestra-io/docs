21eabf7c7d46c7e92dbd05e4fae95dc4f9248b03
Taron Chatoyan <chatoyan48@gmailc.com>
2024-03-27 16:50:43 +0400

    upgraded cloud page

---
+++ components/cloud/Features.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="container-fluid bg-dark-4">
+        <div class="container">
+            <div class="cloud-cards">
+                <Card
+                    :icon="RocketLaunch"
+                    title="Get started  In minutes"
+                />
+                <Card
+                    :icon="CreditCardOff"
+                    title="Sign up for free No credit card required"
+                />
+                <Card
+                    :icon="CloudCheck"
+                    title="Fully-managed Infrastructure"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+  import RocketLaunch from "vue-material-design-icons/RocketLaunch.vue"
+  import CreditCardOff from "vue-material-design-icons/CreditCardOff.vue"
+  import CloudCheck from "vue-material-design-icons/CloudCheck.vue"
+</script>
+
+<style lang="scss" scoped>
+    @import "../../assets/styles/variable";
+
+    .container-fluid {
+        position: relative;
+        &::before {
+            content: "";
+            position: absolute;
+            width: calc($spacer * 11);
+            height: calc($spacer * 11);
+            background: linear-gradient(180deg, rgba(98, 24, 255, 0) 0%, #6117FF 100%);
+            filter: blur(82px);
+            z-index: 5;
+            left: 50%;
+            top: -5rem;
+
+            @include media-breakpoint-down(lg) {
+                left: 35%;
+                top: 50%;
+            }
+        }
+    }
+    .cloud-cards {
+        padding: calc($spacer * 2) 0 calc($spacer * 5.6);
+        display: flex;
+        justify-content: center;
+        gap: calc($spacer * 1.8);
+        position: relative;
+        z-index: 6;
+
+        :deep(.title-block) {
+            align-items: center;
+
+            .card-title {
+                max-width: 60%;
+            }
+        }
+
+        @include media-breakpoint-down(lg) {
+            flex-direction: column;
+            align-items: center;
+
+            :deep(.card) {
+                height: unset;
+                width: 60%;
+            }
+        }
+
+        @include media-breakpoint-down(md) {
+            :deep(.card) {
+                width: auto;
+                min-width: 100%;
+            }
+        }
+    }
+</style>
--- components/cloud/Header.vue
+++ components/cloud/Header.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="container-fluid bg-dark-4">
-        <div class="pt-5 d-flex flex-column justify-content-center">
-            <div class="text-center header text-white mx-auto mt-4">
-                <h1 class="fw-light">
-                    Kestra <span class="text-gradient">Cloud</span> Comming Soon
-                </h1>
-                <h6 class="pb-4 fw-light lh-lg">
-                    Register now to be the first to know when we launch!
-                </h6>
-            </div>
+        <div class="container py-5 my-3">
+            <div class="hero-container py-5 d-flex justify-content-center">
+                <div class="header text-white">
+                    <img src="/landing/cloud/cloud-hero.svg" alt="Kestra Cloud Comming Soon">
+                    <h1 class="fw-light my-3">
+                        Kestra <span class="text-gradient">Cloud</span> Comming Soon
+                    </h1>
+                    <h6 class="fw-light lh-lg m-0">
+                        Register now to be the first to know when we launch!
+                    </h6>
+                </div>
 
-            <div class="container">
-                <div class="col-12 col-md-8 col-lg-5 form-container mx-auto pt-3 pb-5">
-                    <div id="hubspotForm" data-aos="zoom-in" />
+                <div class="col-12 col-md-8 col-lg-5 form-container mx-auto pt-3">
+                    <div id="hubspotForm" data-aos="zoom-in"/>
                 </div>
             </div>
         </div>
@@ -20,113 +21,118 @@
 </template>
 
 <script>
-    export default {
-        mounted() {
-            if (process.client) {
-                if (window.hbspt) {
-                    window.hbspt.forms.create({
-                        region: "eu1",
-                        portalId: "27220195",
-                        formId: "fbce3efa-1f99-4ab9-928c-26167aa51424",
-                        target: "#hubspotForm",
-                        onFormSubmit: function($form) {
-                            if (window.dataLayer) {
-                                window.dataLayer.push({'event': 'cloud_form'});
-                            }
-                        },
-                    })
-                }
-            }
-        },
-    }
+  export default {
+    mounted() {
+      if (process.client) {
+        if (window.hbspt) {
+          window.hbspt.forms.create({
+            region: "eu1",
+            portalId: "27220195",
+            formId: "fbce3efa-1f99-4ab9-928c-26167aa51424",
+            target: "#hubspotForm",
+            onFormSubmit: function ($form) {
+              if (window.dataLayer) {
+                window.dataLayer.push({'event': 'cloud_form'});
+              }
+            },
+          })
+        }
+      }
+    },
+  }
 </script>
 
 <style lang="scss" scoped>
-@import "../../assets/styles/variable";
+    @import "../../assets/styles/variable";
 
-.form-group.required .form-label::after {
-    content: "*";
-    color: red;
-}
-
-.custom-control-input {
-    background-color: $black-4;
-}
-
-.container-fluid {
-    h1{
-        font-size:$font-size-4xl !important;
-        line-height: $font-size-4xl
-    }
-    h6{
-        font-size: $font-size-xl;
+    .form-group.required .form-label::after {
+        content: "*";
+        color: red;
     }
 
-    .text-gradient {
-        background: linear-gradient(90.03deg, #e151f7 2.16%, #5c47f5 65.09%);
-        -webkit-text-fill-color: transparent;
-        background-clip: text;
-    }
-
-    .input {
+    .custom-control-input {
         background-color: $black-4;
-        border: 1px;
-        border-style: $black-3;
-        border: none;
     }
 
-    .para {
-        color: $white-3;
-    }
-
-    .desc {
-        color: $white-3;
-        line-height: 1.375rem;
-        font-size: $font-size-sm
-    }
-
-    .container {
-        background-image: url("/landing/contact/Mask.svg");
-        z-index: 20;
-    }
-
-    .form-container {
-        z-index: 10;
-        background-color: $black-2;
-        border-style: solid;
-        border-color: $black-3;
-        padding: 1rem;
-        padding-bottom: 0rem;
-        margin-bottom: 3rem;
-
+    .container-fluid {
+        position: relative;
         &::after {
             content: "";
             position: absolute;
             width: 262px;
             height: 170px;
             background: linear-gradient(180deg,
-                    rgba(98, 24, 255, 0) 0%,
-                    #6117ff 100%);
+                rgba(98, 24, 255, 0) 0%,
+                #6117ff 100%);
             filter: blur(100px);
-            left: 0px;
-            top: 0px;
-            z-index: -1;
+            left: 15%;
+            top: 10%;
+            z-index: 1;
         }
 
-        &::before {
-            content: "";
-            position: absolute;
-            width: 262px;
-            height: 170px;
-            background: linear-gradient(180deg,
-                    rgba(98, 24, 255, 0) 0%,
-                    #6117ff 100%);
+        .hero-container {
+            h1 {
+                font-size: $font-size-4xl !important;
+                line-height: $font-size-4xl;
+                max-width: 80%;
+            }
 
-            filter: blur(100px);
-            right: 0px;
-            bottom: 0px;
-            z-index: -1;
+            h6 {
+                font-size: $font-size-xl;
+            }
+
+            .text-gradient {
+                background: linear-gradient(90.03deg, #e151f7 2.16%, #5c47f5 65.09%);
+                -webkit-text-fill-color: transparent;
+                background-clip: text;
+            }
+
+            @include media-breakpoint-down(lg) {
+                flex-direction: column;
+                gap: calc($spacer * 2);
+
+                h1 {
+                    max-width: unset;
+                }
+            }
         }
+
+
+        .input {
+            background-color: $black-4;
+            border: 1px;
+            border-style: $black-3;
+            border: none;
+        }
+
+        .para {
+            color: $white-3;
+        }
+
+        .desc {
+            color: $white-3;
+            line-height: 1.375rem;
+            font-size: $font-size-sm
+        }
+
+        .container {
+            background-image: url("/landing/contact/Mask.svg");
+            z-index: 20;
+        }
+
+        .form-container {
+            background-color: $black-2;
+            border-style: solid;
+            border-color: $black-3;
+            padding: 1rem 2rem 0;
+            height: fit-content;
+
+            @include media-breakpoint-down(lg) {
+                margin: 0 !important;
+                width: 100%;
+            }
+        }
+
+
     }
-}
 </style>
