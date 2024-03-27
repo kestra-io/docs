46c819643c6bea823eea4c1a1b699dd593ee2d5d
Taron Chatoyan <chatoyan48@gmailc.com>
2024-03-27 14:58:01 +0400

    added borders of the code blocks

--- components/content/ProseCode.vue
+++ components/content/ProseCode.vue
@@ -93,6 +93,7 @@
 
     .code-block {
         background-color: $black-2;
+        border: $block-border;
         padding: 1.25rem 1.5rem;
         border-radius: var(--bs-border-radius-lg);
         color: var(--bs-white);
