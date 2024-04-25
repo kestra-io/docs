5c20091eef3312fa063a7fc7bd7f51a9ceb0ab2c
Taron Chatoyan <47221390+t-chatoyan@users.noreply.github.com>
2024-04-25 11:55:10 +0400

    created videos page (#1223)
    * created videos page
    
    * fixed slug for page tutorial-videos
    
    * added filtering to get tutorial-videos
    
    * fixed featured video displaying
    
    * added endpoint in menu for tutorial videos
    
    * fixed videos category texts
    
    ---------
    
    Co-authored-by: Taron Chatoyan <chatoyan48@gmailc.com>

---
+++ components/videos/TutorialVideo.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="video-container">
+        <iframe
+            width="366"
+            height="212"
+            :src="video.iframeUrl"
+            :title="video.title"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+        />
+        <p class="category">{{video.category}}</p>
+        <h3 class="title">{{video.title}}</h3>
+        <p class="video-info" v-if="video.publicationDate">
+            {{getYMD(video.publicationDate)}}
+        </p>
+        <p class="canal-name">{{video.author}}</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "TutorialVideo",
+    props: {
+      video: {
+        type: Object,
+        required: true
+      },
+      getYMD: {
+        type: Function,
+        required: true
+      }
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+    @import "../../assets/styles/variable";
+
+    .video-container {
+        display: flex;
+        flex-direction: column;
+        gap:  calc($spacer * 0.25);
+
+        iframe {
+            border: 1px solid $black-6;
+            border-radius: calc($spacer * 0.5);
+            width: 100%;
+        }
+
+        p {
+            margin: 0;
+            font-size: $font-size-sm;
+            line-height: calc($spacer * 1.375);
+            font-weight: 400;
+        }
+
+        p.category {
+            color: $purple-36;
+        }
+
+        h3.title {
+            font-size: $font-size-base;
+            font-weight: 400;
+            line-height: calc($spacer * 1.5);
+            color: $white;
+            margin: 0;
+        }
+
+        p.video-info {
+            color: $white-3;
+        }
+
+        p.canal-name {
+            color: $black-8;
+        }
+    }
+</style>
