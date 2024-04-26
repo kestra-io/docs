c83b87e3a3104ec6dafa8ba01d65e688b6bdccb7
YannC <ycoornaert@kestra.io>
2024-04-26 11:30:21 +0200

    fix: cloud license agreement

---
+++ pages/cloud-license-agreement.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="container bd-gutter mt-3 my-md-4 bd-layout">
+        <article class="bd-main order-1">
+            <ContentDoc />
+        </article>
+    </div>
+</template>
+
