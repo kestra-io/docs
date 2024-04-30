a2d2f2d3e037aac72c53861700c3a42a97bdd26f
Taron Chatoyan <chatoyan48@gmailc.com>
2024-04-30 17:41:27 +0400

    created Julia and R pages

---
+++ components/features/language/sub-pages/Scripts.vue
@@ -0,0 +1,170 @@
+<template>
+    <div class="container-fluid bg-dark-4">
+        <div class="container">
+            <div class="container-title mb-5">
+                <h2 class="title">Orchestrating {{title}} Scripts with <span>Kestra</span></h2>
+            </div>
+            <div class="row mb-5 align-items-center justify-content-between" v-for="(scriptItem, index) in scripts">
+                <div
+                    :class="`col-md-5 order-1 ${(index + 2) % 2 === 0 ? 'order-md-0' : 'order-md-1'}`"
+                    :data-aos="`${(index + 2) % 2 === 0 ? 'fade-right' : 'fade-left'}`"
+                >
+                    <h3>{{scriptItem.title}}</h3>
+                    <p>{{scriptItem.description}}</p>
+                </div>
+                <div
+                    :class="`
+                        col-md-6 order-0
+                        ${(index + 2) % 2 === 0 ? 'order-md-1' : 'order-md-0'}
+                        ${scriptItem.mask ? scriptItem.mask : 'mask'}
+                    `"
+                    :data-aos="`${(index + 2) % 2 !== 0 ? 'fade-right' : 'fade-left'}`"
+                >
+                    <NuxtImg
+                        loading="lazy"
+                        format="webp"
+                        :width="scriptItem.imgWidth"
+                        :height="scriptItem.imgHeight && scriptItem.imgHeight"
+                        class="img-fluid"
+                        :src="scriptItem.source"
+                        :alt="scriptItem.title"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: undefined,
+      },
+      scripts: {
+        type: Array,
+        default: null,
+        required: true,
+      },
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+    @import "../../../../assets/styles/variable";
+
+    .container-fluid {
+        position: relative;
+        z-index: 1;
+
+        .container {
+            border-bottom: $block-border;
+
+            .container-title {
+                padding-top: calc($spacer * 4);
+                width: 100%;
+                display: flex;
+                justify-content: center;
+
+                .title {
+                    text-align: center;
+                    font-size: 50px;
+                    font-weight: 600;
+                    text-align: center;
+                    color: $white;
+
+                    @include media-breakpoint-up(lg) {
+                        max-width: 60%;
+                        line-height: 50px;
+                    }
+
+                    span {
+                        background: linear-gradient(90.03deg, #E151F7 2.16%, #5C47F5 65.09%);
+                        background-clip: text;
+                        -webkit-background-clip: text;
+                        -webkit-text-fill-color: transparent;
+                        color: $white;
+                    }
+                }
+            }
+
+            h3 {
+                color: $white;
+                font-size: $h2-font-size;
+                font-weight: 300;
+            }
+
+            p, ul {
+                color: $white-1;
+                font-size: $font-size-md;
+                font-weight: 300;
+
+                b {
+                    color: $white;
+                    font-weight: 600;
+                }
+            }
+
+            .mask, .mask-1 {
+                position: relative;
+
+                & > img {
+                    z-index: 10;
+                    width: 100%;
+                }
+
+                &::after {
+                    width: calc($spacer * 15);
+                    height: calc($spacer * 15);
+                    position: absolute;
+                    content: "";
+                    filter: blur(65px);
+                    background: linear-gradient(180deg, rgba(98, 24, 255, 0.00) 0%, #6117FF 100%);
+                    z-index: -2;
+                }
+            }
+
+            .mask {
+
+                &::after {
+                    right: 214px;
+                    bottom: 86px;
+
+                    @include media-breakpoint-down(lg) {
+                        right: 38px;
+                        bottom: 51px;
+                    }
+
+                    @include media-breakpoint-down(md) {
+                        right: 26px;
+                    }
+
+                }
+            }
+
+            .mask-1 {
+
+                background-image: url(/landing/features/mask.svg);
+
+                &::before {
+                    width: 9rem;
+                    height: 8rem;
+                    position: absolute;
+                    content: "";
+                    filter: blur(79px);
+                    background: linear-gradient(180deg, rgba(98, 24, 255, 0) 0%, #6117FF 100%);
+                    z-index: -2;
+                    right: 530px;
+                    bottom: 220px;
+                }
+
+                &::after {
+                    right: 139px;
+                    bottom: 166px;
+                }
+            }
+        }
+    }
+
+</style>
---
+++ pages/features/code-in-any-language/julia.vue
@@ -0,0 +1,123 @@
+<template>
+    <div>
+        <Head>
+            <Title>Orchestrate your Julia Scripts with Kestra</Title>
+            <Meta name="description" content="Build declaratively your workflows and seamlessly run all your Julia Scripts with Kestra" />
+        </Head>
+
+        <FeaturesLanguageSubPagesHeader
+            title="Julia Scripts"
+            description="Build declaratively your workflows and seamlessly run all your Python Scripts"
+            source="/landing/features/language/julia/header.svg"
+        />
+        <NuxtLazyHydrate when-visible>
+            <FeaturesLanguageSubPagesFeatures
+                :features="juliaFeatures"
+            />
+        </NuxtLazyHydrate>
+        <NuxtLazyHydrate when-visible>
+            <FeaturesLanguageSubPagesPower
+                title="with Julia"
+                :description="juliaPowerDesc"
+                source="/landing/features/language/julia/power.svg"
+            />
+        </NuxtLazyHydrate>
+
+        <NuxtLazyHydrate when-visible>
+            <FeaturesLanguageSubPagesScripts
+                title="Julia"
+                :scripts="juliaScripts"
+            />
+        </NuxtLazyHydrate>
+        <NuxtLazyHydrate when-visible>
+            <FeaturesLanguageSubPagesOrchestrator
+                :description="juliaOrchestratorDesc"
+                source="/landing/features/language/julia/scripts/julia-puzzle.svg"
+            />
+        </NuxtLazyHydrate>
+        <NuxtLazyHydrate when-visible>
+            <FeaturesLanguagePlugins />
+        </NuxtLazyHydrate>
+        <NuxtLazyHydrate when-visible>
+            <LayoutFooterContact
+                title="Getting Started with  Declarative Orchestration"
+                darkButtonText="Get started"
+                darkButtonHref="docs/getting-started"
+                purpleButtonText="Talk to us"
+                purpleButtonHref="https://kestra.io/demo"
+            />
+        </NuxtLazyHydrate>
+    </div>
+</template>
+
+<script setup>
+  const { origin } = useRequestURL()
+
+  const juliaFeatures = {
+    title: 'Advanced Julia Scripting with Kestra',
+    childList: [
+      {
+        source: '/landing/features/language/sub-pages/icon-customize.svg',
+        title: 'Customize at Runtime',
+        description: 'Install any Julia package dynamically as your project demands, maintaining flexibility and power.',
+      },
+      {
+        source: '/landing/features/language/sub-pages/icon-sync.svg',
+        title: 'Import or Sync your Entire Project',
+        description: 'Sync your entire Julia project into Kestra, ensuring that all components, from packages to custom modules, are managed effectively.',
+      },
+      {
+        source: '/landing/features/language/sub-pages/icon-cloud.svg',
+        title: 'Cloud-Agnostic Execution',
+        description: 'Deploy and execute your Julia scripts on any cloud platform using Kestra’s Task Runners, optimizing for performance and cost efficiency.',
+      },
+    ],
+  };
+
+  const juliaPowerDesc = [
+    'Julia is renowned for high-performance numerical analysis and computational science. Leverage Kestra to orchestrate your Julia scripts, enhancing their capabilities in large-scale analytics and machine learning applications. From data ingestion to complex numerical simulations, Kestra streamlines your Julia workflows, accelerating development and deployment.'
+  ];
+
+  const juliaScripts = [
+    {
+      title: 'Embrace GitOps best practices for your Julia scripts',
+      description: 'Kestra takes Julia script orchestration to the next level by embracing Continuous Integration/Continuous Deployment (CI/CD) and GitOps principles. Manage your Julia workflows with the same rigor as your application code: version control, automated testing, and consistent, repeatable deployment environments. Kestra  lets you define workflows in YAML, ensuring that your orchestration is as maintainable and scalable as your applications.',
+      source: '/landing/features/language/git-with-kestra.svg',
+      imgWidth: '520px',
+      imgHeight: '282px',
+    },
+    {
+      title: 'Integration with Docker, Seamless Environment Management',
+      description: 'Simplify managing environments for Julia scripts with Kestra’s Docker integration. Ensure your scripts execute in isolated and consistent settings to enhance reproducibility and scalability. Manage everything from Kestra\'s intuitive editor, making environment management straightforward and effective.',
+      source: '/landing/features/language/julia/scripts/docker.svg',
+      imgWidth: '567px',
+      imgHeight: '420px',
+    },
+    {
+      title: 'Embedded Julia Scripts for Simplicity',
+      description: 'For simpler tasks requiring immediate results, use Kestra’s task feature to embed Julia scripts directly into your workflows. This is perfect for quick data manipulations, statistical calculations, or alert setups within broader workflows, enhancing both speed and efficiency.',
+      source: '/landing/features/language/julia/scripts/embedded-julia.svg',
+      imgHeight: '420px',
+      mask: 'mask-1',
+    }
+  ];
+
+  const juliaOrchestratorDesc = [
+    'Integrate seamlessly with your existing Julia scripts without requiring any code modifications. Continue developing in Julia as usual, and let Kestra handle the orchestration. This seamless integration allows you to focus on creating powerful, efficient code without worrying about the operational overhead.',
+    'Maintain your focus on writing great code, not on adapting it to fit the orchestration tool. Whether your scripts are set up for data processing, automation, or any other task, integrating them into Kestra is as simple as defining your workflow.',
+  ];
+
+  useHead({
+    meta: [
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@kestra_io' },
+      { name: 'twitter:title', content: "Code in Any language with Kestra the Language Agnostic Platform" },
+      {
+        name: 'twitter:description',
+        content: "Separate your business logic from your orchestration logic with a versatile set of language-agnostic developer tools"
+      },
+      { name: 'twitter:image', content: `${origin}/landing/features/declarative/header-bg.svg` },
+      { name: 'twitter:image:alt', content: "Declarative Data Orchestration with Kestra" }
+    ]
+  })
+</script>
