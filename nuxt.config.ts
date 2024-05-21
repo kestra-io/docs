1ccce6a02bb06039f4054a1e7724e390a7f76349
Taron Chatoyan <chatoyan48@gmailc.com>
2024-05-21 15:57:55 +0400

    fixed nuxt.config.ts for MDC

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,8 +8,14 @@ export default defineNuxtConfig({
         'nuxt-gtag',
         'nuxt-multi-cache',
         'vue3-carousel-nuxt',
-        'nuxt-lazy-hydrate'
+        'nuxt-lazy-hydrate',
+        '@nuxtjs/mdc'
     ],
+    mdc: {
+        components: {
+            prose: false, // Disable predefined prose components
+        },
+    },
     sitemap: {
         sitemaps: {
             default: {
@@ -73,9 +79,9 @@ export default defineNuxtConfig({
                 'remark-flexible-markers': {
                     markerClassName: 'type-mark',
                 },
-                'remark-code-import': {
-                    rootDir: process.cwd()
-                },
+                // 'remark-code-import': {
+                //     rootDir: process.cwd()
+                // },
             }
         },
     },
@@ -206,6 +212,7 @@ export default defineNuxtConfig({
     },
 
     devtools: {
+        enabled: true,
         timeline: {
             enabled: true
         }
