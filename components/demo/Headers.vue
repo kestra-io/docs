e704d0122eee721788072de58b1f43e9f606d631
MartinRst <mproset@kestra.io>
2024-06-04 16:22:58 +0200

    Update Headers.vue

--- components/demo/Headers.vue
+++ components/demo/Headers.vue
@@ -4,8 +4,8 @@
             <div class="row">
                 <div class="col-12 col-md-5 d-flex align-items-center">
                     <div class="schedule-demo">
-                        <h1>Ready to See Kestra in <span>Action</span>?</h1>
-                        <p class="description">Connect with one of our Product Specialist to get the best out of our product and its value proposition</p>
+                        <h1>Connect with our Product <span>Specialists</span></h1>
+                        <p class="description"> Evaluate Kestra Enterprise, made for organizations seeking to secure production workloads with high-security standards and enterprise support</p>
                     </div>
                 </div>
                 <div class="col-12 col-md-6 align-items-center d-flex">
