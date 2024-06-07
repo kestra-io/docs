5ddd336514b8aaf5c9cfc0102f92818d165fa3a9
Taron Chatoyan <chatoyan48@gmailc.com>
2024-06-07 18:17:54 +0400

    added test og icon

--- components/layout/Container.vue
+++ components/layout/Container.vue
@@ -170,7 +170,7 @@
       meta: [
         {property: 'og:title', content: title},
         {property: 'og:description', content: description},
-        {property: 'og:image', content: `https://kestra.io/${ogImage}.svg`},
+        {property: 'og:image', content: `/${ogImage}.svg`},
         {property: 'og:image:alt', content: title},
         {property: 'og:url', content: 'https://kestra.io'},
       ],
