94caeefcff0ddc5bdc7d2ccd2ff9736575fc421a
Taron Chatoyan <chatoyan48@gmailc.com>
2024-06-13 17:21:26 +0400

    removed nodeHtmlToImage dependency

--- server/api/pluginimage.js
+++ server/api/pluginimage.js
@@ -1,5 +1,5 @@
 import url from "node:url";
-import nodeHtmlToImage from 'node-html-to-image'
+// import nodeHtmlToImage from 'node-html-to-image'
 
 export default defineEventHandler(async (event) => {
     const runtimeConfig = useRuntimeConfig();
@@ -16,7 +16,7 @@ export default defineEventHandler(async (event) => {
 
 
     const html =  `
-    </div>
+        </div>
             <html>
             <head>
                 <style>
@@ -122,10 +122,10 @@ export default defineEventHandler(async (event) => {
         </html>
     `;
 
-    const ogImage = await nodeHtmlToImage({
-        html
-    });
-
-
-    return  ogImage
+    // const ogImage = await nodeHtmlToImage({
+    //     html
+    // });
+    //
+    //
+    // return  ogImage
 });
