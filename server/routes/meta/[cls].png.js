7969e56270c9d3dd90afd3cc8f0d08a6e9562438
Taron Chatoyan <chatoyan48@gmailc.com>
2024-06-14 12:03:06 +0400

    removed usage Buffer

--- server/routes/meta/[cls].png.js
+++ server/routes/meta/[cls].png.js
@@ -9,7 +9,7 @@ export default defineEventHandler(async (event) => {
     let svgString = `<svg xmlns="http://www.w3.org/2000/svg" style="-webkit-user-select: none;"  width="1200" height="625">
         <rect  width="1200" height="625" fill="#0e0e0e" />
         <foreignObject x="0" y="0" width="1200" height="625">
-            <div xmlns="http://www.w3.org/1999/xhtml" style="width: 1200px; height: 625px; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-image: url('http://localhost:3001/o5g-bg.svg'); background-size: cover; background-position: center; color: #ffffff; padding: 20px;">
+            <div xmlns="http://www.w3.org/1999/xhtml" style="width: 1200px; height: 625px; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-image: url('http://localhost:3001/og-bg.svg'); background-size: cover; background-position: center; color: #ffffff; padding: 20px;">
                 <div style="width: 278.6px; height: 64.34px; position: absolute; top: 108.04px; left: 200.64px; transform: translate(-50%, -50%); background-image: url('https://kestra.io/logo-white.svg'); background-size: 100% 100%; color: #ffffff; padding: 20px;"></div>
                 <div style="width: 425px; height: 425px; position: absolute; top: 50%; left: 75%; transform: translate(-50%, -50%); background-image: url('${icon}'); background-size: cover; background-position: center; color: #ffffff; padding: 20px;"></div>
                 <div style="position: absolute; top: 187.88px; left: 55.64px;">
@@ -20,12 +20,11 @@ export default defineEventHandler(async (event) => {
             </div>
         </foreignObject>
     </svg>`;
-    const svgBuffer = Buffer.from(svgString);
 
 
     event.node.res.writeHead(200, {
         'Content-Type': 'image/svg+xml',
     });
 
-    event.node.res.end(svgBuffer);
+    event.node.res.end(svgString);
 })
