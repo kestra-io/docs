7f07ec8cd41915d50076634f72de46b7937d9dd8
MartinRst <mproset@kestra.io>
2024-07-08 16:19:19 +0200

    Update How.vue

--- components/home/How.vue
+++ components/home/How.vue
@@ -92,7 +92,7 @@
                         <HomeConnectionLine :lineN="3" :strokeDasharray="strokeDasharray3"/>
                     </div>
                     <h3>Schedule your workflows</h3>
-                    <p><p>Configure your workflows to run on a schedule, in response to event-based triggers, via webhooks, through APIs, or in real-time with milliseconds latency.</p>
+                    <p>Configure your workflows to run on a schedule, in response to event-based triggers, via webhooks, through APIs, or in real-time with milliseconds latency.</p>
                 </div>
                 <div class="col-md-6">
                     <div class="img-container">
