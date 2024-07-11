2c724530a39db5b6ef224c8e9b83de49bbd61836
Taron Chatoyan <47221390+t-chatoyan@users.noreply.github.com>
2024-07-11 13:42:58 +0400

    `More Related Blueprints` are not clickable (#1557)
    Co-authored-by: Taron Chatoyan <chatoyan48@gmailc.com>

--- components/blueprints/Related.vue
+++ components/blueprints/Related.vue
@@ -9,7 +9,13 @@
                 type: Array,
                 default: []
             }
-        }
+        },
+        methods: {
+          generateCardHref (blueprint) {
+            let tag = this.tags.find(f => f?.id == blueprint.tags[0]);
+            return `/blueprints/${tag.name.replace(' ', '-')}/${blueprint.id}-${slugify(blueprint.title)}`
+          }
+        },
     }
 </script>
 
@@ -17,7 +23,7 @@
     <LayoutSection subtitle="More Related" subtitle-after="Blueprints">
         <div class="row">
             <div class="col-lg-4 col-md-6 mb-4" v-for="blueprint in relatedBlueprints" data-aos="zoom-in" :key="blueprint.id">
-                <BlueprintsListCard :blueprint="blueprint"  :tags="tags"/>
+                <BlueprintsListCard :blueprint="blueprint"  :tags="tags" :href="generateCardHref(blueprint)"/>
             </div>
         </div>
     </LayoutSection>
