3e3786e0f4e645b8901154ebe89d8b552a3f8fc2
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-08-26 22:53:47 +0200

    feat(site): add security headers

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -1,7 +1,8 @@
-import { useCDNHeaders } from '#nuxt-multi-cache/composables'
-
 export default defineEventHandler((event) => {
     const url = event.node.req.url;
+
+    event.node.res.setHeader("X-Frame-Options", "DENY");
+    event.node.res.setHeader("X-Content-Type-Options", "nosniff");
 
     if (url.startsWith("/api/_content/")) {
         event.node.res.setHeader("Cache-Control", "public, max-age=604800, immutable");
