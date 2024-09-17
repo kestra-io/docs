7288ba67e4205cd877b8d4a498eaa9969e3e839c
Taron Chatoyan <chatoyan48@gmailc.com>
2024-09-17 15:00:34 +0400

    added multi-select for topics

--- components/content/GuidesChildCard.vue
+++ components/content/GuidesChildCard.vue
@@ -1,41 +1,34 @@
 <template>
     <div class="row card-group mb-2">
         <div class="p-0 row filters">
-            <div class="col-xl-auto col-md-6 pb-3 pb-xl-0 order-2 order-xl-0">
-                <select class="form-select bg-dark-2" aria-label="Filter by topic" v-model="topic" @change="changeFilter">
-                    <option :value="null" disabled selected>Filter by topic</option>
-                    <option value="DevOps">DevOps</option>
-                    <option value="Python">Python</option>
-                </select>
+            <div class="col-xl-auto col-sm-6 col-12 pb-3 pb-xl-0">
+                <SelectMultiSelect
+                    name="topic"
+                    :selectedValue="topic"
+                    :options="topicOptions"
+                    :removeItem="removeTopicItem"
+                    :selectItem="selectTopicItem"
+                    :toggleDropdown="toggleTopicDropdown"
+                    :showDropdown="showTopicDropdown"
+                />
             </div>
-            <div class="col-xl-auto col-md-6 pb-3 pb-xl-0 order-1 order-xl-1">
-                <div>
-                    <div :class="`multi-select  bg-dark-2 ${showDropdown ? 'focused' : ''}`" @click="toggleDropdown">
-                        <div class="selected-items">
-                            <span v-if="stage.length === 0">Filter by stage</span>
-                            <div v-for="(item, index) in stage" :key="index" class="selected-item">
-                                <p>{{ item }}</p>
-                                <Close @click.stop="removeItem(index)" />
-                            </div>
-                            <ChevronDown />
-                        </div>
-                    </div>
-
-                    <div class="custom-select">
-                        <ul v-if="showDropdown" class="dropdown-options">
-                            <li v-for="option in options" :key="option" @click="selectItem(option)">
-                                {{ option }}
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+            <div class="col-xl-auto col-sm-6 col-12 pb-3 pb-xl-0">
+                <SelectMultiSelect
+                    name="stage"
+                    :selectedValue="stage"
+                    :options="stageOptions"
+                    :removeItem="removeStageItem"
+                    :selectItem="selectStageItem"
+                    :toggleDropdown="toggleStageDropdown"
+                    :showDropdown="showStageDropdown"
+                />
             </div>
-            <div :class="`col-xl col-md-${stage.length > 0 ? 6 : 12} pb-3 pb-xl-0 order-0 form-group order-xl-2`">
+            <div :class="`col-xl col-md-${stage.length > 0 ? 6 : 12} pb-3 pb-xl-0 form-group`">
                 <Magnify />
                 <input type="text" class="form-control bg-dark-2" placeholder="Search guides" v-model="search">
             </div>
-            <div class="col-xl-auto col-md-6 pb-3 pb-xl-0 order-4 order-xl-4">
-                <div class="clear-filter" @click="removeFilter" v-if="stage.length > 0">
+            <div class="col-xl-auto col-md-6 pb-3 pb-xl-0">
+                <div class="clear-filter" @click="removeFilter" v-if="stage.length || topic.length || search">
                     <DeleteOutline/>
                     <span>Clear filters</span>
                 </div>
@@ -52,6 +45,7 @@
                     </div>
                     <div>
                         <h4 class="card-title">{{ item.title }}</h4>
+                        <p class="card-text">{{item.description}}</p>
                     </div>
                     <div class="topics">
                         <span v-for="(topic, index) in item.topics" :key="index" class="topic-item">{{topic}}</span>
@@ -81,11 +75,13 @@
     const route = useRoute();
 
     const navigation = ref([]);
-    const topic = ref(null);
     const stage = ref([]);
-    const showDropdown = ref(false); // To toggle the dropdown visibility
+    const topic = ref([]);
+    const showStageDropdown = ref(false);
+    const showTopicDropdown = ref(false);
     const search = ref("");
-    const options = ['All stages', 'Getting Started', 'Intermediate', 'Advanced']; // Dropdown options
+    const stageOptions = ['Getting Started', 'Intermediate', 'Advanced'];
+    const topicOptions = ['DevOps', 'Python'];
 
     const stages = ref({
       "Getting Started": "#5A3ABC",
@@ -93,25 +89,46 @@
       "Advanced": "#AB0009"
     });
 
-    const toggleDropdown = () => {
-      showDropdown.value = !showDropdown.value;
+    const toggleStageDropdown = () => {
+      showStageDropdown.value = !showStageDropdown.value;
+      if (showStageDropdown.value) {
+        showTopicDropdown.value = false;
+      }
     };
 
-    const selectItem = (option) => {
-      if (option === 'All stages') {
-        stage.value = ['Getting Started', 'Intermediate', 'Advanced']
-      } else if (!stage.value.includes(option)) {
+    const toggleTopicDropdown = () => {
+      showTopicDropdown.value = !showTopicDropdown.value;
+      if (showTopicDropdown.value) {
+        showStageDropdown.value = false;
+      }
+    };
+
+    const selectStageItem = (option) => {
+      if (!stage.value.includes(option)) {
         stage.value = [...stage.value, option];
       }
-      showDropdown.value = false;
+      showStageDropdown.value = false;
     };
 
-    const removeItem = (index) => {
+    const selectTopicItem = (option) => {
+      if (!topic.value.includes(option)) {
+        topic.value = [...topic.value, option];
+      }
+      showTopicDropdown.value = false;
+    };
+
+    const removeStageItem = (index) => {
       stage.value = stage.value.filter((item, i) => i !== index);
+    };
+
+    const removeTopicItem = (index) => {
+      topic.value = topic.value.filter((item, i) => i !== index);
     };
 
     const removeFilter = () => {
       stage.value = [];
+      topic.value = [];
+      search.value = '';
     };
 
     let currentPage = null;
@@ -135,12 +152,10 @@
 
           if (Array.isArray(stage.value) && stage.value.length > 0) {
             queryParams.stage = { $in: stage.value };
-          } else if (stage.value) {
-            queryParams.stage = stage.value;
           }
 
-          if (topic.value) {
-            queryParams.topics = { $contains: topic.value }
+          if (Array.isArray(topic.value) && topic.value.length > 0) {
+            queryParams.topics = { $contains: topic.value };
           }
 
           if (search.value) {
@@ -175,8 +190,8 @@
       fetchChildDocs()
     }, 1000);
 
-    watch([currentPage, search, () => stage.value], ([pageVal, searchVal, stageVal]) => {
-      debouncedFilterPlugins(pageVal, searchVal, stageVal);
+    watch([currentPage, search, () => stage.value, () => topic.value], () => {
+      debouncedFilterPlugins();
     });
 
 </script>
@@ -196,6 +211,7 @@
     .card-text {
         font-size: $font-size-sm !important;
         line-height: 1rem !important;
+        color: $white-3;
     }
     .card-stage {
         border-radius: 4px;
@@ -276,7 +292,6 @@
         display: flex;
         align-items: center;
         gap: 4px;
-        height: 100%;
 
         span {
             color: $white-3;
@@ -288,113 +303,6 @@
                 fill: $white-3;
             }
         }
-    }
-
-    .multi-select {
-        display: flex;
-        padding: 4px 40px 4px 6px;
-        border-radius: 5px;
-        position: relative;
-        border: $block-border;
-        font-size: $font-size-sm !important;
-        height:  2rem;
-        color: $white;
-        min-width: 221px;
-
-        &.focused {
-            box-shadow: 0 0 0 0.25rem rgba(132, 5, 255, 0.25);
-        }
-
-
-        .selected-items {
-            display: flex;
-            gap: 4px;
-            align-items: center;
-
-            overflow-y: hidden;
-            overflow-x: auto;
-            &::-webkit-scrollbar {
-                display: none;
-            }
-        }
-
-
-        :deep(.material-design-icon) {
-            position: absolute;
-            right: 10px;
-
-            .material-design-icon__svg {
-                bottom: 0;
-                fill: $white;
-            }
-        }
-
-        .selected-item {
-            background-color: $black-6;
-            border-radius: 4px;
-            padding: 0 4px;
-            display: flex;
-            align-items: center;
-            gap: 4px;
-            max-height: 20px;
-            cursor: pointer;
-
-            p {
-                margin: 0;
-                font-size: 1rem;
-                font-weight: 500;
-                white-space: nowrap;
-                color: $white;
-            }
-
-            :deep(.material-design-icon) {
-                position: unset;
-                right: 0;
-
-                .material-design-icon__svg {
-                    position: unset;
-                    fill: $white;
-                }
-            }
-        }
-    }
-
-    .custom-select {
-        position: relative;
-        width: 100%;
-        top: 2px;
-    }
-
-    .dropdown-button {
-        width: 100%;
-        padding: 10px;
-        background-color: #333;
-        color: white;
-        border: none;
-        cursor: pointer;
-        border-radius: 5px;
-    }
-
-    .dropdown-options {
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-        background-color: #444;
-        position: absolute;
-        width: 100%;
-        top: 100%;
-        z-index: 1;
-        border-radius: 5px;
-    }
-
-    .dropdown-options li {
-        padding: 10px;
-        color: white;
-        cursor: pointer;
-    }
-
-    .dropdown-options li:hover {
-        background-color: #666;
     }
 
 </style>
