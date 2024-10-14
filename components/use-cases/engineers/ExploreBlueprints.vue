27e5e196ed3b7599282efd8acb2d03b334b748e0
Taron Chatoyan <chatoyan48@gmailc.com>
2024-10-14 12:49:30 +0400

    fixed use cases blueprints

--- components/use-cases/engineers/ExploreBlueprints.vue
+++ components/use-cases/engineers/ExploreBlueprints.vue
@@ -34,13 +34,13 @@
   const config = useRuntimeConfig();
   const blueprints = ref([])
   const props = defineProps({
-    query: {
+    tag: {
       type: String,
-      required: true
-    },
+      required: false
+    }
   })
   const { data: blueprintsData } = await useAsyncData('blueprints', () => {
-    return $fetch(`${config.public.apiUrl}/blueprints?type=${props.query}`)
+    return $fetch(`${config.public.apiUrl}/blueprints?tags=${props.tag}`)
   });
   const {data: tags} = await useAsyncData('blueprints-tags', () => {
     return $fetch(`${config.public.apiUrl}/blueprints/tags`)
@@ -48,7 +48,7 @@
   const generateCardHref =async (blueprint) => {
     let tag = await tags.value.find(f => f?.id == blueprint.tags[0]);
     if (!tag || !tag.name) {
-        return `/blueprints/unknown/${blueprint.id}-${slugify(blueprint.title)}`; 
+        return `/blueprints/unknown/${blueprint.id}-${slugify(blueprint.title)}`;
     }
     return `/blueprints/${tag.name?.replace(' ', '-')}/${blueprint.id}-${slugify(blueprint.title)}`
   }
