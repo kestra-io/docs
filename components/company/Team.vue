7e0162bab608cf1523f88d8dc9f307f7d2d3dd79
Nicolas Callens <125994028+Nico-Kestra@users.noreply.github.com>
2025-01-22 17:20:56 +0100

    About-us-Add-Ala (#2121)

--- components/company/Team.vue
+++ components/company/Team.vue
@@ -223,13 +223,13 @@
                     </span>
                 </div>
                 <div class="team-card" data-aos="zoom-in">
-                    <img class="img-fluid team-member" src="/landing/company/teams/alin.png" alt="Antoine Lin"/>
+                    <img class="img-fluid team-member" src="/landing/company/teams/asouid.png" alt="Ala Eddine Souid"/>
                     <div class="w-100">
-                        <div class="team-name">Antoine Lin</div>
-                        <div class="team-title font-small">Lead Finance & Admin</div>
+                        <div class="team-name">Ala Eddine Souid</div>
+                        <div class="team-title font-small">Software Engineer</div>
                     </div>
                     <span>
-                        <a href="https://www.linkedin.com/in/antoine-lin-a49aaa74/" target="_blank" class="me-2"><linkedin /></a>
+                        <a href="https://www.linkedin.com/in/alaa-eddine-souid-354987134/" target="_blank" class="me-2"><linkedin /></a>
                     </span>
                 </div>
             </div>
