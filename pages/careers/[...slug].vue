6c9b41c6ae24ab66f69a8cc1eba9fb79432db112
Ludovic DEHON <tchiot.ludo@gmail.com>
2025-02-13 23:00:54 +0100

    fix(website): company page detail failed

--- pages/careers/[...slug].vue
+++ pages/careers/[...slug].vue
@@ -20,7 +20,7 @@
         </template>
         <div v-else class="container bd-gutter mt-3 my-md-4 bd-layout">
             <article class="bd-main order-1">
-                <ContentRenderer :value="page"/>
+                <ContentRenderer v-if="page" :value="page.body"/>
             </article>
         </div>
         <LayoutFooterContact
@@ -39,66 +39,41 @@
     const slug = "/careers/" + (route.params.slug instanceof Array ? route.params.slug.join('/') : route.params.slug);
     const title = ref();
 
-    const page = ref(null);
 
-    if (slug === '/careers/') {
-        useHead({
-            meta: [
-                { name: 'twitter:card', content: 'summary-large-image' },
-                { name: 'twitter:site', content: '@kestra_io' },
-                { name: 'twitter:title', content: "Join Us and Shape the Future of Orchestration Software" },
-                {
-                    name: 'twitter:description',
-                    content: "Discover exciting career opportunities at Kestra. Join our passionate team and help us shape the future of orchestration software"
-                },
-                { name: 'twitter:image', content: `${origin}/og-image.png` },
-                { name: 'twitter:image:alt', content: "About-Us" },
-                { property: 'og:title', content: "Join Us and Shape the Future of Orchestration Software" },
-                {
-                  property: 'og:description',
-                  content: "Discover exciting career opportunities at Kestra. Join our passionate team and help us shape the future of orchestration software"
-                },
-                { property: 'og:image', content: `${origin}/og-image.png` },
-                { property: 'og:image:type', content: "image/svg+xml" },
-                { property: 'og:image:alt', content: "Join Us and Shape the Future of Orchestration Software" },
-            ]
-        })
-    } else {
-        const {data:page, error} = await useAsyncData(`Careers-Page-Item-${slug}`, () => {
-            try {
-                return queryCollection("careers").path(slug).first();
-            } catch (error) {
-                throw createError({statusCode: 404, message: error.toString(), data: error, fatal: true})
-            }
-        });
-
-        if (error && error.value) {
-            throw error.value;
+    const {data: page, error} = await useAsyncData(route.path, () => {
+        try {
+            return queryCollection("careers").path(slug).first();
+        } catch (error) {
+            throw createError({statusCode: 404, message: error.toString(), data: error, fatal: true})
         }
+    });
 
-        page.value.image = `${origin}/og-image.png`
-
-        useContentHead(page)
-
-        useHead({
-            meta: [
-                { name: 'twitter:card', content: 'summary-large-image' },
-                { name: 'twitter:site', content: '@kestra_io' },
-                { name: 'twitter:title', content: page.value.title },
-                { name: 'twitter:description', content: page.value.description },
-                { name: 'twitter:image:alt', content: page.value.title },
-                { name: 'twitter:image', content: `${origin}/og-image.png` },
-                { name: 'twitter:image:alt', content: "About-Us" },
-                { property: 'og:title', content: page.value.title },
-                { property: 'og:description', content: page.value.description },
-                { property: 'og:image', content: `${origin}/og-image.png` },
-                { property: 'og:image:type', content: "image/svg+xml" },
-                { property: 'og:image:alt', content: page.value.title },
-            ]
-        })
-
-        title.value = page.value.title;
+    if (error && error.value) {
+        throw error.value;
     }
+
+    page.value.image = `${origin}/og-image.png`
+
+    useContentHead(page)
+
+    useHead({
+        meta: [
+            { name: 'twitter:card', content: 'summary-large-image' },
+            { name: 'twitter:site', content: '@kestra_io' },
+            { name: 'twitter:title', content: page.value.title },
+            { name: 'twitter:description', content: page.value.description },
+            { name: 'twitter:image:alt', content: page.value.title },
+            { name: 'twitter:image', content: `${origin}/og-image.png` },
+            { name: 'twitter:image:alt', content: "About-Us" },
+            { property: 'og:title', content: page.value.title },
+            { property: 'og:description', content: page.value.description },
+            { property: 'og:image', content: `${origin}/og-image.png` },
+            { property: 'og:image:type', content: "image/svg+xml" },
+            { property: 'og:image:alt', content: page.value.title },
+        ]
+    })
+
+    title.value = page.value.title;
 
 </script>
 
