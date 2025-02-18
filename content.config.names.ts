cfafb7e0569675d98171d9807a4b0f130eab04d5
Bart Ledoux <bledoux@kestra.io>
2025-02-18 19:18:09 +0100

    fix collection prefix

--- content.config.names.ts
+++ content.config.names.ts
@@ -4,10 +4,11 @@ export function slugify(text:string) {
     return slugifyLib(text, {
         lower: true,
         locale: 'en',
+        remove: /[^0-9a-zA-Z_]/g
     });
 }
 
-const contentPrefix = process.env.CF_PAGES_BRANCH && process.env.CF_PAGES_BRANCH !== 'main' ? `${slugify(process.env.CF_PAGES_BRANCH)}-` : ''
+const contentPrefix = process.env.CF_PAGES_BRANCH && process.env.CF_PAGES_BRANCH !== 'main' ? `${slugify(process.env.CF_PAGES_BRANCH)}_` : ''
 
 export const CollectionNames = {
     docs: `${contentPrefix}docs`,
