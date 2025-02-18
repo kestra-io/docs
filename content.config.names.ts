a13f256cebfc86de03ad4ea25208077b4c85435b
Bart Ledoux <bledoux@kestra.io>
2025-02-18 17:04:27 +0100

    make content prefix depend on branch

--- content.config.names.ts
+++ content.config.names.ts
@@ -1,6 +1,10 @@
+import {slugify} from '~/utils/url'
+
+const contentPrefix = process.env.CF_PAGES_BRANCH && process.env.CF_PAGES_BRANCH !== 'main' ? `${slugify(process.env.CF_PAGES_BRANCH)}-` : ''
+
 export const CollectionNames = {
-    docs: 'docs_a',
-    blogs: 'blogs_b',
-    careers: 'careers_c',
-    misc: 'misc_d'
+    docs: `${contentPrefix}docs`,
+    blogs: `${contentPrefix}blogs`,
+    careers: `${contentPrefix}careers`,
+    misc: `${contentPrefix}misc`
 } as const
--- utils/url.ts
+++ utils/url.ts
@@ -1,12 +1,12 @@
 import slugifyLib from "slugify";
 
-export function slugify(text) {
+export function slugify(text:string) {
     return slugifyLib(text, {
         lower: true,
         locale: 'en',
     });
 }
 
-export function camelToKebabCase(text) {
+export function camelToKebabCase(text:string) {
     return text.replace(/([a-z])([A-Z][a-z])/, '$1-$2').toLowerCase();
 }
