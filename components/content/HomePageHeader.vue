7a9402e77345a9609e92d3264dce95b86ff5ad11
Bart Ledoux <bledoux@kestra.io>
2025-02-21 11:46:25 +0100

    fix: updates to docs homepage on mobile

--- components/content/HomePageHeader.vue
+++ components/content/HomePageHeader.vue
@@ -21,16 +21,31 @@
     .home-page-header {
         h2 {
             padding-top: 0;
-            font-size: $font-size-xl;
-            font-weight: 600;
+            font-size: $font-size-md !important;
+            font-weight: 500;
+            margin-top: 2rem;
+            margin-bottom: .75rem;
+            @include media-breakpoint-up(md) {
+                margin-top: 0;
+                margin-bottom: 0;
+            }
         }
 
         :deep(p){
-            font-size: $font-size-sm;
+            font-size: $font-size-xs;
             line-height: 1.5em;
+            margin: .5rem 0;
+            @include media-breakpoint-up(md) {
+                font-size: $font-size-sm;
+                margin: 1rem 0;
+            }
         }
 
         .kestra-logo {
+            display: none;
+            @include media-breakpoint-up(md) {
+                display: block;
+            }
             float: right;
             width: 132px;
             margin: 3rem;
