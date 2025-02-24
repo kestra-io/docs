9ef37b13576493f6ef80f69a52e75cdee48e7195
MartinRst <mproset@kestra.io>
2025-02-24 09:20:25 +0100

    aws step function

---
+++ pages/vs/aws-step-function.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Head>
+            <Title>Airflow vs Kestra</Title>
+            <Meta name="description" content="What are the differences between Kestra and AWS Step Function" />
+        </Head>
+
+        <Header />
+        <Main />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<script>
+    import Header from '~/components/vs/aws-step-function/Header.vue';
+    import Main from '~/components/vs/aws-step-function/Main.vue';
+
+    export default {
+        components: {Header, Main}
+    }
+</script>
