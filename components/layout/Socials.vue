b5e17085525a6423ec4fb39903550dd2970798d8
Bart Ledoux <bledoux@kestra.io>
2025-03-05 09:48:23 +0100

    fix: warning in dev for socials

--- components/layout/Socials.vue
+++ components/layout/Socials.vue
@@ -2,46 +2,42 @@
     <ul class="list-unstyled">
         <li class="list-inline-item">
             <a href="https://github.com/kestra-io" title="GitHub" target="_blank">
-                <github title="" />
+                <Github title="" />
             </a>
         </li>
         <li class="list-inline-item ml-1">
             <a href="https://twitter.com/kestra_io" title="Twitter" target="_blank">
-                <twitter />
+                <Twitter />
             </a>
         </li>
         <li>
             <a href="https://web-cdn.bsky.app/profile/kestra.io" title="BlueSky" target="_blank">
-                <bluesky />
+                <BlueSky />
             </a>
         </li>
         <li class="list-inline-item ml-1">
             <a href="https://www.linkedin.com/company/kestra" title="LinkedIn" target="_blank">
-                <linkedin />
+                <LinkedIn />
             </a>
         </li>
         <li class="list-inline-item ml-1">
             <a href="https://www.youtube.com/@kestra-io" title="YouTube" target="_blank">
-                <youtube />
+                <Youtube />
             </a>
         </li>
         <li class="list-inline-item ml-1">
             <a href="https://kestra.io/slack" title="Slack" target="_blank">
-                <slack title="" />
+                <Slack title="" />
             </a>
         </li>
     </ul>
 </template>
 
-<script>
-    import Linkedin from "vue-material-design-icons/Linkedin.vue";
+<script setup>
+    import LinkedIn from "vue-material-design-icons/Linkedin.vue";
     import Slack from "vue-material-design-icons/Slack.vue";
     import Github from "vue-material-design-icons/Github.vue";
     import Youtube from "vue-material-design-icons/Youtube.vue";
     import Twitter from "../icons/TwitterXIcon.vue";
     import BlueSky from "../icons/BlueSkyIcon.vue";
-
-    export default {
-        components: {Github, Slack, Twitter, Linkedin, Youtube, BlueSky}
-    }
 </script>
