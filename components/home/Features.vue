fa905e4b1179eef459b1351dc990a3e9868b570e
Bart Ledoux <bledoux@kestra.io>
2025-03-17 10:55:40 +0100

    make feature card (CSS only)

--- components/home/Features.vue
+++ components/home/Features.vue
@@ -1,8 +1,21 @@
 <template>
     <div class="container">
-        <h2><span>Simplicity scales</span>, complexity fails</h2>
-        <p>Mix the flexibility of code-based orchestration
-            with a no-code interface that anyone can learn in minutes.</p>
+        <h2><span>Simplicity Scales,</span> Complexity Fails</h2>
+        <p>
+            Mix the flexibility of code-based orchestration<br/>
+            with a no-code interface that anyone can learn in minutes.
+        </p>
+        <div class="cta-buttons">
+            <a class="btn btn-primary" href="#">See All Features</a>
+            <button class="btn btn-secondary" @click="">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.5 7.5V16.5L16.5 12L9.5 7.5Z" fill="black"/>
+                    <path d="M20 4H4C2.9 4 2 4.9 2 6V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM20 18.01H4V5.99H20V18.01Z" fill="black"/>
+                </svg>
+                Overview Video
+            </button>
+        </div>
+        <HomeFeaturesBlock/>
     </div>
 </template>
 
@@ -14,16 +27,35 @@
     .container {
         text-align: center;
         color: white;
+        padding-top: 4rem;
+
         h2 {
             font-size: 2.5rem;
             font-weight: 700;
-            margin-bottom: 2rem;
+            margin-bottom: .5rem;
             span {
                 color: $primary;
                 background: linear-gradient(90deg, #7C2EEA 0%, #658AF9 100%) no-repeat center;
                 background-size: 350px;
                 -webkit-background-clip: text;
                 -webkit-text-fill-color: transparent;
+            }
+        }
+        p{
+            text-align: center;
+            margin: 0 auto;
+            text-wrap: pretty;
+            margin-bottom: 2rem;
+        }
+        .cta-buttons{
+            display: flex;
+            justify-content: center;
+            gap: 1rem;
+            margin-bottom: 2rem;
+            .btn{
+                display: flex;
+                align-items: center;
+                gap: .5rem;
             }
         }
     }
---
+++ components/home/FeaturesBlock.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="card-block">
+        <div v-for="feat of features" class="block-content">
+            <div class="clipper">
+                <div class="block">
+                    <img :src="feat.icon" alt="Feature Icon"/>
+                </div>
+            </div>
+            <h3 :style="{textDecorationColor: feat.color}">{{ feat.name }}</h3>
+            <p>{{ feat.description }}</p>
+        </div>
+        <div class="block-center-image">
+            <img src="@/assets/images/homepage/features-cube.svg" alt="Feature Container"/>
+            <img src="@/assets/images/homepage/features-kestra.svg" alt="Kestra Monogram" class="kestra-logo" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import APIIcon from "@/assets/images/homepage/features-api.svg";
+import CloudIcon from "@/assets/images/homepage/features-cloud.svg";
+import CogIcon from "@/assets/images/homepage/features-cog.svg";
+import LogosIcon from "@/assets/images/homepage/features-Logos.svg";
+
+const features = [
+    {
+        name: "Connect to Any API or Service",
+        description: "Automating everything from data pipelines, microservices, business processes to infrastructure provisioning.",
+        icon: APIIcon,
+        color: "#E58238",
+    },
+    {
+        name: "Deploy Anywhere",
+        description: "Run Kestra anywhere—on-prem, hybrid, or any cloud, no dependencies, no installation headaches.",
+        icon: CloudIcon,
+        color: "#3991FF",
+    },
+    {
+        name: "Build Everything As Code & From the UI",
+        description: "Empower every team members to collaborate from a single tool—code for developers, UI for everyone else, all in sync.",
+        icon: CogIcon,
+        color: "#21CE9C",
+    },
+    {
+        name: "Write Business Logic in Any Language",
+        description: "Orchestrate custom business logic in Python, R, Java, Julia, Ruby, and any kind of language.",
+        icon: LogosIcon,
+        color: "#FCE07C",
+    }
+]
+</script>
+
+<style lang="scss" scoped>
+    @import "../../assets/styles/variable";
+    .card-block {
+        position: relative;
+        background: linear-gradient(180deg, #21242E 0%, #1A1C24 100%);
+        border-radius: 1rem;
+        box-shadow: 0px 12px 24px 8px #00000017;
+        padding: 6rem;
+        margin: 2rem 0;
+        display: grid;
+        grid-template-columns: 1fr 220px 1fr;
+        gap: 3rem;
+    }
+
+    .block-center-image{
+        background: url(@/assets/images/homepage/features-bg.svg) no-repeat center;
+        grid-column: 2 / 3;
+        grid-row: 1 / 3;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+
+        .kestra-logo{
+            position: absolute;
+            width: 50px;
+            top: 39%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            transition: all 0.2s;
+        }
+    }
+
+    .block-content{
+        text-align: left;
+        h3{
+            font-size: 1.2rem;
+            line-height: 1.4rem;
+            text-underline-offset: 20%;
+            text-decoration: underline;
+            text-decoration-thickness: 4px;
+            filter: grayscale(1);
+        }
+        &:nth-child(2n + 1){
+            text-align:right;
+        }
+        &:hover{
+            h3{
+                filter: none;
+            }
+        }
+        .clipper{
+            position: absolute;
+            z-index: 1;
+            left: 50%;
+            top: 50%;
+            clip-path: polygon(-212% -128%, 201% -135%, -4% 6%);
+            height: 100px;
+            width: 100px;
+            .block{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                left: -50px;
+                width: 100%;
+                height: 100%;
+                transition: transform 0.2s;
+            }
+        }
+
+        &:hover{
+            .block{
+                transform: translateY(-83px);
+            }
+        }
+
+        // on hover, hide all siblings with the class block-center-image
+        &:hover ~ .block-center-image .kestra-logo{
+            opacity: 0;
+            transform: translate(-50%, 0);
+        }
+    }
+
+</style>
