5b360b790559f870c586248d9e9f3fdf7d701c56
MilosPaunovic <paun992@hotmail.com>
2025-04-01 13:31:37 +0200

    feat(ui): implement the new cloud page

--- components/cloud/Features.vue
+++ components/cloud/Features.vue
@@ -1,83 +1,122 @@
 <template>
-    <div class="container-fluid bg-dark-4">
-        <div class="container">
-            <div class="cloud-cards">
-                <Card
-                    :icon="RocketLaunch"
-                    titleHtml="Get started </br> In minutes"
-                />
-                <Card
-                    :icon="CreditCardOff"
-                    titleHtml="Sign up for free </br> No credit card required"
-                />
-                <Card
-                    :icon="CloudCheck"
-                    titleHtml="Fully-managed </br> Infrastructure"
-                />
+    <div class="container mt-5 pt-5">
+        <div class="row mt-5">
+            <div class="col-md-12 col-lg-7">
+                <h2>
+                    Kestra Cloud
+                    <span class="highlight">Early Adopter Program</span>
+                </h2>
+                <h5 class="mt-3">
+                    Be among the first to experience Kestra Cloud with exclusive
+                    access before general availability.
+                </h5>
+                <div class="row mt-5">
+                    <div class="col-6">
+                        <h6>
+                            <img src="./images/calendar_month.png" />
+                            Get Exclusive 30-Day Access
+                        </h6>
+                        <p>
+                            Explore Kestra Cloud with full, exclusive access for
+                            30 days prior to general availability.
+                        </p>
+                    </div>
+                    <div class="col-6">
+                        <h6>
+                            <img src="./images/small_cloud.png" />
+                            Fully Managed Infrastructure
+                        </h6>
+                        <p>
+                            No infrastructure setup, deployment worries, or
+                            ongoing maintenance. We handle the heavy lifting
+                            while you focus on your workflows.
+                        </p>
+                    </div>
+                    <div class="col-6 mt-3">
+                        <h6>
+                            <img src="./images/support_agent.png" />
+                            Dedicated Experts Exchange
+                        </h6>
+                        <p>
+                            Receive personalized support and expert guidance
+                            throughout your access period.
+                        </p>
+                    </div>
+                    <div class="col-6 mt-3">
+                        <h6>
+                            <img src="./images/check_circle.png" />
+                            Enterprise Feature Set
+                        </h6>
+                        <p>
+                            Access enterprise features including enhanced
+                            security, RBAC, observability, and governance during
+                            your exclusive access.
+                        </p>
+                    </div>
+                    <div class="col-12 mt-5">
+                        <p class="mb-2">
+                            “Kestra Cloud has been a pivotal part of giving us
+                            flexibility and scalability we need to pull off
+                            complex processes we do at Foundation Direct.”
+                        </p>
+                        <p class="mb-0">
+                            <strong>
+                                Michael Heidner - CEO & Kestra Cloud Early
+                                Adopter
+                            </strong>
+                        </p>
+                        <img src="./images/foundation.png" />
+                    </div>
+                </div>
             </div>
+            <div id="cloudForm" class="col-md-12 col-lg-5 form" />
         </div>
     </div>
 </template>
 
-<script setup>
-  import RocketLaunch from "vue-material-design-icons/RocketLaunch.vue"
-  import CreditCardOff from "vue-material-design-icons/CreditCardOff.vue"
-  import CloudCheck from "vue-material-design-icons/CloudCheck.vue"
+<script setup lang="ts">
+import { hubspotFormCreate } from "~/utils/hubspot.js";
+
+hubspotFormCreate("cloud_form", {
+    portalId: "27220195",
+    formId: "ceb64a26-1d2a-4335-b1bf-d856d1854ba8",
+    region: "eu1",
+    target: "#cloudForm",
+});
 </script>
 
-<style lang="scss" scoped>
-    @import "../../assets/styles/variable";
+<style scoped lang="scss">
+.cloud {
+    & img {
+        width: 180px;
+        height: auto;
+    }
 
-    .container-fluid {
+    h6 > img {
+        width: 30px;
+        height: auto;
+        margin-right: 10px;
+    }
+
+    & .form {
         position: relative;
-        &::before {
-            content: "";
-            position: absolute;
-            width: calc($spacer * 11);
-            height: calc($spacer * 11);
-            background: linear-gradient(180deg, rgba(98, 24, 255, 0) 0%, #6117FF 100%);
-            filter: blur(82px);
-            z-index: 5;
-            left: 50%;
-            top: -5rem;
+        padding: 60px 40px 0 40px;
+        border-radius: 16px;
+        background-color: white;
 
-            @include media-breakpoint-down(lg) {
-                left: 35%;
-                top: 50%;
+        &::after {
+            @media (min-width: 992px) {
+                content: "";
+                position: absolute;
+                top: -20%;
+                right: -50%;
+                width: 400px;
+                height: 400px;
+                background-image: url("./images/cloud_visual.png");
+                background-size: contain;
+                background-repeat: no-repeat;
             }
         }
     }
-    .cloud-cards {
-        padding: calc($spacer * 2) 0 calc($spacer * 5.6);
-        display: flex;
-        justify-content: center;
-        gap: calc($spacer * 1.8);
-        position: relative;
-        z-index: 6;
-
-        :deep(.title-block) {
-            align-items: center;
-        }
-
-        :deep(.card) {
-            width: 100%;
-            height: unset;
-        }
-
-        @include media-breakpoint-down(lg) {
-            flex-direction: column;
-            align-items: center;
-
-            :deep(.card) {
-                width: 60%;
-            }
-        }
-
-        @include media-breakpoint-down(md) {
-            :deep(.card) {
-                width: auto;
-                min-width: 100%;
-            }
-        }
-    }
+}
 </style>
---
+++ components/cloud/HowItWorks.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="container my-5 how-it-works">
+        <h2>How It Works</h2>
+        <div class="row my-5 pb-5">
+            <div
+                v-for="(column, index) in columns"
+                :key="index"
+                class="col-md-12 col-lg"
+            >
+                <div class="mb-3 numbers">{{ index + 1 }}</div>
+                <h6 class="mb-3">{{ column.title }}</h6>
+                <p>{{ column.description }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const columns = [
+    {
+        title: "Request Access",
+        description:
+            "Complete our short form to request your spot in the program.",
+    },
+    {
+        title: "Selection & Scheduling",
+        description:
+            "Our team selects qualified participants and schedules a brief call with you.",
+    },
+    {
+        title: "Validation Call",
+        description:
+            "We'll confirm your participation and outline the details and conditions of your exclusive access.",
+    },
+    {
+        title: "Setup & Launch",
+        description:
+            "We provision your Kestra Cloud instance so you can start orchestrating workflows immediately.",
+    },
+    {
+        title: "Feedback & Next Steps",
+        description:
+            "After your exclusive access, we'll collect your feedback and discuss personalized next steps for ongoing use.",
+    },
+];
+</script>
+
+<style scoped lang="scss">
+.cloud {
+    & .how-it-works::after {
+        content: "";
+        position: absolute;
+        bottom: -45%;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url("./images/gradient_bottom.png");
+        background-repeat: no-repeat;
+    }
+
+    & .numbers {
+        font-weight: 600;
+        font-size: 12px;
+        height: 24px;
+        width: 24px;
+        border: 1px solid grey;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+}
+</style>
