4d934c666a9c418d332ae806f48b075bffa08256
Bart Ledoux <bledoux@kestra.io>
2025-04-03 11:27:07 +0200

    remove annoying hover effect

--- components/home/Card.vue
+++ components/home/Card.vue
@@ -47,12 +47,6 @@ const relativeMouse = computed(() => {
     border-radius: 1rem;
     box-shadow: 0px 12px 24px 8px #00000017;
     border: 1px solid #2C2E4B;
-     &:hover{
-         background:
-             linear-gradient(90deg, rgba(255, 255, 255, .05) 0%, rgba(255, 255, 255, .05) 100%),
-             linear-gradient(180deg, #21242E99 0%, #1A1C2499 100%),
-             linear-gradient(90deg,#1A1C24 0%, #373a44 50%, #1A1C24 100%);
-     }
 }
 
 .home-card :deep(a){
