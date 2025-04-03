15add98caeefbe91f51f227c591d7017580f8df7
Barthélémy Ledoux <ledouxb@me.com>
2025-04-03 14:02:37 +0200

    refactor: use one single components for all four misc pages (#2260)

--- pages/cloud-license-agreement.vue
+++ pages/cloud-license-agreement.vue
@@ -1,18 +1,3 @@
 <template>
-    <div class="container bd-gutter mt-3 my-md-4 bd-layout">
-        <article class="bd-main order-1">
-            <ContentRenderer :value="page" class="bd-markdown" />
-        </article>
-    </div>
+    <Misc />
 </template>
-
-<script setup>
-const {public:{CollectionNames}} = useRuntimeConfig()
-const route = useRoute()
-const {data: page} = await useAsyncData(
-    route.path,
-    () => queryCollection(CollectionNames.misc).path(route.path).first()
-);
-</script>
-
-
