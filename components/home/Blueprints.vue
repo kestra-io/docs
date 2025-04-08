3918fa929f6f442c818e5b9dde0b8cca0ff806c6
Bart Ledoux <bledoux@kestra.io>
2025-04-08 09:54:51 +0200

    refactor: extract the blueprints carousel from HP

--- components/home/Blueprints.vue
+++ components/home/Blueprints.vue
@@ -4,13 +4,7 @@
             <h2><span>250+ Blueprints,</span> Ready to Build.</h2>
             <p>Jump start your workflows with a growing library of workflow templates</p>
         </div>
-        <div class="blueprints-container">
-            <button class="navigation navigation-left" @click="scrollLeft"><ArrowLeftIcon/></button>
-            <button class="navigation navigation-right" @click="scrollRight"><ArrowRightIcon/></button>
-            <div class="blueprints-carousel" ref="wrapper">
-                <BlueprintsListCard v-for="blueprint in blueprints" :key="blueprint.id" :blueprint="blueprint" :href="generateCardHref(blueprint)"/>
-            </div>
-        </div>
+        <HomeBlueprintsCarousel :blueprints="blueprints" />
         <div class="button-container">
             <NuxtLink href="/blueprints" class="btn btn-lg btn-primary">Explore Blueprints</NuxtLink>
         </div>
@@ -18,30 +12,9 @@
 </template>
 
 <script lang="ts" setup>
-import ArrowLeftIcon from "vue-material-design-icons/ArrowLeft.vue"
-import ArrowRightIcon from "vue-material-design-icons/ArrowRight.vue"
 const config = useRuntimeConfig()
 const { data } = await useFetch<{results: any[]}>(`${config.public.apiUrl}/blueprints/versions/latest?page=1&size=20`)
-
-const blueprints = computed(() => data.value?.results)
-
-const generateCardHref = (blueprint: {id: string}) => {
-  return `/blueprints/${blueprint.id}`
-}
-
-const wrapper = ref<HTMLElement | null>(null)
-
-const scrollLeft = () => {
-    if(wrapper.value){
-        wrapper.value.scrollTo({left:wrapper.value.scrollLeft - 400, behavior: 'smooth'})
-    }
-}
-
-const scrollRight = () => {
-    if(wrapper.value){
-        wrapper.value.scrollTo({left:wrapper.value.scrollLeft + 400, behavior: 'smooth'})
-    }
-}
+const blueprints = computed(() => data.value?.results ?? [])
 </script>
 
 <style lang="scss" scoped>
@@ -65,41 +38,6 @@ const scrollRight = () => {
     }
     p {
         color: #777;
-    }
-    .blueprints-container {
-        position: relative;
-        .navigation{
-            position: absolute;
-            top: 50%;
-            transform: translateY(-50%);
-            background-color: #444955;
-            color: white;
-            border-radius: 50%;
-            z-index: 1;
-            height: 44px;
-            width: 44px;
-            border: none;
-            font-size: 24px;
-            padding: 0;
-            &-left{
-                left: 1rem;
-            }
-            &-right{
-                right: 1rem;
-            }
-        }
-    }
-    .blueprints-carousel {
-        display: flex;
-        flex-wrap: nowrap;
-        gap: 20px;
-        margin: 1rem 2rem;
-        overflow-x: auto;
-        scrollbar-width: none;
-        >a{
-            flex: 1;
-            min-width: 350px;
-        }
     }
     .button-container{
         text-align: center;
