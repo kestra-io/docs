4ecbfa41d5a73c37ceadcfd2d159bf8548eb5370
MartinRst <125883179+MartinRst@users.noreply.github.com>
2025-04-11 13:47:16 +0200

    kestra vs Aria (#2418)
    * V1
    
    * update hero image

---
+++ pages/vs/vmware-cloud-foundation.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Head>
+            <Title>Kestra Alternative to VmWare Cloud Foundation</Title>
+            <Meta name="description" content="What are the differences between Kestra and Dollar Universe Workload Automation" />
+        </Head>
+
+        <Header />
+        <Main />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<script>
+    import Header from '~/components/vs/vmware-cloud-foundation/Header.vue';
+    import Main from '~/components/vs/vmware-cloud-foundation/Main.vue';
+
+    export default {
+        components: {Header, Main}
+    }
+</script>
