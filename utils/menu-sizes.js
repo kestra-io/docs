605c7ee1763c84ad6bbfcad84faf59b62bd97d30
Piyush Bhaskar <102078527+Piyush-r-bhaskar@users.noreply.github.com>
2025-04-29 15:10:53 +0530

    fixes the height and gap

--- utils/menu-sizes.js
+++ utils/menu-sizes.js
@@ -2,66 +2,66 @@ let menuSizes = {
     product : {
         xxl: {
             width: '440px',
-            height: '490px',
+            height: '435px',
             headerMenuTranslateX: 'calc(50vw - 600px)',
         },
         xl: {
             width: '440px',
-            height: '490px',
+            height: '435px',
             headerMenuTranslateX: 'calc((100vw - 780px) / 2)',
         },
         lg: {
             width: '440px',
-            height: '490px',
+            height: '435px',
             headerMenuTranslateX: 'calc((100vw - 780px) / 2)',
         },
         md: {
             width: '440px',
-            height: '490px',
+            height: '435px',
             headerMenuTranslateX: 'calc((100vw - 780px) / 2)',
         },
     },
     solutions: {
         xxl: {
             width: '1320px',
-            height: '750px',
+            height: '630px',
             headerMenuTranslateX: 'calc(50vw - 600px)',
         },
         xl: {
             width: '1300px',
-            height: '750px',
+            height: '630px',
             headerMenuTranslateX: 'calc((100vw - 1200px) / 2)',
         },
         lg: {
             width: '1250px',
-            height: '750px',
+            height: '630px',
             headerMenuTranslateX: 'calc((100vw - 1120px) / 2)',
         },
         md: {
             width: '1200px',
-            height: '750px',
+            height: '630px',
             headerMenuTranslateX: 'calc((100vw - 380px) / 2)',
         },
     },
     resources: {
         xxl: {
             width: '900px',
-            height: '480px',
+            height: '410px',
             headerMenuTranslateX: 'calc(50vw - 450px)',
         },
         xl: {
             width: '900px',
-            height: '480px',
+            height: '410px',
             headerMenuTranslateX: 'calc((100vw - 900px) / 2)',
         },
         lg: {
             width: '800px',
-            height: '480px',
+            height: '410px',
             headerMenuTranslateX: 'calc((100vw - 800px) / 2)',
         },
         md: {
             width: '580px',
-            height: '480px',
+            height: '410px',
             headerMenuTranslateX: 'calc((100vw - 580px) / 2)',
         }
     }
