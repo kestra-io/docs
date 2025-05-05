07ac9de0d13b228590a5baf289c13a8c2a1d2ba6
brian.mulier <bmmulier@hotmail.fr>
2025-05-05 15:31:18 +0200

    feat: add scalar API DOC instead of rapidoc

--- content/docs/api-reference/index.md
+++
@@ -1,6 +0,0 @@
----
-title: API Reference
-icon: /docs/icons/api.svg
----
-
-<ChildTableOfContents :max="1" />
---
+++ pages/api-docs/ee.vue
@@ -0,0 +1,19 @@
+<template>
+    <ApiReference :configuration="configuration" />
+</template>
+
+<script setup lang="ts">
+    import { ApiReference, type ApiReferenceConfiguration } from '@scalar/api-reference'
+
+    const configuration: ApiReferenceConfiguration = {
+        url: "/kestra-ee.yml",
+        forceDarkModeState: "dark",
+        hideTestRequestButton: true,
+        hideDarkModeToggle: true,
+        hideClientButton: true,
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use '@scalar/api-reference/style.css' as *;
+</style>
