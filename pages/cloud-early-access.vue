c97ab9fb1b0cea942c2ba9eb3030973f0a7cbf9b
Piyush Bhaskar <102078527+Piyush-r-bhaskar@users.noreply.github.com>
2025-05-14 10:54:30 +0530

    feat(core): add cloud early access page with form component

---
+++ pages/cloud-early-access.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="cloud">
+        <Head>
+            <Title>Kestra Cloud Early Access</Title>
+            <Meta
+                name="description"
+                content="Fill the form to get access to Kestra cloud early access."
+            />
+            <Meta name="robots" content="noindex,nofollow" />
+        </Head>
+        <CloudEarlyAccessForm />
+    </div>
+</template>
+
