a644280e9d8fd9324b465f4f94e09ef9659c0b65
Barthélémy Ledoux <bledoux@kestra.io>
2025-05-14 15:48:57 +0200

    optimize according to anim

--- pages/rive.vue
+++ pages/rive.vue
@@ -28,6 +28,7 @@ onMounted(() => {
         onLoad: () => {
             riveLoaded.value = true
             anim.resizeDrawingSurfaceToCanvas();
+            anim.delete();
         },
     });
 })
