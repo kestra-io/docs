3a11a11a5fd93b534a12e7ba87e361284bb8d7ad
Bart Ledoux <bledoux@kestra.io>
2025-07-12 16:26:30 +0200

    update the mainetance page

--- pages/maintenance.vue
+++ pages/maintenance.vue
@@ -1,3 +1,14 @@
 <template>
-  Maintenance
+  <Error
+    :error="{
+      statusCode: 503,
+      message: 'Maintenance mode',
+      stack: ''
+    }"
+    title="Maintenance Mode"
+    description="The site is currently undergoing maintenance. Please check back later."
+  />
 </template>
+
+<script lang="ts" setup>
+</script>
