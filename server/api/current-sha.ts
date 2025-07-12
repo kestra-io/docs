2e5364acbb55dc6e2a6e61d622cf13e8b9c0f422
Bart Ledoux <bledoux@kestra.io>
2025-07-12 22:44:27 +0200

    wait in ci for current-sha to correspond

--- server/api/current-sha.ts
+++ server/api/current-sha.ts
@@ -1,6 +1,4 @@
 export default defineEventHandler(async (event) => {
     const currentSHA = useRuntimeConfig().public.currentSHA
-    return {
-        sha: currentSHA
-    }
+    return currentSHA
 })
