31202a265b4bee9fc19a18e9da3b1177c95d2306
Bart Ledoux <bledoux@kestra.io>
2025-07-12 15:59:11 +0200

    remove console log

--- server/api/current-sha.ts
+++ server/api/current-sha.ts
@@ -5,14 +5,12 @@ export default defineEventHandler(async (event) => {
     // deal with only get and put of currentSha using useStorage
     if (event.method === 'GET') {
         const currentSha = await storage.getItem(KEY_CURRENT_SHA)
-        console.log(`Current SHA fetched: ${currentSha}`)
         return { sha: currentSha }
     }
 
     if (event.method === 'PUT') {
         const body = await readBody(event)
         await storage.setItem(KEY_CURRENT_SHA, body.sha)
-        console.log(`Current SHA set: ${body.sha}`)
         return { sha: body.sha }
     }
 })
