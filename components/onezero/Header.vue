7c327564d5e6e5465a21118514a02242a484ff14
Piyush Bhaskar <impiyush0012@gmail.com>
2025-09-02 18:49:55 +0530

    with component directory

---
+++ components/onezero/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <header>
+        <h1>Kestra 1.0</h1>
+        <p>The Declarative Agentic Orchestration Platform</p>
+    </header>
+</template>
+<script setup lang="ts">
+</script>
+<style lang="scss" scoped>
+</style>
