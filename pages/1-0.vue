44b2d05ad81c7d62e39db7128ac8b0d84d014488
Piyush Bhaskar <impiyush0012@gmail.com>
2025-09-02 18:47:09 +0530

    feat(1-0): create page and directory for 1-0 landing

---
+++ pages/1-0.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="features">
+
+        <Head>
+            <Title>Introducing Kestra 1.0: The Declarative Agentic Orchestration Platform."</Title>
+            <Meta name="description" content="Build workflows faster with AI-assisted code, autonomous agents, and production-ready features. Scale AI Agents in days instead of weeks with full governance and no vendor lock-in." />
+        </Head>
+    </div>
+</template>
