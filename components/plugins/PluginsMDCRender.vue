cc2601c894962e5b900a9bcb3d51a49930970355
Bart Ledoux <bledoux@kestra.io>
2025-09-23 17:45:10 +0200

    start moving plugins to a loader

--- components/plugins/PluginsMDCRender.vue
+++ components/plugins/PluginsMDCRender.vue
@@ -11,24 +11,16 @@
                     :routePath="`/plugins/${pluginType ?? ''}`"
         >
             <template v-slot:markdown="{ content }">
-                <MDCRenderer :value="content">
-                    <template #default="mdcProps">
-                        <pre v-if="mdcProps.error" style="color: white;">{{ mdcProps.error }}</pre>
-                        <ContentRenderer v-else class="markdown" :value="mdcProps?.body"/>
-                    </template>
-                </MDCRenderer>
+                <h1 class="mb-4">INDEX</h1>
+                <pre>{{ page?.body }}</pre>
+                <MDCRenderer v-if="docAst?.body" :body="content" :data="docAst.data" :key="docAst" :components="proseComponents" />
             </template>
         </PluginIndex>
         <Suspense v-else>
             <SchemaToHtml v-if="page.body.jsonSchema" class="plugin-schema" :schema="page.body.jsonSchema" :plugin-type="pluginType ?? ''"
                     :props-initially-expanded="true">
                 <template #markdown="{ content }">
-                    <MDCRenderer :value="content">
-                        <template #default="mdcProps">
-                            <pre v-if="mdcProps.error" style="color: white;">{{ mdcProps.error }}</pre>
-                            <ContentRenderer v-else class="markdown" :value="mdcProps?.body"/>
-                        </template>
-                    </MDCRenderer>
+                    <MDCRenderer v-if="docAst?.body" :body="content" :data="docAst.data" :key="docAst" :components="proseComponents" />
                 </template>
             </SchemaToHtml>
         </Suspense>
@@ -36,7 +28,8 @@
 </template>
 
 <script lang="ts" setup>
-import { MDCRenderer, PluginIndex, SchemaToHtml } from '@kestra-io/ui-libs';
+import { computed, onMounted, ref } from 'vue';
+import { getMDCParser, MDCRenderer, PluginIndex, SchemaToHtml } from '@kestra-io/ui-libs';
 import { getPluginsWithoutDeprecated } from '../../src/utils/plugins/getListOfPlugins';
 import FeatureScopeMarker from '../docs/FeatureScopeMarker.vue';
 
@@ -54,5 +47,25 @@ const props = withDefaults(defineProps<{
     subGroup: undefined
 });
 
-const pluginsWithoutDeprecated = getPluginsWithoutDeprecated(props.plugins || []);
+const docAst = ref<any>();
+onMounted(async () => {
+    if (props.page?.body?.mdc) {
+        const parse = await getMDCParser();
+        docAst.value = await parse(props.page.body.mdc);
+    }
+});
+
+const pluginsWithoutDeprecated = computed(() => getPluginsWithoutDeprecated(props.plugins || []) as any[]);
+
+const proseComponents = {}
+
+// const proseComponents = Object.fromEntries([
+//         ...Object.keys(getCurrentInstance()?.appContext.components ?? {})
+//             .filter(name => name.startsWith("Prose"))
+//             .map(name => name.substring(5).replaceAll(/(.)([A-Z])/g, "$1-$2").toLowerCase())
+//             .map(name => [name, "prose-" + name]),
+//         ["a", ContextDocsLink],
+//         ["ChildCard", ContextChildCard],
+//         ["ChildTableOfContents", ContextChildTableOfContents]
+//     ]);
 </script>
