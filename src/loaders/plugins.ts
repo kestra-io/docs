1193b10b6d6fb968293afeb6b7140a122aa60c83
Bart Ledoux <bledoux@kestra.io>
2025-09-24 08:02:47 +0200

    feat: add plugins collection

---
+++ src/loaders/plugins.ts
@@ -0,0 +1,33 @@
+import type { Loader } from "astro/loaders";
+import { $fetch } from "../utils/fetch";
+import type { Plugin } from "@kestra-io/ui-libs";
+import { z } from "astro:content";
+
+export default {
+    name: "plugins",
+    async load({ store }){
+        const plugins = await $fetch<Plugin[]>("https://api.kestra.io/v1/plugins/subgroups")
+        store.clear()
+        for(const plugin of plugins) {
+            const id = plugin.name
+            store.set({
+                id,
+                data: plugin,
+            })
+        }
+    },
+    schema: z.object({
+        name: z.string(),
+        title: z.string(),
+        group: z.string(),
+        longDescription: z.string().optional(),
+        description: z.string().optional(),
+        subGroup: z.string().optional(),
+        tooltipContent: z.string().optional(),
+        categories: z.array(z.string()).optional(),
+        controllers: z.array(z.string()).optional(),
+        storages: z.array(z.string()).optional(),
+        aliases: z.array(z.string()).optional(),
+        guides: z.array(z.string()).optional(),
+    })
+} satisfies Loader
