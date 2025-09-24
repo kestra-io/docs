70f3c55026200a31b44dbe3598421c6588e0d5d6
Bart Ledoux <bledoux@kestra.io>
2025-09-24 11:50:05 +0200

    feat: plugins display proper icon

--- src/utils/plugins/getPluginIcon.ts
+++ src/utils/plugins/getPluginIcon.ts
@@ -1,24 +1,28 @@
 import { $fetch } from "../fetch";
 
-export async function getIcon(pluginType?: string, group?:string, subGroup?: string) {
-    const originalIcons = await $fetch(`https://api.kestra.io/v1/plugins/${pluginType}/icons/subgroups`);
-    const elementIcons = await $fetch(`https://api.kestra.io/v1/plugins/${pluginType}/icons`);
+function colorFixedB64Icon(b64Icon: string) {
+    return Buffer.from(Buffer.from(b64Icon, 'base64').toString('utf-8').replaceAll("currentColor", "#CAC5DA")).toString('base64');
+}
+
+export async function getIcon(pluginName: string, pluginType?: string, group?:string, subGroup?: string) {
+    const originalIcons = await $fetch(`https://api.kestra.io/v1/plugins/${pluginName}/icons/subgroups`);
+    const elementIcons = await $fetch(`https://api.kestra.io/v1/plugins/${pluginName}/icons`);
     const icons = {...originalIcons, ...elementIcons}
     let icon;
     if (pluginType !== undefined) {
         icon = icons[pluginType];
         if (icon === undefined) {
-            icon = Object.entries(icons).filter(([key]) => pluginType?.includes(key))
-                .sort(([key1], [key2]) => key2.length - key1.length)?.[0]?.[1];
+            const filteredIcons = Object.entries(icons).filter(([key]) => pluginType?.includes(key))
+            icon = filteredIcons.sort(([key1], [key2]) => key2.length - key1.length)?.[0]?.[1];
         }
-    } else if (subGroup === undefined && group) {
-        icon = icons[group];
-    } else if(subGroup) {
+    } else if (subGroup) {
         icon = icons[subGroup];
+    } else if (group) {
+        icon = icons[group];
     }
 
     return {
-        pageIcon: `data:image/svg+xml;base64,${icon}`,
+        pageIcon: icon ? `data:image/svg+xml;base64,${colorFixedB64Icon(icon.icon)}` : undefined,
         icons
     };
 }
