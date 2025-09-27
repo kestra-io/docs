f7a4af835c41268249593dd3326bbc620a4cb0ef
Bart Ledoux <bledoux@kestra.io>
2025-09-27 09:48:09 +0100

    better component definition

--- src/_main.ts
+++ src/_main.ts
@@ -1,8 +1,8 @@
 import type { App } from 'vue';
-import {h} from 'vue';
+import {defineComponent, h} from 'vue';
 
 export default (app: App) => {
   // Register global NuxtLink as the standard <a href> tag
-  app.component('NuxtLink', (props, { slots }) => h('a', { href: typeof props.to === 'string' ? props.to : props.href }, slots.default?.()))
-  app.component('NuxtImg', () => h('img'))
+  app.component('NuxtLink', defineComponent((props, { slots }) => () => h('a', { href: typeof props.to === 'string' ? props.to : props.href }, slots.default?.())))
+  app.component('NuxtImg', defineComponent(() => () => h('img')))
 };
