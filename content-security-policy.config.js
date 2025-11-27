59a41e37072b438777fe6ad94e22c5324ae2cea8
Gilles Perreymond <gperreymond@gmail.com>
2025-11-27 09:35:37 +0100

    chore: add csp for cloudflare rocket loader

--- content-security-policy.config.js
+++ content-security-policy.config.js
@@ -12,9 +12,12 @@ export default {
     // scripts
     'script-src': [
         "'self'",
+        "'ajax.cloudflare.com'",
         "'nonce-{{nonce}}'",
         "'strict-dynamic'",
         "'wasm-unsafe-eval'", 
+        "'unsafe-inline'",
+        "'static.cloudflareinsights.com'",
         "https://cdn.cr-relay.com",
         "https://*.kestra-io.pages.dev",
         "https://*.hs-analytics.net",
@@ -57,6 +60,7 @@ export default {
     ],
     'connect-src': [
         "'self'",
+        "'cloudflareinsights.com'",
         "ws://localhost:4000",
         "https://api.kestra.io",
         "https://kestra.io",
