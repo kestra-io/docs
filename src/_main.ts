21a9e8dbc621586b0ff1259a8005486c560a3adc
Bart Ledoux <bledoux@kestra.io>
2025-12-03 13:09:08 +0100

    some small adjustments

--- src/_main.ts
+++ src/_main.ts
@@ -3,7 +3,10 @@ import {defineComponent, h} from 'vue';
 
 export default (app: App) => {
   // Register global NuxtLink as the standard <a href> tag
-  app.component('NuxtLink', defineComponent((props, { slots }) => () => h('a', { href: typeof props.to === 'string' ? props.to : props.href }, slots.default?.())))
+  app.component('NuxtLink', defineComponent({
+    setup: (props, { slots }) => () => h('a', { href: props.to ?? props.href }, slots.default?.()),
+    props: ['to', 'href']
+  }));
   app.component('NuxtImg', defineComponent(() => () => h('img')))
   app.component('ClientOnly', defineComponent((_, { slots }) => () => slots.default?.()))
 };
