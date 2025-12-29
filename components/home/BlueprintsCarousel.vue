576126be4ccf498cc08fb942e56c8d6afe6e0575
Cbz <cbz704150@gmail.com>
2025-12-29 19:53:26 +0530

    fix: tags casing to match API response #3548

--- components/home/BlueprintsCarousel.vue
+++ components/home/BlueprintsCarousel.vue
@@ -3,7 +3,7 @@
         <button class="navigation navigation-left" @click="scrollLeft"><ArrowLeftIcon/></button>
         <button class="navigation navigation-right" @click="scrollRight"><ArrowRightIcon/></button>
         <div class="blueprints-carousel" ref="wrapper">
-            <BlueprintsListCard v-for="blueprint in blueprints" :key="blueprint.id" :blueprint="blueprint" :href="`/blueprints/${blueprint.id}`"/>
+            <BlueprintsListCard v-for="blueprint in blueprints" :key="blueprint.id" :blueprint="blueprint" :tags="tagsList" :href="`/blueprints/${blueprint.id}`"/>
         </div>
     </div>
 </template>
@@ -15,6 +15,13 @@ import ArrowRightIcon from "vue-material-design-icons/ArrowRight.vue"
 defineProps<{
     blueprints: {id: string}[]
 }>()
+
+// Fetch tags API
+const config = useRuntimeConfig();
+const { data: tags } = await useAsyncData<Array<{id: string, name: string}>>('blueprints-tags-carousel', () => {
+    return $fetch(`${config.public.apiUrl}/blueprints/versions/latest/tags`)
+});
+const tagsList = computed(() => tags.value ?? []);
 
 const wrapper = ref<HTMLElement | null>(null)
 
