e7ce12e7f11880b954a011be436e462ae740f60a
Ludovic DEHON <tchiot.ludo@gmail.com>
2026-01-06 19:57:08 +0100

    fix(site): fix careers page

--- components/careers/Header.vue
+++ components/careers/Header.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="header-container">
         <div class="header container d-flex flex-column align-items-center gap-3">
-            <h1 data-aos="fade-left">Join our <span>Team</span></h1>
-            <h4 data-aos="fade-right">Drive innovation in a globally distributed, collaborative environment</h4>
-            <NuxtLink :href="(fullPath === '/careers' ? '' : '/careers') + '#positions'" class="btn btn-animated btn-purple-animated" data-aos="zoom-in">
-                Open positions
-            </NuxtLink>
+            <slot>
+                <h1 data-aos="fade-left">Join our <span>Team</span></h1>
+                <h4 data-aos="fade-right">Drive innovation in a globally distributed, collaborative environment</h4>
+                <a :href="(fullPath === '/careers' ? '' : '/careers') + '#positions'" class="btn btn-animated btn-purple-animated" data-aos="zoom-in">
+                    Open positions
+                </a>
+            </slot>
         </div>
     </div>
 </template>
@@ -20,14 +22,14 @@
             padding-bottom: calc($spacer * 4.125);
             border-bottom: 1px solid rgba(255, 255, 255, 0.10);
 
-            h1, h4 {
+            :deep(h1), :deep(h4) {
                 color: $white;
                 text-align: center;
                 font-weight: 400;
                 margin-bottom: 0;
             }
 
-            h1 {
+            :deep(h1) {
                 font-size: $font-size-4xl;
 
                 @include media-breakpoint-down(sm) {
@@ -42,7 +44,7 @@
                 }
             }
 
-            h4 {
+            :deep(h4) {
                 font-size: $font-size-xl;
 
                 @include media-breakpoint-down(sm) {
