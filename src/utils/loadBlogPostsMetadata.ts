8dcf7cd9479900f00aeb3c428a47df6010c35160
Bart Ledoux <bledoux@kestra.io>
2026-01-09 17:27:16 +0100

    fix: bring back astro blogposts

--- src/utils/loadBlogPostsMetadata.ts
+++ src/utils/loadBlogPostsMetadata.ts
@@ -1,10 +1,13 @@
 import {promises as fs} from 'fs';
+import yaml from 'js-yaml';
 import type { BlogPost } from '~/components/common/BlogList.vue';
+import generateId from '~/utils/generateId';
 
 const __dirname = new URL('.', import.meta.url).pathname;
 export default async function loadBlogPostsMetadata() {
+    const blogsDir = `${__dirname}/../../content/blogs`
   // first retrieve all blog posts file paths
-  const files = await fs.readdir(`${__dirname}/../../content/blogs`)
+  const files = await fs.readdir(blogsDir)
   const blogPostsMetadata = await Promise.all(files.map(async (file) => {
     // for each file, read its content
     const content = await fs.readFile(`${__dirname}/../../content/blogs/${file}`, 'utf-8');
@@ -12,22 +15,12 @@ export default async function loadBlogPostsMetadata() {
     const match = content.match(/---\n([\s\S]*?)\n---/);
     if (match) {
       const metadataRaw = match[1];
-      const metadataLines = metadataRaw.split('\n');
       // finally extract the metadata key-value pairs
-      // from the TomL-like frontmatter
-      const metadata = metadataLines.reduce<Omit<BlogPost, "date"> & {date: Date}>((acc: any, line) => {
-        const [key, ...rest] = line.split(':');
-        if(key.trim() === "date") {
-          acc["date"] = new Date(rest.join(':').trim());
-          return acc;
-        }
-        acc[key.trim()] = rest.join(':').trim();
-        return acc;
-      }, {} as Omit<BlogPost, "date"> & {date: Date}
-    )
+      // from the YAML-like frontmatter
+      const metadata = yaml.load(metadataRaw) as unknown as Omit<BlogPost, 'date'> & { date: Date };
       return {
         data: metadata,
-        id: file.replace('.md', ''),
+        id: generateId({entry: file}),
       };
     }
     return null;
