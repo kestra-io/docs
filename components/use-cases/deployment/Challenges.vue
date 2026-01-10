2918b21a4a0e2dd6d28bee7a24ed0f3e48f84b07
Ludovic DEHON <tchiot.ludo@gmail.com>
2026-01-10 23:58:39 +0100

    feat(site): missing head title & desc

--- components/use-cases/deployment/Challenges.vue
+++ components/use-cases/deployment/Challenges.vue
@@ -28,63 +28,27 @@
 </template>
 
 <script setup lang="ts">
-    import RefreshAuto from "vue-material-design-icons/RefreshAuto.vue";
-    import CommentQuoteOutline from "vue-material-design-icons/CommentQuoteOutline.vue";
-    import ChartBellCurve from "vue-material-design-icons/ChartBellCurve.vue";
-    import FileChartOutline from "vue-material-design-icons/FileChartOutline.vue";
-    import ImageFilterCenterFocus from "vue-material-design-icons/ImageFilterCenterFocus.vue";
     import Card from '../../card/Card.vue';
+
+
+    type CardDetail = {
+        title?: string;
+        description?: string;
+    };
 
     type CardItem = {
         icon?: string;
-        cardInfo?: any;
+        cardInfo?: CardDetail;
     };
 
     const props = withDefaults(defineProps<{
         title?: string;
         description?: string | undefined;
         titleHtml?: string;
+        cardsData: Array<CardItem>
     }>(), {
         description: undefined,
     });
-
-    const cardsData = [
-        {
-            icon: RefreshAuto,
-            cardInfo: {
-                title: 'Healthcare  Data Management',
-                description: 'Automates the integration and management of patient data from various sources, ensuring that records are always updated and accurate.'
-            },
-        },
-        {
-            icon: CommentQuoteOutline,
-            cardInfo: {
-                title: 'Genomic Data Processing',
-                description: 'Process large volumes of genomic data quickly and accurately. Kestra automates sequencing workflows and data analysis, significantly cutting down processing times and minimizing errors,'
-            },
-        },
-        {
-            icon: ChartBellCurve,
-            cardInfo: {
-                title: 'Regulatory Compliance',
-                description: 'Automate the generation of compliance reports, access logs, and data usage reports. Ensure  compliance with regulatory standards reducing the risk of breaches and improving data governance.'
-            },
-        },
-        {
-            icon: FileChartOutline,
-            cardInfo: {
-                title: 'Predictive Analytics',
-                description: ' Integrates your workflows with machine learning models and analytics platforms to utilize patient data for predictive analytics.'
-            },
-        },
-        {
-            icon: ImageFilterCenterFocus,
-            cardInfo: {
-                title: 'Streamlined Data Management',
-                description: 'Centralize your fragmented data workflow into a unified control plane, thereby simplifying orchestration and improving efficiency.'
-            },
-        },
-    ];
 </script>
 
 <style lang="scss" scoped>
