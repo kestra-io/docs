51442090664b022b8f6fd18047979bc1ee076f6a
Ludovic DEHON <tchiot.ludo@gmail.com>
2026-01-13 11:42:26 +0100

    feat(site): reintroduce cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { defineMiddleware } from "astro:middleware";
 import { sequence } from "astro/middleware";
 import { API_URL } from "astro:env/client";
+import contentSecurityPolicyConfig from "../content-security-policy.config";
 
 const sendRedirect = (redirectUrl: string) => {
     return new Response("", {
@@ -24,7 +25,7 @@ const logger = defineMiddleware(async (context, next) => {
         "method": context.request.method,
         "url": context.request.url,
         "status": response.status,
-        "ip": !context.isPrerendered ? context.request.headers.get('x-forwarded-for') : null,
+        "ip": !context.isPrerendered ? context.request.headers.get('x-real-ip') : null,
         "length": response.headers.get("content-length"),
         "route": context.routePattern,
         "routeParams": context.params,
@@ -95,12 +96,45 @@ const incomingRedirect = defineMiddleware(async (context, next) => {
     return next();
 });
 
-
 const securityHeaders = defineMiddleware(async (context, next) => {
     const response = await next();
 
-    response.headers.set("X-Frame-Options", "DENY");
-    response.headers.set("X-Content-Type-Options", "nosniff");
+    const localhost: string[] = []
+    if (import.meta.env.DEV) {
+        localhost.push(context.url.protocol + "//" + context.url.host);
+    }
+
+    const contentSecurityPolicy: string = Object.entries(contentSecurityPolicyConfig as Record<string, Array<string> | boolean>)
+        .filter(([key]) => import.meta.env.DEV && key !== "upgrade-insecure-requests")
+        .map(([key, value]) => {
+            let line = key;
+
+            if (typeof value !== "boolean") {
+                if (value.length === 1 && value[0] === "'none'") {
+                    line += " " + value.join(" ");
+                } else {
+                    line += " " + localhost.concat(value).join(" ")
+                }
+            }
+
+            return line;
+        })
+        .join("; ");
+
+    response.headers.set("x-frame-options", "DENY");
+    response.headers.set("x-content-type-options", "nosniff");
+    response.headers.set("x-download-options", "nosniff");
+    response.headers.set("access-control-allow-origin", context.url.protocol + "//" + context.url.host);
+    response.headers.set("cross-origin-opener-policy", "same-origin");
+    response.headers.set("cross-origin-resource-policy", "same-origin");
+    response.headers.set("permissions-policy", "camera=(), display-capture=(), fullscreen=(*), geolocation=(), microphone=()");
+    response.headers.set("referrer-policy", "strict-origin-when-cross-origin");
+    response.headers.set("x-permitted-cross-domain-policies", "none");
+    response.headers.set("content-security-policy", contentSecurityPolicy);
+
+    if (!import.meta.env.DEV) {
+        response.headers.set("strict-transport-security", "max-age=15552000");
+    }
 
     return new Response(await response.text(), {
         status: response.status,
