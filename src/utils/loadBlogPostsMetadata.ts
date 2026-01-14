6dba81fffb73fe4a8a85100f60f6669966a41506
Bart Ledoux <bledoux@kestra.io>
2026-01-14 13:16:14 +0100

    fix: faster build

--- src/utils/loadBlogPostsMetadata.ts
+++ src/utils/loadBlogPostsMetadata.ts
@@ -1,14 +1,24 @@
+import yaml from 'js-yaml';
 import type { BlogPost } from '~/components/common/BlogList.vue';
 import generateId from '~/utils/generateId';
 
 export default async function loadBlogPostsMetadata() {
-  const files = import.meta.glob('../../content/blogs/*.md', {eager: true});
+  const files = import.meta.glob("../../content/blogs/*.md", { eager: true, as: 'raw' });
   // first retrieve all blog posts file paths
-  const blogPostsMetadata = await Promise.all(Object.entries(files).map(async ([filePath, file]: [string, any]) => {
-    return {
-        id: generateId({entry: filePath.replace('../../content/blogs/', '')}),
-        data: file.frontmatter as BlogPost
+  const blogPostsMetadata = await Promise.all(Object.entries(files).map(async ([filePath, content]) => {
+    // extract frontmatter metadata from the markdown content
+    const match = content.match(/---\n([\s\S]*?)\n---/);
+    if (match) {
+      const metadataRaw = match[1];
+      // finally extract the metadata key-value pairs
+      // from the YAML-like frontmatter
+      const metadata = yaml.load(metadataRaw) as unknown as Omit<BlogPost, 'date'> & { date: Date };
+      return {
+        data: metadata,
+        id: generateId({entry: filePath}),
+      };
     }
+    return null;
   }));
   return blogPostsMetadata.filter(post => post !== null);
 }
