12d54963356395a703dca68a94a7984d3bd0004d
Bart Ledoux <bledoux@kestra.io>
2026-01-14 13:23:52 +0100

    use query raw

--- src/utils/loadBlogPostsMetadata.ts
+++ src/utils/loadBlogPostsMetadata.ts
@@ -3,7 +3,7 @@ import type { BlogPost } from '~/components/common/BlogList.vue';
 import generateId from '~/utils/generateId';
 
 export default async function loadBlogPostsMetadata() {
-  const files = import.meta.glob("../../content/blogs/*.md", { eager: true, as: 'raw' });
+  const files: Record<string, string> = import.meta.glob("../../content/blogs/*.md", { eager: true, query: '?raw', });
   // first retrieve all blog posts file paths
   const blogPostsMetadata = await Promise.all(Object.entries(files).map(async ([filePath, content]) => {
     // extract frontmatter metadata from the markdown content
