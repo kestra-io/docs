04c60dfe8003b04f1d10c4ed707eb0823f670935
Ludovic DEHON <tchiot.ludo@gmail.com>
2026-01-14 09:34:50 +0100

    fix(site): remove data folders

--- components/solutions/UseCases.vue
+++ components/solutions/UseCases.vue
@@ -29,7 +29,6 @@
 
 <script>
 import Section from "~/components/layout/Section.vue";
-import useCases from "~/data/use-cases.js";
 
 export default {
     components: { Section },
@@ -37,6 +36,35 @@ export default {
         ...useCases,
     }),
 };
+
+const useCases  = {
+    subtitle: "Find Out More About Kestra's",
+    subtitleAfter: "Capabilities",
+    cases: [
+        {
+            link: "/use-cases/change-data-capture",
+            img: "/landing/usecases/cdc/intro.svg",
+            title: "Change data capture",
+            description:
+                "Streamline Complex Data Pipelines and Automate Repetitive Tasks for Enhanced Efficiency",
+        },
+        {
+            link: "/use-cases/modern-data-stack",
+            img: "/landing/usecases/mds/intro.svg",
+            title: "Modern Data Stack Integration",
+            description:
+                "Integrate leading data tools from the Modern Data Stack with Kestra",
+        },
+        {
+            link: "/use-cases/ci-cd",
+            img: "/landing/usecases/cicd/intro.svg",
+            title: "CI/CD for your Kestra Workflows",
+            description:
+                "Embrace software engineering practices for versioning, deploying, and maintaining data workflows",
+        },
+    ],
+};
+
 </script>
 
 <style lang="scss" scoped>
