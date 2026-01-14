75abeb9f678f7ec015111c70371db426936beaaa
Bart Ledoux <bledoux@kestra.io>
2026-01-14 13:35:58 +0100

    fix plugin core

--- src/utils/loadBlogPostsMetadata.ts
+++ src/utils/loadBlogPostsMetadata.ts
@@ -3,10 +3,9 @@ import type { BlogPost } from '~/components/common/BlogList.vue';
 import generateId from '~/utils/generateId';
 
 export default async function loadBlogPostsMetadata() {
-  const files: Record<string, string> = import.meta.glob("../../content/blogs/*.md", { eager: true, query: '?raw', });
+  const files: Record<string, string> = import.meta.glob("../../content/blogs/*.md", { eager: true, query: '?raw', import: 'default' });
   // first retrieve all blog posts file paths
   const blogPostsMetadata = await Promise.all(Object.entries(files).map(async ([filePath, content]) => {
-    // extract frontmatter metadata from the markdown content
     const match = content.match(/---\n([\s\S]*?)\n---/);
     if (match) {
       const metadataRaw = match[1];
