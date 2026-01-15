c4c6bc657462557d40ca808e74f1921b8717c52f
Bart Ledoux <bledoux@kestra.io>
2026-01-15 14:13:14 +0100

    type the docs metadata properly

--- src/utils/loadBlogPostsMetadata.ts
+++ src/utils/loadBlogPostsMetadata.ts
@@ -1,9 +1,9 @@
 import type { CollectionEntry } from 'astro:content';
 import yaml from 'js-yaml';
 import generateId from '~/utils/generateId';
+const files: Record<string, string> = import.meta.glob("../../content/blogs/*.md", { eager: true, query: '?raw', import: 'default' });
 
 export default async function loadBlogPostsMetadata() {
-  const files: Record<string, string> = import.meta.glob("../../content/blogs/*.md", { eager: true, query: '?raw', import: 'default' });
   // first retrieve all blog posts file paths
   const blogPostsMetadata = await Promise.all(Object.entries(files).map(async ([filePath, content]) => {
     const match = content.match(/---\n([\s\S]*?)\n---/);
---
+++ src/utils/loadDocsMetadata.ts
@@ -0,0 +1,23 @@
+import type { CollectionEntry } from 'astro:content';
+import yaml from 'js-yaml';
+import generateId from '~/utils/generateId';
+const files: Record<string, string> = import.meta.glob("../../content/docs/**/*.md", { eager: true, query: '?raw', import: 'default' });
+
+export default async function loadDocsMetadata() {
+  // first retrieve all blog posts file paths
+  const docsMetadata = await Promise.all(Object.entries(files).map(async ([filePath, content]) => {
+    const match = content.match(/---\n([\s\S]*?)\n---/);
+    if (match) {
+      const metadataRaw = match[1];
+      // finally extract the metadata key-value pairs
+      // from the YAML-like frontmatter
+      const metadata = yaml.load(metadataRaw) as unknown as unknown as CollectionEntry<"docs">["data"];
+      return {
+        data: metadata,
+        id: generateId({entry: filePath.slice(24)}), // remove ../../content/docs/ from the path
+      };
+    }
+    return null;
+  }));
+  return docsMetadata.filter(doc => doc !== null);
+}
