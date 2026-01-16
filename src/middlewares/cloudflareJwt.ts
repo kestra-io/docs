43c4ef55552edba48926348abb1efe8a32f3942a
Ludovic DEHON <tchiot.ludo@gmail.com>
2026-01-17 00:07:19 +0100

    add jwt verification

--- src/middlewares/cloudflareJwt.ts
+++ src/middlewares/cloudflareJwt.ts
@@ -3,56 +3,66 @@ import {CLOUDFLARE_TEAM_DOMAIN} from "astro:env/server";
 import {CLOUDFLARE_POLICY_AUD} from "astro:env/server";
 import {defineMiddleware} from "astro:middleware";
 
+const sendError = (body: string): Response => {
+    return new Response(body, {
+        status: 403,
+        headers: { "Content-Type": "text/plain" },
+    });
+}
 
-const fetch = async (request: Request): Promise<Response | null> => {
+const fetch = async (request: Request, jwtAssertionToken: string | null): Promise<Response | null> => {
     // Verify the POLICY_AUD environment variable is set
     if (!CLOUDFLARE_POLICY_AUD) {
-        return new Response("Missing required audience", {
-            status: 403,
-            headers: { "Content-Type": "text/plain" },
-        });
+        return sendError("Missing required audience");
     }
 
     // Verify the POLICY_AUD environment variable is set
     if (!CLOUDFLARE_TEAM_DOMAIN) {
-        return new Response("Missing required team domain", {
-            status: 403,
-            headers: { "Content-Type": "text/plain" },
-        });
+        return sendError("Missing required team domain");
     }
 
     // Get the JWT from the request headers
-    const token = request.headers.get("cf-access-jwt-assertion");
-
     // Check if token exists
-    if (!token) {
-        return new Response("Missing required CF Access JWT", {
-            status: 403,
-            headers: { "Content-Type": "text/plain" },
-        });
+    if (!jwtAssertionToken) {
+        return sendError("Missing required CF Access JWT");
     }
 
-    try {
-        // Create JWKS from your team domain
-        const JWKS = createRemoteJWKSet(
-            new URL(`${CLOUDFLARE_TEAM_DOMAIN}/cdn-cgi/access/certs`)
-        );
 
-        // Verify the JWT
-        const { payload } = await jwtVerify(token, JWKS, {
-            issuer: CLOUDFLARE_TEAM_DOMAIN,
-            audience: CLOUDFLARE_POLICY_AUD,
-        });
+    // Create JWKS from your team domain
+    let JWKS = null;
+    try {
+        JWKS = createRemoteJWKSet(new URL(`${CLOUDFLARE_TEAM_DOMAIN}/cdn-cgi/access/certs`));
+    } catch (error) {
+        // Token verification failed
+        const message = error instanceof Error ? error.message : "Unknown error";
+        const full = error instanceof Error ? JSON.stringify(error) : "";
+        const debug = JSON.stringify({CLOUDFLARE_TEAM_DOMAIN, CLOUDFLARE_POLICY_AUD, jwtAssertionToken});
+        const stacktrace = error instanceof Error ? error.stack : "";
+
+        return sendError(`Invalid createRemoteJWKSet: ${message}\n${full}\n${debug}\n${stacktrace}`);
+    }
+
+    // Verify the JWT
+    try {
+        await jwtVerify(
+            jwtAssertionToken,
+            JWKS,
+            {
+                issuer: CLOUDFLARE_TEAM_DOMAIN,
+                audience: CLOUDFLARE_POLICY_AUD,
+            }
+        );
 
         // Token is valid, proceed with your application logic
         return null;
     } catch (error) {
         // Token verification failed
         const message = error instanceof Error ? error.message : "Unknown error";
-        return new Response(`Invalid token: ${message}`, {
-            status: 403,
-            headers: { "Content-Type": "text/plain" },
-        });
+        const full = error instanceof Error ? JSON.stringify(error) : "";
+        const debug = JSON.stringify({CLOUDFLARE_TEAM_DOMAIN, CLOUDFLARE_POLICY_AUD, jwtAssertionToken});
+        const stacktrace = error instanceof Error ? error.stack : "";
+
+        return sendError(`Invalid jwtVerify: ${message}\n${full}\n${debug}\n${stacktrace}`);
     }
 }
 
@@ -62,7 +72,7 @@ export default defineMiddleware(async (context, next) => {
         return next();
     }
 
-    const response = await fetch(context.request);
+    const response = await fetch(context.request, context.request.headers.get("cf-access-jwt-assertion"));
 
     if (response) {
         return response;
