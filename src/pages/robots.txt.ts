9055aba5dfa991795c722eb63c4c147815f3c023
Bart Ledoux <bledoux@kestra.io>
2026-01-22 16:13:01 +0100

    fix: robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,7 +1,8 @@
 import type { APIRoute } from "astro"
+import * as envField from "astro:env/server"
 
-export const GET: APIRoute = async (context) => {
-    const disabled = import.meta.env.DEV || context.url.hostname === "kestra.io"
+export const GET: APIRoute = async () => {
+    const disabled = import.meta.env.DEV || envField.PREVIEW
 
     const result = `# indexing ${disabled ? "disabled" : "enabled"}
 
