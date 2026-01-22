1946272e3ff0de21aa33664b93084314dcaaca15
Bart Ledoux <bledoux@kestra.io>
2026-01-22 17:00:57 +0100

    remove vue3-count-to

--- astro.config.mjs
+++ astro.config.mjs
@@ -169,11 +169,7 @@ export default defineConfig({
                 },
             },
         },
-        optimizeDeps: {
-            include: ["vue3-count-to"],
-        },
         ssr: {
-            noExternal: ["vue3-count-to"],
             external: [
                 "node:fs/promises",
                 "node:fs",
