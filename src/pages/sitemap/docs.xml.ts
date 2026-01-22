804ab66c4dd12c257e786ca7d3cc6972dd09fadf
Ludovic DEHON <tchiot.ludo@gmail.com>
2026-01-22 20:56:14 +0100

    fix: sitemap for doc was invalid

--- src/pages/sitemap/docs.xml.ts
+++ src/pages/sitemap/docs.xml.ts
@@ -4,7 +4,13 @@ import { sitemapResponse } from "~/utils/sitemap.ts"
 
 export const GET: APIRoute = async () => {
     const allBlogPosts = await getCollection("docs")
-    const urls = allBlogPosts.map((content) => `https://kestra.io/docs/${content.id}`)
+    const urls = allBlogPosts.map(
+        (content) => {
+            const page = content.id.replace("<index>", "")
+
+            return `https://kestra.io/docs${page ? "/" + page : ""}`
+        },
+    )
 
     return sitemapResponse(urls)
 }
