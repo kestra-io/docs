3ad7495d1b6c03d4c2684cae95987b791033128b
Ludovic DEHON <tchiot.ludo@gmail.com>
2026-01-24 11:45:43 +0100

    fix: finally the good one for posthog

--- src/pages/t/[...path].ts
+++ src/pages/t/[...path].ts
@@ -2,9 +2,10 @@ export const prerender = false
 
 import type { APIRoute } from "astro"
 
+const proxyDomain = new URL("https://eu.posthog.com/")
+const staticProxyDomain = new URL("https://eu-assets.i.posthog.com/static/")
+
 export const ALL: APIRoute = async ({ request, clientAddress }) => {
-    const proxyDomain = new URL("https://eu.i.posthog.com/")
-    const staticProxyDomain = new URL("https://eu-assets.i.posthog.com/static/")
     const requestUrl = new URL(request.url)
     const realUrl = requestUrl.pathname.substring(2, requestUrl.pathname.length)
 
@@ -12,6 +13,7 @@ export const ALL: APIRoute = async ({ request, clientAddress }) => {
         realUrl,
         realUrl.startsWith("/static") ? staticProxyDomain : proxyDomain,
     )
+
 
     // Forward headers, excluding ones that shouldn't be proxied
     const headers = new Headers()
@@ -21,16 +23,18 @@ export const ALL: APIRoute = async ({ request, clientAddress }) => {
         "content-length",
         "transfer-encoding",
         "accept-encoding",
+        "cookie",
     ]
 
-    for (const [key, value] of Object.entries(request.headers)) {
-        if (value && !excludedHeaders.includes(key.toLowerCase())) {
+    for (const [key, value] of request.headers.entries()) {
+        if (!excludedHeaders.includes(key.toLowerCase())) {
             headers.set(key, value)
         }
     }
 
     // Forward client IP for geolocation
     const clientIp =
+        request.headers.get("CF-Connecting-IP") ||
         request.headers.get("x-real-ip") ||
         request.headers.get("x-forwarded-for") ||
         clientAddress
@@ -39,9 +43,19 @@ export const ALL: APIRoute = async ({ request, clientAddress }) => {
         headers.set("x-forwarded-for", clientIp)
     }
 
-    const response = await fetch(proxyUrl.href, request);
+    const forwardedRequest = new Request(proxyUrl, {
+        headers: headers,
+        body: request.method !== "GET" && request.method !== "HEAD" ? await request.arrayBuffer() : null,
+        method: request.method,
+        credentials: request.credentials,
+        mode: request.mode,
+        redirect: request.redirect,
+        referrer: request.referrer
+    })
+
+    const response = await fetch(forwardedRequest)
 
     return new Response(response.body, {
-        headers: headers,
+        headers: response.headers,
     })
 }
