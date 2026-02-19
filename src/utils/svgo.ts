2857a2495f623cb14091fce79d5bdba9b1c5600b
Bart Ledoux <bledoux@kestra.io>
2026-02-19 10:17:57 +0100

    add comments to svgo params

--- src/utils/svgo.ts
+++ src/utils/svgo.ts
@@ -13,15 +13,21 @@ export function optimizeSvgIcon(svgInput: string, prefix: string) {
                     },
                 },
                 {
+                    // avoid height and width attributes because scaling is handled via CSS
                     name: "removeDimensions",
                 },
                 {
+                    // when using <svg> elements as icons, we want to ensure that IDs are unique
+                    // to prevent conflicts when multiple icons are used on the same page
+                    // ids are used for gradients and clip paths, so we can't remove them entirely
                     name: "prefixIds",
                     params: {
                         prefix: () => prefix,
                     }
                 }
             ]
-        // replace self closing tags with explicit closing tags to ensure compatibility with all browsers
+        // replace self closing tags with explicit closing tags to ensure
+        // compatibility with svg coming out of Figma
+        // contents of <foreignObject> in particular cannot be self closing
         }).data.replace(/<(\w+)([^>]*)\/>/g, "<$1$2></$1>")
     }
