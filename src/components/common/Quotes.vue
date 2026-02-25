aab1ca1912c3ad34fa9a4cca66d198627731e82c
Bart Ledoux <bledoux@kestra.io>
2026-02-25 15:03:05 +0100

    update styles

--- src/components/common/Quotes.vue
+++ src/components/common/Quotes.vue
@@ -20,12 +20,12 @@ const activeQuote = ref(0)
 </script>
 
 <template>
-    <div class="quote-wrapper">
-        <div class="quote-tabs" role="tablist">
+    <div class="wrapper">
+        <div class="tabs" role="tablist">
             <button
                 v-for="(quote, index) in quotes"
                 :key="quote.quote"
-                class="quote-tab"
+                class="tab"
                 @click="activeQuote = index"
                 v-html="quote.smallLogoSvg"
                 role="tab"
@@ -34,59 +34,137 @@ const activeQuote = ref(0)
                 :id="'quote-tab-' + index"
             />
         </div>
-        <div class="quote-content">
-            <div class="quote-text">
+        <div class="content">
+            <div class="text">
                 <blockquote>{{ quotes[activeQuote].quote }}</blockquote>
                 <span v-html="quotes[activeQuote].author" />
             </div>
+        </div>
+        <div v-if="quotes[activeQuote].kpis" class="kpis">
+            <div v-for="kpi in quotes[activeQuote].kpis" :key="kpi.name" class="kpi">
+                <strong>{{ kpi.value }}</strong>
+                <span>{{ kpi.name }}</span>
+            </div>
+        </div>
+        <div class="navigation">
+            <a v-if="quotes[activeQuote].storyLink" :href="quotes[activeQuote].storyLink" target="_blank" rel="noopener noreferrer">
+                Read the Story
+            </a>
+            <button @click="() => { if(activeQuote > 0) {activeQuote--} }">&lt;</button>
+            <button @click="() => { if(activeQuote < quotes.length - 1) {activeQuote++} }">&gt;</button>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.quote-wrapper {
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
+.wrapper {
     background-color: var(--ks-background-purple-light);
     color: var(--ks-content-primary);
-    .quote-tabs {
+    .tabs {
         display: flex;
         justify-content: center;
         border-bottom: 1px solid var(--ks-border-secondary);
-        .quote-tab{
+        margin: 0 auto;
+        .tab{
             background: none;
             border: none;
             cursor: pointer;
-            padding: 0.5rem;
+            padding: .8rem 0.5rem;
             display: flex;
-            max-width: 300px;
+            max-width: 20%;
             align-items: center;
             justify-content: center;
+            color: var(--ks-content-primary);
             flex: 1;
-            svg {
-                width: 2rem;
-                height: 2rem;
+            :global(svg) {
+                height: 1.5rem;
                 fill: var(--ks-content-primary);
                 transition: fill 0.3s ease;
             }
-            &[aria-selected='true'] {
+            &[aria-selected='true'], &:hover {
                 background-color: var(--ks-background-purple-hover);
+                border-bottom: 1px solid var(--ks-border-active);
+                margin-bottom: -1px;
             }
         }
     }
-    .quote-content{
+
+    .content{
         text-align: center;
-        margin: 4rem;
+        padding: 4rem;
         blockquote{
+            font-size: 28px;
             &::after, &::before {
                 color: var(--ks-content-color-highlight);
-                content: '”';
-                font-size: 1.5rem;
-                line-height: 1rem;
-                margin: .5rem;
-                vertical-align: middle;
+                font-size: 2rem;
+                line-height: 2.5rem;
+                margin: .2rem;
+                vertical-align: bottom;
                 margin-left: 0.5rem;
+            }
+            &::before {
+                content: '“';
+            }
+            &::after {
+                content: '”';
+            }
+        }
+        > div > span{
+            font-family: var(--font-family-jetbrains-mono);
+            font-size: 1rem;
+        }
+    }
+
+    .kpis {
+        display: flex;
+        justify-content: stretch;
+        gap: 2rem;
+        border-top: 1px solid var(--ks-border-secondary);
+        width: 70%;
+        margin: 0 auto;
+        padding: 1rem 0;
+        .kpi {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            flex: 1;
+            strong {
+                font-size: 1.5rem;
+            }
+            span {
+                font-size: 0.7rem;
+                color: var(--ks-content-secondary);
+            }
+        }
+    }
+
+    .navigation {
+        display: flex;
+        justify-content: end;
+        gap: .5rem;
+        align-items: center;
+        font-size: .8rem;
+        padding: 1rem;
+        a, button {
+            color: var(--ks-content-primary);
+            border: 1px solid var(--ks-border-primary-inverse);
+            border-radius: 2px;
+            text-decoration: none;
+            font-size: .5rem;
+            padding: .15rem .6rem;
+            &:hover {
+                background: var(--ks-background-purple-hover);
+            }
+        }
+        a{
+            font-weight: bold;
+        }
+        button {
+            cursor: pointer;
+            background: none;
+            &:disabled {
+                color: var(--ks-content-secondary);
+                cursor: not-allowed;
             }
         }
     }
