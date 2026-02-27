7fec1b62c0c7980e60c2526a9ae853d48a27eb52
Bart Ledoux <bledoux@kestra.io>
2026-02-27 17:27:16 +0100

    fix: update the demo page

--- src/components/demo/Meeting.vue
+++ src/components/demo/Meeting.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref, useTemplateRef } from "vue"
+import { onMounted, ref, useTemplateRef } from "vue"
 import axios from "axios"
 import posthog from "posthog-js"
 import { useGtm } from "@gtm-support/vue-gtm"
@@ -103,6 +103,7 @@ const onSubmit = async (e: Event) => {
                 })
                 .catch((error) => {
                     valid.value = false
+                    console.error("Error submitting form data to HubSpot", error)
                     if (
                         error?.response?.data?.errors?.some?.(
                             (e: any) => e.errorType === "BLOCKED_EMAIL",
@@ -147,17 +148,47 @@ function withContactParams(
         return `${base}${sep}${qp.toString()}`
     }
 }
+
+onMounted(() => {
+    if (getHubspotTracking() === null) {
+        const base = getMeetingUrl()
+        const current = new URLSearchParams(window.location.search)
+        meetingUrl.value = withContactParams(base, {
+            firstname: current.get("firstname"),
+            lastname: current.get("lastname"),
+            email: current.get("email"),
+        })
+        valid.value = true
+    }
+})
 </script>
 
 <template>
     <div class="col-12 col-lg-6 align-items-center d-flex meeting-container">
-        <div v-if="valid === false" class="meeting-form">
-            <img
+        <div v-if="valid" class="custom-meetings-iframe-container embed-responsive">
+            <div class="iframe-wrapper">
+                <iframe
+                    v-if="meetingUrl"
+                    :src="meetingUrl"
+                    class="embed-responsive-item"
+                    style="
+                        min-height: 750px;
+                        min-width: 350px;
+                        border: none;
+                        width: 100%;
+                    "
+                    allowtransparency="true"
+                    sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+                />
+            </div>
+        </div>
+        <div v-else class="meeting-form">
+            <!-- <img
                 class="background d-none d-md-block"
                 :src="headerBackground.src"
                 aria-hidden="true"
                 data-usal="fade-r"
-            />
+            /> -->
             <form
                 class="row needs-validation"
                 ref="demo-form"
@@ -225,23 +256,158 @@ function withContactParams(
                 </div>
             </form>
         </div>
-
-        <div v-else class="custom-meetings-iframe-container embed-responsive">
-            <div class="iframe-wrapper">
-                <iframe
-                    v-if="meetingUrl"
-                    :src="meetingUrl"
-                    class="embed-responsive-item"
-                    style="
-                        min-height: 750px;
-                        min-width: 350px;
-                        border: none;
-                        width: 100%;
-                    "
-                    allowtransparency="true"
-                    sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
-                />
-            </div>
-        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+@import "~/assets/styles/variable";
+.meeting-container {
+    position: relative;
+    background-color: var(--ks-background-secondary);
+    .alert.alert-danger{
+        color: var(--ks-content-alert-danger)
+    }
+    @include media-breakpoint-up(md) {
+        padding: 0;
+        &::after,
+        &::before {
+            position: absolute;
+            content: "";
+            z-index: -2;
+            width: 30.6rem;
+            height: 15.6rem;
+
+            filter: blur(95px);
+        }
+        &::after {
+            right: 10rem;
+            bottom: 7.375rem;
+        }
+        &::before {
+            left: 16.3rem;
+            top: 4rem;
+        }
+    }
+    @include media-breakpoint-down(xxl) {
+        &::after,
+        &::before {
+            width: 10rem;
+            height: 10rem;
+        }
+        &::after {
+            right: 21rem;
+            bottom: 12.375rem;
+        }
+        &::before {
+            left: 21rem;
+        }
+    }
+    @include media-breakpoint-down(lg) {
+        &::after {
+            right: 12rem;
+        }
+        &::before {
+            left: 12rem;
+        }
+    }
+    @include media-breakpoint-down(md) {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .custom-meetings-iframe-container {
+        width: 100%;
+        position: relative;
+        @include media-breakpoint-down(md) {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        &::after,
+        &::before {
+            position: absolute;
+            content: "";
+            z-index: -2;
+            width: 15.6rem;
+            height: 24.6rem;
+
+            filter: blur(50px);
+        }
+        &::after {
+            left: 25.6rem;
+            bottom: 3.87rem;
+        }
+        &::before {
+            right: 22.875rem;
+            top: 1.625rem;
+        }
+        @include media-breakpoint-down(xxl) {
+            &::after {
+                left: 13.6rem;
+                bottom: 8.87rem;
+            }
+            &::before {
+                right: 12.875rem;
+            }
+        }
+        @include media-breakpoint-down(lg) {
+            &::after {
+                left: 7.6rem;
+            }
+            &::before {
+                right: 6.8rem;
+            }
+        }
+        @include media-breakpoint-down(md) {
+            .iframe-wrapper {
+                margin-bottom: -3.5rem;
+            }
+            &::after {
+                left: 6rem;
+            }
+            &::before {
+                right: 14rem;
+            }
+        }
+    }
+    .meeting-form {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        img.background {
+            width: 644px;
+            max-width: 100%;
+            z-index: 0;
+            position: absolute;
+            z-index: 1;
+            opacity: 0.2;
+        }
+        form {
+            border-radius: 0.25rem;
+            padding: 0 1rem 2rem 1rem;
+            width: 85%;
+            border: 1px solid var(--ks-border-primary);
+            background: white;
+            @include media-breakpoint-up(lg) {
+                width: 75%;
+            }
+            @include media-breakpoint-down(md) {
+                position: relative;
+                top: auto;
+                width: 100%;
+                border-radius: 0.5rem;
+            }
+            .form-label {
+                opacity: 0;
+            }
+        }
+        @include media-breakpoint-down(md) {
+            margin: 2rem 0;
+            width: 100%;
+            .col-12 {
+                margin-bottom: -12px;
+            }
+        }
+    }
+}
+</style>
