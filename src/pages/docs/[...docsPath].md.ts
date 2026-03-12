09ade308f07e5044b71ad14cc3c4cfb0e1d2cdbf
Bart Ledoux <bledoux@kestra.io>
2026-03-12 14:11:31 +0100

    add title to docsPath

--- src/pages/docs/[...docsPath].md.ts
+++ src/pages/docs/[...docsPath].md.ts
@@ -5,7 +5,10 @@ export async function getStaticPaths() {
     const docsPages = await getCollection("docs")
     return docsPages.map((doc) => ({
         params: { docsPath: doc.id },
-        props: { source: doc.body },
+        props: {
+            title: doc.data.title,
+            source: doc.body,
+        },
     }))
 }
 
@@ -14,7 +17,7 @@ export async function getStaticPaths() {
  * to be used by AI or other tools that want to consume the markdown content directly.
  */
 export const GET: APIRoute = ({ props }) => {
-    return new Response(props.source, {
+    return new Response(`# ${props.title}\n\n${props.source}`, {
         status: 200,
         headers: {
             "Content-Type": "text/markdown",
