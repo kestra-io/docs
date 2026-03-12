3029f71116c00c31d6836d6fbed64cf34f397658
Bart Ledoux <bledoux@kestra.io>
2026-03-12 12:28:01 +0100

    feat: add API route to serve raw markdown content for documentation

---
+++ src/pages/docs/[...docsPath].md.ts
@@ -0,0 +1,23 @@
+import type { APIRoute } from "astro"
+import { getCollection } from "astro:content"
+
+export async function getStaticPaths() {
+    const docsPages = await getCollection("docs")
+    return docsPages.map((doc) => ({
+        params: { docsPath: doc.id },
+        props: { source: doc.body },
+    }))
+}
+
+/**
+ * respond with the raw markdown content of the doc page.
+ * to be used by AI or other tools that want to consume the markdown content directly.
+ */
+export const GET: APIRoute = ({ props }) => {
+    return new Response(props.source, {
+        status: 200,
+        headers: {
+            "Content-Type": "text/markdown",
+        },
+    })
+}
