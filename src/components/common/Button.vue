3bf49f6d65fb2e17ccee2bd79c8dfe2bed55188b
Pradumna Saraf <pradumnasaraf@gmail.com>
2026-03-13 14:28:52 +0530

    fix(docs): restore visible welcome page button borders

--- src/components/common/Button.vue
+++ src/components/common/Button.vue
@@ -1,8 +1,8 @@
 <template>
-    <a v-if="href" :href="href" :class="`btn btn-${type}`" @click="onClick">
+    <a v-if="href" :href="href" :class="['btn', `btn-${type}`]" @click="onClick">
         <slot></slot>
     </a>
-    <button v-else :class="`btn btn-${type}`" @click="onClick">
+    <button v-else :class="['btn', `btn-${type}`]" @click="onClick">
         <slot></slot>
     </button>
 </template>
@@ -13,28 +13,8 @@
         href?: string
         onClick?: (event: MouseEvent) => void
     }>(), {
-        type: "",
+        type: "secondary",
         href: undefined,
         onClick: () => {}
     })
 </script>
-
-<style lang="scss" scoped>
-
-
-    .btn {
-        border-radius: 0.25rem;
-        padding: 0.5rem 1rem;
-        text-decoration: none;
-        font-size: $font-size-sm;
-        font-size: $font-size-sm;
-        border: 1px solid var(--ks-border-primary);
-    }
-
-    .btn-primary {
-        background-color: var(--ks-background-button-primary);
-        color: var(--ks-content-primary);
-    }
-
-
-</style>
