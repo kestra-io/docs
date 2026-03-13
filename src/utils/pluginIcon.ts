bc9495e5f99993395ee2834e39ffe09e99e6caf2
Bart Ledoux <bledoux@kestra.io>
2026-03-13 12:11:16 +0100

    fix: integrations

--- src/utils/pluginIcon.ts
+++ src/utils/pluginIcon.ts
@@ -1,21 +1,23 @@
 import { API_URL } from "astro:env/client"
 import { optimizeSvgIcon } from "~/utils/svgo"
 
+const iconCache: Record<string, string> = {}
+
 export async function fetchPluginIcon(group: string): Promise<string> {
-    const iconResponse = await fetch(
-        `${API_URL}/plugins/icons/${group}`,
-    )
+    if (iconCache[group]) {
+        return iconCache[group]
+    }
+
+    const iconResponse = await fetch(`${API_URL}/plugins/icons/${group}`)
 
     if (!iconResponse.ok) {
-        throw new Error("Failed to fetch icon", {
+        throw new Error(`Failed to fetch icon "${group}"`, {
             cause: iconResponse.statusText,
         })
     }
 
-    const icon = optimizeSvgIcon(
-        await iconResponse.text(),
-        group,
-    )
+    const icon = optimizeSvgIcon(await iconResponse.text(), group)
+    iconCache[group] = icon
 
     return icon
 }
