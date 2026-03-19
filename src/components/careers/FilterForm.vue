65fcdbac80b5729beeea907c395a8d867d545657
Bart Ledoux <bledoux@kestra.io>
2026-03-19 22:57:21 +0100

    fix: add working filter

---
+++ src/components/careers/FilterForm.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+    import {
+        searchDepartment,
+        searchLocation,
+        searchString,
+    } from "./filterState"
+    defineProps<{
+        departments: Map<string, string>
+        locations: Map<string, string>
+    }>()
+</script>
+
+<template>
+    <form>
+        <input
+            type="search"
+            placeholder="Search roles"
+            name="search"
+            v-model="searchString"
+        />
+        <select v-model="searchDepartment" name="department">
+            <option value="">Department</option>
+            <option v-for="(name, id) in departments" :key="id" :value="id">
+                {{ name }}
+            </option>
+        </select>
+        <select v-model="searchLocation" name="location">
+            <option value="">Location</option>
+            <option v-for="(name, id) in locations" :key="id" :value="id">
+                {{ name }}
+            </option>
+        </select>
+    </form>
+</template>
+
+<style lang="scss" scoped>
+    form {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+        margin-bottom: 2rem;
+
+        input,
+        select {
+            padding: 0.75rem 1rem;
+            border: 1px solid var(--ks-border-primary);
+            border-radius: 4px;
+            font-size: 1rem;
+            background-color: var(--ks-background-body);
+            color: var(--ks-content-primary);
+            transition: border-color 0.3s ease;
+
+            &:focus {
+                outline: none;
+                border-color: var(--ks-content-color-highlight);
+            }
+        }
+        input {
+            flex: 1;
+        }
+        select {
+            appearance: base-select;
+        }
+    }
+</style>
---
+++ src/components/careers/Positions.vue
@@ -0,0 +1,145 @@
+<script lang="ts" setup>
+    import ArrowRight from "vue-material-design-icons/ArrowRight.vue"
+    import type { AshbyJob } from "~/utils/careers.ts"
+    import { slugify } from "@kestra-io/ui-libs"
+    import {
+        searchDepartment,
+        searchLocation,
+        searchString,
+    } from "./filterState"
+    import { computed } from "vue"
+
+    const props = defineProps<{
+        positions: Array<AshbyJob>
+    }>()
+
+    const positionsByDepartment = computed(() => {
+        const departments = new Map<string, AshbyJob["department"]>()
+        return {
+            departments,
+            positions: props.positions.reduce(
+                (acc, position) => {
+                    if (searchString.value) {
+                        const search = searchString.value.toLowerCase()
+                        if (!position.title.toLowerCase().includes(search)) {
+                            return acc
+                        }
+                    }
+                    if (
+                        searchDepartment.value &&
+                        position.department?.id !== searchDepartment.value
+                    ) {
+                        return acc
+                    }
+                    if (
+                        searchLocation.value &&
+                        position.location.id !== searchLocation.value
+                    ) {
+                        return acc
+                    }
+                    const departmentId = position.department?.id ?? "-"
+                    if (position.department) {
+                        departments.set(departmentId, position.department)
+                    }
+                    if (!acc[departmentId]) {
+                        acc[departmentId] = []
+                    }
+                    acc[departmentId].push(position)
+                    return acc
+                },
+                {} as Record<string, AshbyJob[]>,
+            ),
+        }
+    })
+</script>
+
+<template>
+    <section id="positions">
+        <div
+            v-for="(positions, departmentId) in positionsByDepartment.positions"
+            :key="departmentId"
+            class="container"
+        >
+            <h2 class="mb-4">
+                {{
+                    positionsByDepartment.departments.get(departmentId)?.name ||
+                    "Other"
+                }}
+            </h2>
+            <ul class="department-positions">
+                <li v-for="doc in positions" :key="doc.id">
+                    <a
+                        :href="`/careers/${doc.jobPostingIds[0]}-${slugify(doc.title)}`"
+                    >
+                        <div class="info-block">
+                            <span>{{ doc.title }}</span>
+                        </div>
+
+                        <div class="location-block">
+                            <span>{{ doc.location.name }}</span>
+                        </div>
+                        <div class="arrow-block">
+                            <ArrowRight alt="arrow_right" />
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    section {
+        .department-positions {
+            list-style-type: none;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            margin-bottom: 4rem;
+        }
+
+        h2 {
+            color: var(--ks-content-tertiary);
+            font-family: $font-family-monospace;
+            font-size: 1rem;
+            font-weight: 400;
+        }
+
+        li {
+            a {
+                padding: 1rem 1rem;
+                text-decoration: none;
+                display: flex;
+                align-items: center;
+                gap: 1.5rem;
+                transition: background-color 0.8s ease;
+                &:hover {
+                    background-color: var(--ks-background-secondary);
+                }
+
+                span {
+                    color: var(--ks-content-primary);
+                    font-size: $h6-font-size;
+                }
+
+                .info-block {
+                    display: flex;
+                    align-items: center;
+                    gap: 1.5rem;
+                    flex-grow: 1;
+
+                    span {
+                        font-weight: 700;
+                    }
+                }
+
+                .arrow-block {
+                    display: flex;
+                    align-items: center;
+                    gap: 1.5rem;
+                    font-size: 16px;
+                }
+            }
+        }
+    }
+</style>
