8fe5e6b125f51678b69e6b3a1472e183a8c9670f
Bart Ledoux <bledoux@kestra.io>
2026-03-23 17:13:33 +0100

    fix: more plugins parallelisation

--- src/utils/plugins/getPluginIcon.ts
+++ src/utils/plugins/getPluginIcon.ts
@@ -1,5 +1,3 @@
-import { $fetchApi } from "~/utils/fetch"
-
 function colorFixedB64Icon(b64Icon: string) {
     return Buffer.from(
         Buffer.from(b64Icon, "base64")
@@ -8,24 +6,23 @@ function colorFixedB64Icon(b64Icon: string) {
     ).toString("base64")
 }
 
-export function getPluginIconsAsString(icons: Record<string, { icon: string }>) {
+export function getPluginIconsAsString(
+    icons: Record<string, { icon: string }>,
+) {
     return Object.fromEntries(
-        (Object.entries(icons) as Array<[string, { icon: string }]>).map(([key, value]) => [
-            key,
-            colorFixedB64Icon(value.icon),
-        ]),
+        (Object.entries(icons) as Array<[string, { icon: string }]>).map(
+            ([key, value]) => [key, colorFixedB64Icon(value.icon)],
+        ),
     )
 }
 
 export async function getIcon(
-    pluginName: string,
+    originalIcons: Record<string, { icon: string }>,
+    elementIcons: Record<string, { icon: string }>,
     pluginType?: string,
     group?: string,
     subGroup?: string,
 ) {
-    const originalIcons = await $fetchApi(`/plugins/${pluginName}/icons/subgroups`)
-    const elementIcons = await $fetchApi(`/plugins/${pluginName}/icons`)
-
     const originalIconsAsString = getPluginIconsAsString(originalIcons)
     const elementIconsAsString = getPluginIconsAsString(elementIcons)
 
@@ -38,8 +35,12 @@ export async function getIcon(
     if (pluginType !== undefined) {
         icon = icons[pluginType]
         if (icon === undefined) {
-            const filteredIcons = Object.entries(icons).filter(([key]) => pluginType?.includes(key))
-            icon = filteredIcons.sort(([key1], [key2]) => key2.length - key1.length)?.[0]?.[1]
+            const filteredIcons = Object.entries(icons).filter(([key]) =>
+                pluginType?.includes(key),
+            )
+            icon = filteredIcons.sort(
+                ([key1], [key2]) => key2.length - key1.length,
+            )?.[0]?.[1]
         }
     } else if (subGroup) {
         icon = icons[subGroup]
@@ -48,7 +49,9 @@ export async function getIcon(
     }
 
     return {
-        currentPageIcon: icon ? `data:image/svg+xml;base64,${colorFixedB64Icon(icon)}` : undefined,
+        currentPageIcon: icon
+            ? `data:image/svg+xml;base64,${colorFixedB64Icon(icon)}`
+            : undefined,
         subGroupsIcons: icons,
     }
 }
