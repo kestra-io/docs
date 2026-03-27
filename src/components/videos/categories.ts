461dff08df767620805c270a75b90f5fc2d51095
Bart Ledoux <bledoux@kestra.io>
2026-03-27 10:20:23 +0100

    fix: removed unused categories file

--- src/components/videos/categories.ts
+++
@@ -1,6 +0,0 @@
-export const categories = new Map([
-    ["all", "All videos"],
-    ["deep-dive", "Deep Dive Tutorials"],
-    ["quick-start", "Quick Start Tutorials"],
-    ["feature-highlight", "Feature Highlight"],
-])
