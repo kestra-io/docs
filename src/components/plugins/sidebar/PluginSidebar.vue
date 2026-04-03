6e9b641b54bfd4d87fdf54bfc2ac6b4d019a1c7d
Piyush Bhaskar <102078527+Piyush-r-bhaskar@users.noreply.github.com>
2026-04-03 15:51:38 +0530

    feat: revamp plugins (#4437)

--- src/components/plugins/sidebar/PluginSidebar.vue
+++ src/components/plugins/sidebar/PluginSidebar.vue
@@ -1,6 +1,11 @@
 <template>
     <nav v-if="pluginWrapper || pluginsWithoutDeprecated.length > 0" class="plugin-sidebar">
-        <div class="search" data-bs-toggle="modal" data-bs-target="#search-modal" title="Search">
+        <div
+            class="search"
+            data-bs-toggle="modal"
+            data-bs-target="#search-modal"
+            title="Search"
+        >
             <div class="input-group">
                 <div class="input-icon">
                     <span class="input-group-text"><Magnify /></span>
@@ -8,52 +13,56 @@
                 </div>
             </div>
         </div>
+
         <div class="sidebar-content">
             <h3 v-if="pluginWrapper" class="plugin-title">{{ title }}</h3>
+
             <div v-if="subGroupWrappers.length > 0" class="subgroups-list">
                 <details
-                    v-for="subGroup in subGroupWrappers"
-                    :key="subGroup.subGroup"
+                    v-for="sub in subGroupWrappers"
+                    :key="sub.subGroup"
                     class="subgroup-item"
-                    :open="isSubGroupOpen(subGroup)"
+                    :open="isSubGroupOpen(sub)"
                 >
                     <summary class="subgroup-title">
-                        <ChevronRight class="chevron-icon" />
-                        <a :href="navigateToSubgroup(subGroup)" class="subgroup-link">
-                            {{ subGroupName(subGroup) }}
+                        <a :href="subGroupHref(sub)" class="subgroup-link">
+                            {{ subGroupName(sub) }}
                         </a>
+                        <ChevronRight class="chevron-icon chevron-closed" />
+                        <ChevronDown class="chevron-icon chevron-open" />
                     </summary>
+
                     <PluginElements
-                        :grouped-elements="groupPluginElements(subGroup)"
+                        :grouped-elements="groupPluginElements(sub)"
                         :plugin-name="pluginName"
-                        :sub-group="subGroup"
+                        :sub-group="sub"
                         :show-line="true"
-                        :routeParts="routeParts"
+                        :route-parts="routeParts"
                     />
                 </details>
             </div>
+
             <PluginElements
                 v-else-if="pluginWrapper && Object.keys(groupedDirectElements).length > 0"
                 :grouped-elements="groupedDirectElements"
                 :plugin-name="pluginName"
                 :show-line="true"
-                :routeParts="routeParts"
+                :route-parts="routeParts"
             />
         </div>
-
-        <hr class="my-4" />
 
         <div class="plugin-links">
             <div class="link-section">
                 <h4>Request a plugin</h4>
-                <a href="https://kestra.io/slack" external target="_blank" class="link">
-                    Ask on slack
+                <a href="https://kestra.io/slack" target="_blank" class="link">
+                    Ask on Slack <OpenInNew />
                 </a>
             </div>
+
             <div class="link-section">
                 <h4>Create a plugin</h4>
-                <a href="/docs/plugin-developer-guide" external target="_blank" class="link">
-                    Go to the developer platform
+                <a href="/docs/plugin-developer-guide" target="_blank" class="link">
+                    Go to the developer platform <OpenInNew />
                 </a>
             </div>
         </div>
@@ -69,12 +78,19 @@
         type PluginElement,
         type Plugin,
     } from "@kestra-io/ui-libs"
-
     import Magnify from "vue-material-design-icons/Magnify.vue"
     import ChevronRight from "vue-material-design-icons/ChevronRight.vue"
+    import OpenInNew from "vue-material-design-icons/OpenInNew.vue"
+    import ChevronDown from "vue-material-design-icons/ChevronDown.vue"
     import PluginElements from "~/components/plugins/PluginElements.vue"
 
-    const props = defineProps<{
+    const {
+        pluginWrapper,
+        pluginsWithoutDeprecated,
+        pluginName,
+        title,
+        routeParts,
+    } = defineProps<{
         pluginWrapper: Plugin | undefined
         pluginsWithoutDeprecated: Plugin[]
         pluginName: string
@@ -82,9 +98,17 @@
         routeParts: string[]
     }>()
 
-    const groupPluginElements = (subGroup: Plugin): Record<string, PluginElement[]> =>
+    const subGroupWrappers = computed(() =>
+        pluginsWithoutDeprecated.filter((p) => p.subGroup !== undefined),
+    )
+
+    const groupedDirectElements = computed(() =>
+        pluginWrapper ? groupPluginElements(pluginWrapper) : {},
+    )
+
+    const groupPluginElements = (plugin: Plugin): Record<string, PluginElement[]> =>
         Object.fromEntries(
-            Object.entries(subGroup)
+            Object.entries(plugin)
                 .filter(([key, value]) => isEntryAPluginElementPredicate(key, value))
                 .map(([key, value]) => [
                     key,
@@ -92,141 +116,156 @@
                 ]),
         )
 
-    const subGroupWrappers = computed(() =>
-        props.pluginsWithoutDeprecated.filter((p) => p.subGroup !== undefined),
-    )
+    const isSubGroupOpen = (sub: Plugin) =>
+        routeParts.length >= 2 &&
+        slugify(subGroupName(sub)) === routeParts[1]
 
-    const groupedDirectElements = computed(() =>
-        props.pluginWrapper ? groupPluginElements(props.pluginWrapper) : {},
-    )
-
-    const isSubGroupOpen = (subGroup: Plugin) =>
-        props.routeParts.length >= 2 && slugify(subGroupName(subGroup)) === props.routeParts[1]
-
-    const navigateToSubgroup = (subGroup: Plugin) =>
-        `/plugins/${props.pluginName}/${slugify(subGroupName(subGroup))}`
+    const subGroupHref = (sub: Plugin) =>
+        `/plugins/${pluginName}/${slugify(subGroupName(sub))}`
 </script>
 
 <style scoped lang="scss">
-
-
     .plugin-sidebar {
-        @include media-breakpoint-up(lg) {
-            padding: 2rem;
+        @include media-breakpoint-up(xl) {
             position: sticky;
             display: block !important;
             max-height: 100%;
             overflow-y: auto;
             overflow-x: hidden;
-            min-width: 260px;
-            border-right: $block-border;
+            min-width: 209px;
             z-index: 10;
         }
-        @include media-breakpoint-down(lg) {
+
+        @include media-breakpoint-down(xl) {
             display: none;
         }
+
         .sidebar-content {
             display: flex;
             flex-direction: column;
             gap: $spacer;
         }
+
         .plugin-title {
             margin: 0;
-            padding: calc($spacer / 2) $spacer;
-            font-size: $font-size-md;
+            padding: calc($spacer / 2) 0;
+            font-size: $font-size-sm;
             font-weight: 600;
             color: var(--ks-content-primary);
             text-transform: uppercase;
             overflow: hidden;
             text-overflow: ellipsis;
             white-space: nowrap;
-            @include media-breakpoint-down(lg) {
-                font-size: $font-size-sm;
-            }
         }
+
         .subgroups-list {
             display: flex;
             flex-direction: column;
             gap: calc($spacer / 2);
         }
+
         .subgroup-item {
             border: none;
             border-radius: 0;
             overflow: visible;
-            .chevron-icon {
-                margin-left: 0.5rem;
-                transition: transform 0.1s ease;
+            position: relative;
+
+            :deep(svg) {
+                position: relative;
+                top: 4px;
             }
-            &[open] .subgroup-title {
-                color: var(--ks-content-color-highlight);
-                .chevron-icon {
-                    transform-origin: 53% 60%;
-                    transform: rotate(90deg);
+
+            .chevron-open {
+                display: none;
+            }
+
+            &[open] {
+                .chevron-closed {
+                    display: none;
                 }
-                .subgroup-link {
-                    color: var(--ks-content-link);
+
+                .chevron-open {
+                    display: inline-flex;
+                }
+
+                .subgroup-title {
+                    color: var(--ks-content-color-highlight);
+
+                    .subgroup-link {
+                        color: var(--ks-content-link);
+                    }
                 }
             }
         }
+
         .subgroup-title {
             background: transparent;
             color: var(--ks-content-primary);
-            font-weight: 500;
+            font-weight: normal;
             font-size: $font-size-sm;
             cursor: pointer;
             user-select: none;
             display: flex;
             align-items: center;
-            gap: calc($spacer / 4);
+            gap: 4px;
+            padding: 0;
             margin: 0;
             border: none;
             transition: color 0.2s ease;
+
             .subgroup-link {
                 display: flex;
                 align-items: center;
                 gap: calc($spacer / 4);
                 text-decoration: none;
                 color: var(--ks-content-primary);
-                width: 100%;
+
                 &:hover {
                     color: var(--ks-content-link);
                 }
             }
+
             .chevron-icon {
                 font-size: $font-size-lg;
                 color: var(--ks-content-primary);
                 flex-shrink: 0;
                 top: -0.18rem;
+                transition: transform 0.1s ease;
             }
         }
+
         .search {
             width: 100%;
             height: 32px;
             padding: calc($spacer * 0.3) calc($spacer * 0.8);
             border-radius: calc($spacer * 0.25);
             background-color: var(--ks-background-secondary);
-            border: 1px solid var(--ks-border-primary);
+            border: $block-border;
             margin: 0 auto $spacer;
             cursor: pointer;
-            @include media-breakpoint-down(lg) {
-                width: 100%;
+
+            @include media-breakpoint-down(xl) {
                 margin-top: $spacer;
             }
+
             :deep(.material-design-icon__svg) {
                 bottom: 0;
                 fill: #8b8b8d;
             }
+
             .input-group {
                 width: 100%;
                 height: 100%;
                 display: flex;
                 gap: calc($spacer * 0.5);
                 align-items: center;
+
                 .input-icon {
                     max-height: 100%;
                     display: flex;
                     gap: calc($spacer * 0.5);
                     align-items: center;
+
                     input {
                         background: transparent;
                         border: none;
@@ -235,11 +274,13 @@
                         font-weight: 400;
                         font-size: $font-size-sm;
                         width: 100%;
+
                         &::placeholder {
                             color: var(--ks-content-inactive);
                         }
                     }
                 }
+
                 .input-group-text {
                     max-height: 100%;
                     background-color: transparent;
@@ -249,32 +290,30 @@
                 }
             }
         }
+
         .plugin-links {
             display: flex;
             flex-direction: column;
             gap: $spacer;
-            margin-top: $spacer;
+            margin-top: calc($spacer * 2);
+
             .link-section {
                 h4 {
-                    font-size: 1rem;
-                    font-weight: 700;
+                    font-size: $font-size-sm;
                     color: var(--ks-content-primary);
                     margin: 0;
                 }
+
                 .link {
                     color: var(--ks-content-link);
                     text-decoration: none;
-                    font-size: 14px;
-                    font-weight: normal;
+                    font-size: $font-size-xs;
+
                     &:hover {
                         text-decoration: underline;
                     }
                 }
             }
-        }
-        hr {
-            border-color: var(--ks-border-secondary);
-            opacity: 1;
         }
     }
 </style>
