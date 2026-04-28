f774a246166e0670a633d31edb6e986d93fb894b
MartinRst <mproset@kestra.io>
2026-04-28 11:35:04 +0200

    software providers page

--- src/utils/menu-sizes.ts
+++ src/utils/menu-sizes.ts
@@ -7,7 +7,7 @@ const menuSizes: Record<string, MenuSize> = {
     },
     solutions: {
         width: "782px",
-        height: "300px"
+        height: "340px"
     },
     resources: {
         width: "285px",
