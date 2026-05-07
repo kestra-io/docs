96723714155d2b5441b3bb82f4cee88a9c55b1a6
AJ Emerich <aj-emerich@proton.me>
2026-05-07 16:41:28 +0200

    Update getNavigationTree.ts

--- src/utils/getNavigationTree.ts
+++ src/utils/getNavigationTree.ts
@@ -9,7 +9,7 @@ const navigationTree = {
         "Tutorial",
         "Architecture",
         "Contribute to Kestra",
-        "User Interface & No Code",
+        "UI & No Code",
         // "Video Tutorials"
     ],
     "Build with Kestra": [
