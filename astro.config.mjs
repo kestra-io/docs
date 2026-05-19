87e69e12b2e2680e66c3690701ac0301d44517f8
vfanucci <vfanucci@kestra.io>
2026-05-19 12:13:48 +0200

    seo(redirects): add /sitemap_index.xml -> /sitemap/index.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -213,6 +213,7 @@ export default defineConfig({
         "/slack": "https://api.kestra.io/v1/communities/slack/redirect",
         "/trust":
             "https://app.drata.com/trust/0a8e867d-7c4c-4fc5-bdc7-217f9c839604",
+        "/sitemap_index.xml": "/sitemap/index.xml",
     },
     vite: {
         plugins: [
